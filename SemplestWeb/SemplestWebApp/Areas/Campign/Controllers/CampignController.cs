0dcf60e83b468d8c8be4327d26cff14c50badb7f
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-03-21 20:15:39 +0000

    Ver 1
    git-svn-id: file:///semplestb/final_repo@11 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestWebApp/Areas/Campign/Controllers/CampignController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SemplestWebApp.Areas.Campign.Controllers
+{
+    public class CampignController : Controller
+    {
+        //
+        // GET: /Campign/Campign/
+
+        public ActionResult CampignSetup()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SemplestWeb/SemplestWebApp/Services/SemplestDataService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace SemplestWebApp.Services
+{
+    public class SemplestDataService : DataService<SemplestWebApp.Models.SemplestEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            config.SetEntitySetAccessRule("Campaign", EntitySetRights.AllRead
+                    | EntitySetRights.WriteMerge | EntitySetRights.WriteReplace);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+    }
+}
