32f18cade6d57dc36cb3a3e6f014d7af32d2600d
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-03-23 20:00:38 +0000

    git-svn-id: file:///semplestb/final_repo@46 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestBidding/src/semplest/bidding/optimization/GaussPdf.java
@@ -0,0 +1,17 @@
+package semplest.bidding.optimization;
+
+public class GaussPdf extends ParametricFunction {
+
+	
+	@Override
+	public double function(double[] input) throws Exception  {
+		throw new Exception("Parameters must be specified for Gaussian distribution");
+//		return 0;
+	}
+
+	@Override
+	public double function(double[] input, double[] parameters) {
+		return Math.exp(-0.5*Math.pow((input[0]-parameters[0])/parameters[1],2))/Math.sqrt(2*Math.PI)/parameters[1];
+	}
+
+}
---
+++ SEMplestBidding/src/semplest/bidding/optimization/ParameterEstimator.java
@@ -0,0 +1,22 @@
+package semplest.bidding.optimization;
+
+
+import flanagan.math.*;
+
+class MinimFunct implements MinimisationFunction{
+
+	@Override
+	public double function(double[] param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
+
+
+public class ParameterEstimator {
+	
+	
+	
+	
+
+}
