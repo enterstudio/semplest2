ea3f46ad24b7c443a6615f8f81c3141c07531091
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-03-23 20:29:05 +0000

    git-svn-id: file:///semplestb/final_repo@55 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestBidding/src/semplest/bidding/optimization/ParameterEstimator.java
+++ SEMplestBidding/src/semplest/bidding/optimization/ParameterEstimator.java
@@ -15,7 +15,42 @@ class MinimFunct implements MinimisationFunction{
 
 public class ParameterEstimator {
 	
+	private ParametricFunction pf = null;
+	private Minimisation min = new Minimisation();
+
+	private double [][] input = null;
+	private double [][] output = null;
 	
+	public ParameterEstimator(ParametricFunction pf){
+		this.pf=pf;
+	}
+	
+	public void estimateParams(){
+		
+		// starting point
+		double[] start = {-5.0};
+		
+		// initial step sizes
+	    double[] step = {0.2D};
+
+	    // convergence tolerance
+	    double ftol = 1e-15;
+
+	    // Nelder and Mead minimisation procedure
+	    min.nelderMead(f, start, step, ftol);
+	}
+	
+	
+	
+	public static void main(String [] args){
+		GaussPdf pdf = new GaussPdf();
+		
+		ParameterEstimator pe = new ParameterEstimator(pdf);
+		
+		double [] x = {0.0};
+		double [] parameters = {0.0, 1.0};
+		System.out.println(pdf.function(x, parameters));
+	}
 	
 	
 
--- SEMplestBidding/src/semplest/bidding/optimization/ParametricFunction.java
+++ SEMplestBidding/src/semplest/bidding/optimization/ParametricFunction.java
@@ -2,9 +2,9 @@ package semplest.bidding.optimization;
 
 public class ParametricFunction {
 	
-	public double function(double [] input) throws Exception{
-		throw new Exception("This is the base class. You must call a derived class method.");
-	}
+//	public double function(double [] input) throws Exception{
+//		throw new Exception("This is the base class. You must call a derived class method.");
+//	}
 	public double function(double [] input, double [] parameters) throws Exception{
 		throw new Exception("This is the base class. You must call a derived class method.");
 	}
