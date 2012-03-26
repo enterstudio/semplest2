63245f87be31adf69f2902b66dd4ebc6541c601c
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-03-26 19:11:23 +0000

    Ver 1
    git-svn-id: file:///semplestb/final_repo@79 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAdminApp/SemplestAdminApp/Controllers/AdminUserAccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SemplestAdminApp.Controllers
+{
+    public class AdminUserAccountController : Controller
+    {
+        //
+        // GET: /AdminUserAccount/
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+    }
+}
