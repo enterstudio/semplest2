dc2d24fe091a362ddc62e2da9948de2a71198220
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-03-26 15:05:40 +0000

    git-svn-id: file:///semplestb/final_repo@75 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestBidding/src/semplest/bidding/estimation/WeibullCurve.java
+++ SEMplestBidding/src/semplest/bidding/estimation/WeibullCurve.java
@@ -2,6 +2,9 @@ package semplest.bidding.estimation;
 
 public class WeibullCurve implements ParametricFunction {
 
+	
+	// This is not a distribution function. The parameter[2] is a scaling and if it is set to 1 then
+	// we get a distribution function.
 	@Override
 	public double function(double[] input, double[] parameters) {
 		// parameters[0]=lambda, parameters[1]=theta, parmaters[2]=amplitude
