75f64011f12710212882b45b4726373518d1a638
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-03-26 20:52:25 +0000

    git-svn-id: file:///semplestb/final_repo@81 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestBidding/src/semplest/bidding/optimization/KeyWordInterface.java
@@ -0,0 +1,11 @@
+package semplest.bidding.optimization;
+
+public interface KeyWordInterface {
+	
+	public String getKeyWord();
+	public double [] getBidInfo();
+	public double [][] getClickInfo();
+	public double [][] getCostInfo();
+	public double [][] getPosInfo();
+
+}
