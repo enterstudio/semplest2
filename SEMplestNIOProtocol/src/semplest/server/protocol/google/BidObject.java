73d3e0aa978abbf081590bcea6f91ecad0411dac
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-03-27 14:41:06 +0000

    git-svn-id: file:///semplestb/final_repo@88 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestNIOProtocol/src/semplest/server/protocol/google/BidObject.java
@@ -0,0 +1,52 @@
+package semplest.server.protocol.google;
+
+public class BidObject
+{
+
+	private Long bidID;
+	private String keyword;
+	private Long microBidAmount;
+	private String approvalStatus;
+	private String matchType;
+	public Long getBidID()
+	{
+		return bidID;
+	}
+	public void setBidID(Long bidID)
+	{
+		this.bidID = bidID;
+	}
+	public String getKeyword()
+	{
+		return keyword;
+	}
+	public void setKeyword(String keyword)
+	{
+		this.keyword = keyword;
+	}
+	public Long getMicroBidAmount()
+	{
+		return microBidAmount;
+	}
+	public void setMicroBidAmount(Long microBidAmount)
+	{
+		this.microBidAmount = microBidAmount;
+	}
+	public String getApprovalStatus()
+	{
+		return approvalStatus;
+	}
+	public void setApprovalStatus(String approvalStatus)
+	{
+		this.approvalStatus = approvalStatus;
+	}
+	public String getMatchType()
+	{
+		return matchType;
+	}
+	public void setMatchType(String matchType)
+	{
+		this.matchType = matchType;
+	}
+	
+}
