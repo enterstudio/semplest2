2509bc537b9385a727d0ca949d69d785f1576537
tutor <tutor@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-03 21:19:30 +0000

    git-svn-id: file:///semplestb/final_repo@177 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAdminApp/SemplestAdminApp/Controllers/CreateNewClientAccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SemplestAdminApp.Controllers
+{
+    public class CreateNewClientAccountController : Controller
+    {
+        //
+        // GET: /CreateNewClient/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
