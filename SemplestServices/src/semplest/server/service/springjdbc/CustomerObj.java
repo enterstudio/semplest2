392be21b762e7fc6599fbeb12639d292d8ba93d4
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-03 16:02:11 +0000

    git-svn-id: file:///semplestb/final_repo@157 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestServices/src/semplest/server/service/springjdbc/CustomerObj.java
@@ -0,0 +1,9 @@
+package semplest.server.service.springjdbc;
+
+public class CustomerObj
+{
+	private String Name;
+	private Double TotalCycleTargetBudget;
+	
+
+}
---
+++ SemplestServices/src/semplest/service/keywords/lda/KeywordGeneratorServiceImpl.java
@@ -0,0 +1,21 @@
+package semplest.service.keywords.lda;
+
+import java.util.ArrayList;
+
+import semplest.services.client.interfaces.SemplestKeywordLDAServiceInterface;
+
+public class KeywordGeneratorServiceImpl implements SemplestKeywordLDAServiceInterface
+{
+	public String getCategories(String json) throws Exception
+	{
+		return null;
+	}
+
+	@Override
+	public ArrayList<String> getCategories(String[] searchTerm) throws Exception
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
