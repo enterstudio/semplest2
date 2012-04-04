c8ea596a1d39d0714695045f7431aa7c29bd8783
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-04 18:21:28 +0000

    git-svn-id: file:///semplestb/final_repo@236 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/service/bidding/BidGeneratorServiceImpl.java
+++ SemplestServices/src/semplest/service/bidding/BidGeneratorServiceImpl.java
@@ -5,12 +5,15 @@ import java.util.HashMap;
 
 import org.apache.log4j.Logger;
 
+import com.google.gson.Gson;
+
 //import com.google.gson.Gson;
 
 import semplest.services.client.interfaces.SemplestBiddingInterface;
 
 public class BidGeneratorServiceImpl implements SemplestBiddingInterface {
 	
+	private static Gson gson = new Gson();
 	private static final Logger logger = Logger.getLogger(BidGeneratorServiceImpl.class);
 
 	@Override
@@ -19,6 +22,17 @@ public class BidGeneratorServiceImpl implements SemplestBiddingInterface {
 		
 	}
 
+	public String getBid(String json) throws Exception
+	{
+		logger.debug("call  getBid(String json)" + json);
+		HashMap<String, String> data = gson.fromJson(json, HashMap.class);
+		Integer customerID = Integer.parseInt(data.get("customerID"));
+		Integer campaignID = Integer.parseInt(data.get("campaignID")); 
+		Integer adGroupID = Integer.parseInt(data.get("adGroupID"));
+		ArrayList<String> keywords = gson.fromJson(data.get("keywords"), ArrayList.class);
+		HashMap<String,Double> res = getBid(customerID,campaignID,adGroupID, keywords);
+		return gson.toJson(res);
+	}
 	@Override
 	public HashMap<String,Double> getBid(Integer customerID,
 			Integer campaignID, Integer adGroupID, ArrayList<String> keywords)
