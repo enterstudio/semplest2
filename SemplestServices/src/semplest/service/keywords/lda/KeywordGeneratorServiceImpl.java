8424b59946fe11e882d1a76b8a1a5a24ca85f54b
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-04 12:29:13 +0000

    git-svn-id: file:///semplestb/final_repo@193 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/service/keywords/lda/KeywordGeneratorServiceImpl.java
+++ SemplestServices/src/semplest/service/keywords/lda/KeywordGeneratorServiceImpl.java
@@ -1,16 +1,28 @@
 package semplest.service.keywords.lda;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
 
 import semplest.keywords.lda.KWGenDmozLDAServer;
 import semplest.services.client.interfaces.SemplestKeywordLDAServiceInterface;
 
 public class KeywordGeneratorServiceImpl implements SemplestKeywordLDAServiceInterface
 {
+	private static Gson gson = new Gson();
+	private static final Logger logger = Logger.getLogger(KeywordGeneratorServiceImpl.class);
+	
 	private KWGenDmozLDAServer kwGen;
 	public String getCategories(String json) throws Exception
 	{
-		return null;
+		logger.debug("call  getCategories(String json)" + json);
+		HashMap<String, String> data = gson.fromJson(json, HashMap.class);
+		String[] searchTerm = new String[] {data.get("searchTerm")};
+		ArrayList<String> res = getCategories(searchTerm);
+		return gson.toJson(res);
 	}
 
 	@Override
