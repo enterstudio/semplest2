fbb770d2b112b5003455e89793983060a8db89fb
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-05 00:48:28 +0000

    git-svn-id: file:///semplestb/final_repo@255 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/TestServiceInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/TestServiceInterface.java
@@ -1,7 +1,7 @@
 package semplest.services.client.interfaces;
 
 
-public interface TestServiceInterface
+public interface TestServiceInterface extends ServiceInitialize
 {
-	public abstract String TestMethod(String uniqueID);
+	public abstract String TestMethod(String uniqueID) throws Exception;
 }
