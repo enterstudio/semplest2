7bea99966f3050c7657e1bf551ff4201d169dcbb
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-05 01:23:52 +0000

    git-svn-id: file:///semplestb/final_repo@259 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestESB/src/semplest/server/ESB/AsyncServletListener.java
+++ SemplestESB/src/semplest/server/ESB/AsyncServletListener.java
@@ -1,15 +1,21 @@
 package semplest.server.ESB;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.AsyncContext;
 import javax.servlet.AsyncEvent;
 import javax.servlet.AsyncListener;
 
 import org.apache.log4j.Logger;
 
+import semplest.server.protocol.ProtocolJSON;
+
 public class AsyncServletListener implements AsyncListener
 {
 
 	private static final Logger logger = Logger.getLogger(AsyncServletListener.class);
-	
+	private static ProtocolJSON json = new ProtocolJSON();
 
 	// Public constructor is required by servlet spec
 	public AsyncServletListener()
@@ -18,17 +24,42 @@ public class AsyncServletListener implements AsyncListener
 
 	public void onComplete(AsyncEvent ae)
 	{
-		//logger.info("AsyncListener: Completed work: " +	ae.getAsyncContext().getRequest().getParameter("uniqueID"));
+		// logger.info("AsyncListener: Completed work: " +
+		// ae.getAsyncContext().getRequest().getParameter("uniqueID"));
 	}
 
 	public void onTimeout(AsyncEvent ae)
 	{
-		String uniqueID = ae.getAsyncContext().getRequest().getParameter("uniqueID");
-		logger.error("AsyncListener: Timeout: " + uniqueID + ":" + ae.getAsyncContext().getRequest().getParameter("service") + ":" + ae.getAsyncContext().getRequest().getParameter("method"));
+		String uniqueID = (String) ae.getAsyncContext().getRequest().getAttribute("uniqueID");
+		logger.error("AsyncListener: Timeout: " + uniqueID + ":" + ae.getAsyncContext().getRequest().getParameter("service") + ":"
+				+ ae.getAsyncContext().getRequest().getParameter("method"));
 		if (ESBServer.esb.getServletAsynchContextMap().containsKey(uniqueID))
 		{
-			ESBServer.esb.getServletAsynchContextMap().remove(uniqueID);
-			logger.info("Removed " + uniqueID + "  from ServletAsynchContextMap");
+			HashMap<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("errorMessage", "Service Timeout");
+			errorMap.put("errorType", "Timeout");
+			errorMap.put("errorID", uniqueID);
+			String ret = json.createJSONHashmap(errorMap);
+			AsyncContext asyncContext = ESBServer.esb.getServletAsynchContextMap().get(uniqueID);
+			if (asyncContext != null)
+			{
+				asyncContext.getResponse().setContentType("text/html");
+				try
+				{
+					PrintWriter out = asyncContext.getResponse().getWriter();
+					out.print(ret);
+				}
+				catch (Exception e)
+				{
+					logger.error(e.getMessage(), e);
+				}
+				asyncContext.complete();
+
+				ESBServer.esb.getServletAsynchContextMap().remove(uniqueID);
+				logger.info("Removed " + uniqueID + "  from ServletAsynchContextMap");
+
+			}
+
 		}
 	}
 
