c7ef60f101f0b1c3424bc7b260fc69d7ac35b4f8
nan <nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-06 16:54:07 +0000

    git-svn-id: file:///semplestb/final_repo@332 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/service/msn/adcenter/MSNAdcenterService.java
+++ SemplestServices/src/semplest/service/msn/adcenter/MSNAdcenterService.java
@@ -16,10 +16,9 @@ public class MSNAdcenterService implements ServiceInterface
 	private static final Logger logger = Logger.getLogger(MSNAdcenterService.class);
 
 	@Override
-	public String ServiceGet(String methodName, String jsonStr)
+	public String ServiceGet(String methodName, String jsonStr) throws Exception
 	{
-		try
-		{
+		
 			AdCenterCredentials adCenterCredentials = new AdCenterCredentialsProduction();
 			TimeServer timeServer = new TimeServerImpl();
 		
@@ -28,33 +27,6 @@ public class MSNAdcenterService implements ServiceInterface
 			Class[] parameterTypes = new Class[] {String.class};
 			Method method = service.getClass().getMethod(methodName, parameterTypes);
 			return (String) method.invoke(service,jsonStr);
-		}
-		catch (SecurityException e)
-		{
-			logger.error(e);
-			e.printStackTrace();
-		}
-		catch (IllegalArgumentException e)
-		{
-			logger.error(e);
-			e.printStackTrace();
-		}
-		catch (NoSuchMethodException e)
-		{
-			logger.error(e);
-			e.printStackTrace();
-		}
-		catch (IllegalAccessException e)
-		{
-			logger.error(e);
-			e.printStackTrace();
-		}
-		catch (InvocationTargetException e)
-		{
-			logger.error(e);
-			e.printStackTrace();
-		}
-		return "MSNAdcenterService Error running " + methodName ;
 		
 	}
 
