d86461f43d02d1814d3a44011342510770a11f6c
tutor <tutor@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-06 17:04:02 +0000

    git-svn-id: file:///semplestb/final_repo@336 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAdminApp/SemplestAdminApp/Controllers/AccountServiceController.cs
+++ SemplestWeb/SemplestAdminApp/SemplestAdminApp/Controllers/AccountServiceController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace SemplestAdminApp.Controllers
 {
-    public class ClientAccountController : Controller
+    public class AccountServiceController : Controller
     {
         //
         // GET: /ClientAccount/
