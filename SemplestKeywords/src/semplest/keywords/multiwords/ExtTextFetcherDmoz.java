08ca671500d4e221a05c53ea9fa2b21ceb019ab6
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-06 16:45:35 +0000

    git-svn-id: file:///semplestb/final_repo@330 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestKeywords/src/semplest/keywords/multiwords/ExtTextFetcherDmoz.java
+++ SemplestKeywords/src/semplest/keywords/multiwords/ExtTextFetcherDmoz.java
@@ -263,6 +263,120 @@ public void fetchMultiWordsSubsequent(int [] n, int minCount){
 		
 	}
 	
+
+public void MultiWordsNoFetch(int [] n, int minCount){
+	
+    Crawler c = new Crawler(); 
+
+	ArrayList<String> lines = ioUtils.readFile(inFileName);
+	
+	HashSet<String> tags = new HashSet<String>();
+
+	String urls, tag;
+	
+	String [] results;
+	
+	FileWriter fstream;
+	BufferedWriter out;
+
+	FileWriter fstream2;
+	BufferedWriter out2;
+	
+	
+	ArrayList<String> doneLines = ioUtils.readFile(outFileName);
+	HashSet<String> doneTags = new HashSet<String>();
+	for (String line : doneLines){
+		line=line.replaceAll(" .*$","");
+		doneTags.add(line);
+	}
+	
+	
+	
+	ArrayList<String> list=null;
+	
+	int totalJobs, doneSoFar=0;
+
+
+	if (patternInCategory.equals("")){
+
+		for (String line: lines){
+			urls=line.replaceFirst("^\\S+ : ", "");
+			tag=line.replaceFirst(" :.*$", "");
+			if(!doneTags.contains(tag)){						
+				tags.add(tag);
+				c.add(tag,urls);
+			}
+		}
+	} else {
+
+		for (String line: lines){
+			tag=line.replaceFirst(" :.*$", "");
+			if (tag.contains(patternInCategory)){
+				urls=line.replaceFirst("^\\S+ : ", "");
+				if(!doneTags.contains(tag)){						
+					tags.add(tag);
+					c.add(tag,urls);
+				}
+
+			}
+		}
+	}
+			
+	totalJobs=tags.size();
+	
+
+	
+	// continue until all are done
+	while(!tags.isEmpty()) {
+		
+		try{ // try:sleep
+			Thread.sleep(3000);
+		} catch( Exception e) {
+			e.printStackTrace();
+		} // try: sleep
+		
+		// fetch from Worker
+		results = c.fetch();
+		if (results.length > 0) {				
+			System.out.println("I am happy. I found "+ results.length+" results...................");
+
+			try { // try: IO
+				fstream = new FileWriter(outFileName,true);
+				out = new BufferedWriter(fstream);
+
+				fstream2 = new FileWriter(outFileName+".orig.2",true);
+				out2 = new BufferedWriter(fstream2);
+
+				for(int i=0;i<results.length; i++){  // for every unit of work
+					// extract the tag first
+					tag=results[i].replaceAll(" .*", "");
+					out.write(tag);
+					list=MultiWords.getMultiWords(results[i], n, minCount);
+					
+					out2.write(results[i]+"\n");
+					
+					for (int j=0;j<n.length;j++){
+						out.write(" ** "+list.get(j));
+					}
+					out.write("\n");
+					doneSoFar++;
+					tags.remove(tag);
+					System.out.println("MultiWordExtraction: "+doneSoFar+" out of "+totalJobs+" done so far. Waiting for "+tags.size()+" jobs to complete.");
+				}
+				
+				out.close();
+				out2.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} // try: IO
+		
+		} else
+			System.out.println("I am sad. I didn't find anything....... still waiting for "+tags.size()+" jobs.");
+
+	} // while
+	
+}
 	/**
 	 * @param args
 	 */
