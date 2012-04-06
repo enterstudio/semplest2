48bf5f2437036b47390a3b59bde20a02eea1db27
lluis <lluis@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-06 13:42:49 +0000

    new interface for keyword service
    git-svn-id: file:///semplestb/final_repo@305 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestKeywordLDAServiceInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestKeywordLDAServiceInterface.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public interface SemplestKeywordLDAServiceInterface extends ServiceInitialize
 {
-	public abstract ArrayList<String> getCategories(String[] searchTerm) throws Exception;
-	public abstract ArrayList<ArrayList<String>> getKeywords(ArrayList<String> categories, String data, int numKw, int[] nGrams) throws Exception;
-
+	public abstract ArrayList<String> getCategories(String companyName, String productSubcategory, String description, String[] adds, String url) throws Exception;
+	public abstract ArrayList<ArrayList<String>> getKeywords(ArrayList<String> categories,String companyName, String productSubcategory, String description, String[] adds, String url, int[] nGrams) throws Exception;
 }
