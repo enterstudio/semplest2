408aa1a3bd5177ffe5890ae1e5c3dbb431ee5631
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-09 15:41:38 +0000

    changed for 4 and 5 length keywords
    git-svn-id: file:///semplestb/final_repo@375 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestKeywords/src/semplest/keywords/multiwords/utils.java
+++ SemplestKeywords/src/semplest/keywords/multiwords/utils.java
@@ -13,14 +13,20 @@ public class utils {
 	public static void tmpToSemplest(String inFileName, String outFileName){
 		ArrayList<String> lines = ioUtils.readFile(inFileName);
 		String [] tuples;
-		StringBuilder buffer2, buffer3;
-		int count2, count3;
+		StringBuilder buffer2, buffer3, buffer4, buffer5;
+		int count2, count3, count4, count5;
 		
 		FileWriter fstream2;
 		BufferedWriter out2;
 		
 		FileWriter fstream3;
 		BufferedWriter out3;
+		
+		FileWriter fstream4;
+		BufferedWriter out4;	
+		
+		FileWriter fstream5;
+		BufferedWriter out5;
 		
 		try {
 			File file =new File(outFileName+".2"); // biwords
@@ -36,6 +42,22 @@ public class utils {
 			
 			fstream3 = new FileWriter(outFileName+".3",true);
 			out3 = new BufferedWriter(fstream3);
+			
+			File file4 =new File(outFileName+".4"); // biwords
+			file4.delete();
+			file4.createNewFile();
+			
+			fstream4 = new FileWriter(outFileName+".4",true);
+			out4 = new BufferedWriter(fstream4);
+			
+			File file5 =new File(outFileName+".5"); // biwords
+			file5.delete();
+			file5.createNewFile();
+			
+			fstream5 = new FileWriter(outFileName+".5",true);
+			out5 = new BufferedWriter(fstream5);
+			
+			
 
 			for (String line : lines){
 				
@@ -48,24 +70,47 @@ public class utils {
 							
 				buffer2 = new StringBuilder();
 				buffer3= new StringBuilder();
-				count2=0; count3=0;
+				buffer4= new StringBuilder();
+				buffer5= new StringBuilder();
+
+				count2=0; count3=0; count4=0; count5=0;
 				for (int i=1;i<tuples.length;i++){
-					if(tuples[i].split("\\+").length==2){
+					switch (tuples[i].split("\\+").length) {
+		            case 2:  
 						buffer2.append(tuples[i]+" ");
 						count2++;
-					} else {
+						break;
+		            case 3:
 						buffer3.append(tuples[i]+" ");
 						count3++;
+						break;
+		            case 4:
+						buffer4.append(tuples[i]+" ");
+						count4++;
+						break;
+		            case 5:
+						buffer5.append(tuples[i]+" ");
+						count5++;
+						break;
+					default:
+						System.out.println("Found length: "+tuples[i].split("\\+").length);
+						break;
 					}
+							
+						
 				}
 				
 				out2.write(tuples[0]+":"+count2+" "+buffer2+"\n");
 				out3.write(tuples[0]+":"+count3+" "+buffer3+"\n");
+				out4.write(tuples[0]+":"+count4+" "+buffer4+"\n");
+				out5.write(tuples[0]+":"+count5+" "+buffer5+"\n");
 							
 				}
 
 			out2.close();
 			out3.close();
+			out4.close();
+			out5.close();
 			
 		} catch (Exception e){
 			e.printStackTrace();
@@ -82,6 +127,8 @@ public class utils {
 		String category = "reference";
 //		tmpToSemplest("/semplest/subhojit/data/dmoz/FullExtText/"+category+".txt","/tmp/"+category+".txt");
 //		tmpToSemplest("/semplest/subhojit/data/dmoz/FullExtText/"+category+".txt","/semplest/data/dmoz/multiwords/"+category+".txt");
+//		tmpToSemplest("/tmp/subhojit.txt","/semplest/data/dmoz/multiwords/all.txt");
+
 
 	}
 
