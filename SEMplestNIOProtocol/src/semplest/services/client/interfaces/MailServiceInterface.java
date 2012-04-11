9cc133636a4266e1e43acf2f8307a2c4cf312248
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-11 18:55:17 +0000

    git-svn-id: file:///semplestb/final_repo@454 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/MailServiceInterface.java
+++
@@ -1,6 +0,0 @@
-package semplest.services.client.interfaces;
-
-public interface MailServiceInterface
-{
-	public abstract Boolean SendEmail(String subject, String recipient, String msgTxt, String filename);
-}
