c941d234c4b2e84c8daab4c24e9afc0bdedc308f
nan <nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-11 14:41:30 +0000

    git-svn-id: file:///semplestb/final_repo@447 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestNIOProtocol/src/semplest/server/protocol/msn/MsnAccountObject.java
@@ -0,0 +1,166 @@
+package semplest.server.protocol.msn;
+
+import java.util.Calendar;
+
+import org.apache.axis.types.UnsignedByte;
+
+import com.microsoft.adcenter.api.customermanagement.Entities.*;
+
+public class MsnAccountObject {
+	
+	private AccountFinancialStatus AccountFinancialStatus;
+	private AccountLifeCycleStatus AccountLifeCycleStatus;
+	private AccountType AccountType;
+	private Long BillToCustomerId;
+	private String CountryCode;
+	private CurrencyType CurrencyType;
+	private Long Id;
+	private LanguageType Language;
+	private Long LastModifiedByUserId;
+	private Calendar LastModifiedTime;
+	private String Name;
+	private String Number;
+	private Long ParentCustomerId;
+	private UnsignedByte PauseReason;
+	private Long PaymentMethodId;
+	private PaymentMethodType PaymentMethodType;
+	private Long PrimaryUserId;
+	// private byte[] TimeStamp;  //This array will cause json crash. Need to convert it to a plain number, such as Long.
+	private TimeZoneType TimeZone;
+	
+	public MsnAccountObject(Account account){
+		AccountFinancialStatus = account.getAccountFinancialStatus();
+		AccountLifeCycleStatus = account.getAccountLifeCycleStatus();
+		AccountType = account.getAccountType();
+		BillToCustomerId = account.getBillToCustomerId();
+		CountryCode = account.getCountryCode();
+		CurrencyType = account.getCurrencyType();
+		Id = account.getId();
+		Language = account.getLanguage();
+		LastModifiedByUserId = account.getLastModifiedByUserId();
+		LastModifiedTime = account.getLastModifiedTime();
+		Name = account.getName();
+		Number = account.getNumber();
+		ParentCustomerId = account.getParentCustomerId();
+		PauseReason = account.getPauseReason();
+		PaymentMethodId = account.getPaymentMethodId();
+		PaymentMethodType = account.getPaymentMethodType();
+		PrimaryUserId = account.getPrimaryUserId();
+		TimeZone = account.getTimeZone();
+		
+	}
+	
+	public AccountFinancialStatus getAccountFinancialStatus() {
+		return AccountFinancialStatus;
+	}
+	public void setAccountFinancialStatus(
+			AccountFinancialStatus accountFinancialStatus) {
+		AccountFinancialStatus = accountFinancialStatus;
+	}
+	public AccountLifeCycleStatus getAccountLifeCycleStatus() {
+		return AccountLifeCycleStatus;
+	}
+	public void setAccountLifeCycleStatus(
+			AccountLifeCycleStatus accountLifeCycleStatus) {
+		AccountLifeCycleStatus = accountLifeCycleStatus;
+	}
+	public AccountType getAccountType() {
+		return AccountType;
+	}
+	public void setAccountType(AccountType accountType) {
+		AccountType = accountType;
+	}
+	public Long getBillToCustomerId() {
+		return BillToCustomerId;
+	}
+	public void setBillToCustomerId(Long billToCustomerId) {
+		BillToCustomerId = billToCustomerId;
+	}
+	public String getCountryCode() {
+		return CountryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		CountryCode = countryCode;
+	}
+	public CurrencyType getCurrencyType() {
+		return CurrencyType;
+	}
+	public void setCurrencyType(CurrencyType currencyType) {
+		CurrencyType = currencyType;
+	}
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public LanguageType getLanguage() {
+		return Language;
+	}
+	public void setLanguage(LanguageType language) {
+		Language = language;
+	}
+	public Long getLastModifiedByUserId() {
+		return LastModifiedByUserId;
+	}
+	public void setLastModifiedByUserId(Long lastModifiedByUserId) {
+		LastModifiedByUserId = lastModifiedByUserId;
+	}
+	public Calendar getLastModifiedTime() {
+		return LastModifiedTime;
+	}
+	public void setLastModifiedTime(Calendar lastModifiedTime) {
+		LastModifiedTime = lastModifiedTime;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getNumber() {
+		return Number;
+	}
+	public void setNumber(String number) {
+		Number = number;
+	}
+	public Long getParentCustomerId() {
+		return ParentCustomerId;
+	}
+	public void setParentCustomerId(Long parentCustomerId) {
+		ParentCustomerId = parentCustomerId;
+	}
+	public UnsignedByte getPauseReason() {
+		return PauseReason;
+	}
+	public void setPauseReason(UnsignedByte pauseReason) {
+		PauseReason = pauseReason;
+	}
+	public Long getPaymentMethodId() {
+		return PaymentMethodId;
+	}
+	public void setPaymentMethodId(Long paymentMethodId) {
+		PaymentMethodId = paymentMethodId;
+	}
+	public PaymentMethodType getPaymentMethodType() {
+		return PaymentMethodType;
+	}
+	public void setPaymentMethodType(PaymentMethodType paymentMethodType) {
+		PaymentMethodType = paymentMethodType;
+	}
+	public Long getPrimaryUserId() {
+		return PrimaryUserId;
+	}
+	public void setPrimaryUserId(Long primaryUserId) {
+		PrimaryUserId = primaryUserId;
+	}
+	public TimeZoneType getTimeZone() {
+		return TimeZone;
+	}
+	public void setTimeZone(TimeZoneType timeZone) {
+		TimeZone = timeZone;
+	}
+	
+	
+
+}
