e4845d6287f75e34596a32a6066a9da61626999f
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-17 20:56:33 +0000

    git-svn-id: file:///semplestb/final_repo@549 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestBidding/src/semplest/bidding/estimation/EstimatorData.java
+++ SEMplestBidding/src/semplest/bidding/estimation/EstimatorData.java
@@ -6,33 +6,41 @@ import java.util.Set;
 
 public class EstimatorData {
 	
-	HashMap<Double,Double> bidVsTarget;
+	HashMap<Double,Double> targetVsBid;
 	
 	public EstimatorData(){
-		bidVsTarget = new HashMap<Double,Double>();
+		targetVsBid = new HashMap<Double,Double>();
 	}
 	
 	public void addData(double bid, double data){
-		bidVsTarget.put(new Double(bid), new Double(data));
+		targetVsBid.put(new Double(bid), new Double(data));
 	}
 	
 	public void addData(Double bid, Double data){
-		bidVsTarget.put(bid, data);
+		targetVsBid.put(bid, data);
 	}
 	
 	public Double getData(Double bid){
-		return bidVsTarget.get(bid);
+		return targetVsBid.get(bid);
 	}
 	
 	public Double [] getBidArray(){
-		Double [] bidArray = new Double[bidVsTarget.size()];
-		Set<Double> bidSet = bidVsTarget.keySet();
+		Double [] bidArray = new Double[targetVsBid.size()];
+		Set<Double> bidSet = targetVsBid.keySet();
 		int i=0;
 		for(Double bid : bidSet){
 			bidArray[i]=bid;
 			i++;
 		}
 		return bidArray;
+	}
+	
+	public double [] getData(Double [] bids){
+		double [] dataArray = new double[bids.length];
+		for (int i=0; i<bids.length; i++){
+			dataArray[i]=targetVsBid.get(bids[i]).doubleValue();
+		}
+		return dataArray;
 	}
 	
 
