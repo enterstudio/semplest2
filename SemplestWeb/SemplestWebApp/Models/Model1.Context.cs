c90a39c5d7c2c075e231088bb56ee984ce8e04fe
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-17 15:04:19 +0000

    git-svn-id: file:///semplestb/final_repo@516 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestWebApp/Models/Model1.Context.cs
+++ SemplestWeb/SemplestWebApp/Models/Model1.Context.cs
@@ -47,5 +47,27 @@ namespace SemplestWebApp.Models
         public DbSet<PhoneType> PhoneTypes { get; set; }
         public DbSet<StateCode> StateCodes { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<AdvertisingEngineCampaign> AdvertisingEngineCampaigns { get; set; }
+        public DbSet<Configuration> Configurations { get; set; }
+        public DbSet<CustomerNote> CustomerNotes { get; set; }
+        public DbSet<CustomerStyle> CustomerStyles { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeCustomerAssociation> EmployeeCustomerAssociations { get; set; }
+        public DbSet<EmployeeType> EmployeeTypes { get; set; }
+        public DbSet<Error> Errors { get; set; }
+        public DbSet<HelpDefinition> HelpDefinitions { get; set; }
+        public DbSet<ProductGroup> ProductGroups { get; set; }
+        public DbSet<ProductGroupCycleType> ProductGroupCycleTypes { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<PromotionAd> PromotionAds { get; set; }
+        public DbSet<PromotionKeywordAssociation> PromotionKeywordAssociations { get; set; }
+        public DbSet<Right> Rights { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RolesRightsAssociation> RolesRightsAssociations { get; set; }
+        public DbSet<SEMCustomerDetail> SEMCustomerDetails { get; set; }
+        public DbSet<SiteLink> SiteLinks { get; set; }
+        public DbSet<sysdiagram> sysdiagrams { get; set; }
+        public DbSet<TransactionHistory> TransactionHistories { get; set; }
+        public DbSet<UserRolesAssociation> UserRolesAssociations { get; set; }
     }
 }
