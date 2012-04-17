fe056a17de285897004d551c7706c16ec3a94083
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-17 16:43:12 +0000

    git-svn-id: file:///semplestb/final_repo@530 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestWebApp/Services/SemplestDataService.svc.cs
+++ SemplestWeb/SemplestWebApp/Services/SemplestDataService.svc.cs
@@ -24,26 +24,29 @@ namespace SemplestWebApp.Services
 
         public void SaveAd(CampaignSetupModel model)
         {
-            var campaign = new Campaign
+            // Campaign is now called as ProductGroup
+            var campaign = new ProductGroup
             {
-                CampaignName = model.ProductGroupName,
+                ProductGroupName = model.ProductGroupName,
                 IsActive = true,
                 CustomerFK = 1,
                 StartDate = model.StartDate,
                 EndDate = model.EndDate,
-                TargetCycleBudget = model.Budget
+                
+                  //TargetCycleBudget = model.Budget
             };
-            CampaignAd cad = new CampaignAd { AdText = model.AdCopy };
+            // CampaignAds is now called as PromotionAd
+            PromotionAd cad = new PromotionAd { AdText = model.AdCopy };
             AdvertisingEngine ae = new AdvertisingEngine { AdvertisingEngine1 = model.Google.ToString() };
-            var ag = new AdGroup { Campaign = campaign };
-            ag.CampaignAds.Add(cad);
-            ae.AdGroups.Add(ag);
+            var ag = new Promotion { ProductGroup = campaign };
+            ag.PromotionAds.Add(cad);
+            //ae.AdGroups.Add(ag);
             Keyword kewword = new Keyword { Keyword1 = model.Words };
             using (var db = new SemplestEntities())
             {
-                db.Campaigns.Add(campaign);
-                db.AdGroups.Add(ag);
-                db.CampaignAds.Add(cad);
+                db.ProductGroups.Add(campaign);
+                db.Promotions.Add(ag);
+                db.PromotionAds.Add(cad);
                 db.AdvertisingEngines.Add(ae);
                 db.SaveChanges();
             }
