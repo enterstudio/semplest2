deab1fd1d02254fa703fb29b605e4757ac65cdf5
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-18 21:46:38 +0000

    git-svn-id: file:///semplestb/final_repo@588 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAdminApp/SemplestAdminApp/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SemplestAdminApp.Models;
+
+namespace SemplestAdminApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(SemplestAdminApp.Models.ProfileModel pm)
+        {
+            using(SemplestEntities dbContext = new SemplestEntities())
+            {
+                var creds = dbContext.Credentials.Where(c => c.Username == pm.UserName && c.Password == pm.Password);
+                if (creds.Count() == 1)
+                {
+                    Session["userId"] = creds.First().UsersFK;
+                    return RedirectToAction("Index", "Home");
+                }
+                else 
+                {
+                    return View(pm);
+                }
+
+            };
+            //return RedirectToRoute("Default");
+            
+        }
+
+    }
+}
