d14cc7947d94b19ece5c72c7399cf13a079e8ce0
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-19 18:56:25 +0000

    git-svn-id: file:///semplestb/final_repo@629 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestServices/src/semplest/test/TestSpringJDBC.java
@@ -0,0 +1,71 @@
+package semplest.test;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import semplest.server.service.springjdbc.TaskRunnerDB;
+import semplest.server.service.springjdbc.TaskRunnerObj;
+
+public class TestSpringJDBC
+{
+	public static final String PROPSFILE = "bin/system.properties";
+	public static final String LOG4JPROPSFILE = "properties/log4j_server.properties";
+
+	public static ClassPathXmlApplicationContext appContext = null;
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			/*Properties properties = new Properties();
+			FileInputStream is = new FileInputStream(PROPSFILE);
+			properties.load(is);
+			is.close();
+			*/
+			appContext = new ClassPathXmlApplicationContext("Service.xml");
+			/*
+			CustomerDB cust = new CustomerDB();
+			List<CustomerObj> customers = cust.getAllCustomers();
+			if (!customers.isEmpty())
+			{
+				System.out.println(customers.get(0).getCustomerPK());
+			}
+			else
+			{
+				System.out.println("No customers found");
+			}
+			*/
+			TaskRunnerDB tasks = new TaskRunnerDB();
+			List<TaskRunnerObj> l = tasks.getScheduleTasks(2);
+			if (!l.isEmpty())
+			{
+				for (int i = 0; i < l.size(); i++)
+				{
+					TaskRunnerObj j = l.get(i);
+					System.out.println(j.getServiceName() + ":" + j.getMethodName() + ":" + j.getParameters());
+				}
+			}
+			else
+			{
+				System.out.println("No tasks found");
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		/*
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+	}
+}
