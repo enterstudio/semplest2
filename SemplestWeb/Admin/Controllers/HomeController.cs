bbe80fb754a9d6d406f86ace896eb12d489e0654
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-19 01:48:07 +0000

    git-svn-id: file:///semplestb/final_repo@596 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/Admin/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Semplest.Admin.Models;
+using SemplestAdminApp.Models;
+
+namespace Semplest.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string usersearch, string accountnumbersearch, string emailsearch)
+        {
+
+            ViewBag.Message = "Welcome to SEMPLEST ADMIN!";
+            SemplestEntities dbcontext = new SemplestEntities();
+
+            //FUTURE: add rearch by email and by account number || u.Email.Contains(emailsearch)
+
+            var viewModel =
+                from u in dbcontext.Users
+                join c in dbcontext.Customers on u.CustomerFK equals c.CustomerPK
+                where ((c.Name.Contains(usersearch) || u.FirstName.Contains(usersearch) || u.LastName.Contains(usersearch)) )
+                select new HomeModel
+                {
+                    Customer = c.Name,
+                    AccountNumber = c.CustomerPK,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Email = u.Email
+                };
+
+            return View(viewModel);
+        }
+
+        //public ActionResult GetUsers(string query)
+        //{
+        //    List<string> users = new List<string>();
+        //    using (SemplestEntities dbContext = new SemplestEntities())
+        //    {
+        //        System.Data.Entity.DbSet dusers = dbContext.Users;
+        //        foreach (User du in dusers)
+        //        {
+        //            users.Add(du.FirstName + " "  + du.LastName);
+        //        }
+        //    }
+        //    var quer = from u in users
+        //               where u.ToLower().Contains(query.ToLower())
+        //               select u;
+
+        //    return Json(quer);
+        //} 
+
+
+    }
+}
---
+++ SemplestWeb/Admin/Scripts/PanelBar.js
@@ -0,0 +1,13 @@
+﻿$("#panelbar-admin").kendoPanelBar({
+    dataSource: [
+                {
+                    text: "Home",
+                    items: [
+                        { text: "Employee Setup" },
+                        { text: "Client Account Setup" },
+                        { text: "Billing/Finance" },
+                        { text: "Configure" }
+                    ]
+                }
+            ], expandMode: "single"
+});
