a8bf1c6021e5f75ebed14044e5b86c66fd177704
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-22 04:20:03 +0000

    Added Categories and Keywords support
    git-svn-id: file:///semplestb/final_repo@710 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/Core/Controllers/CampaignController.cs
+++ SemplestWeb/SemplestAll/Core/Controllers/CampaignController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Reflection;
 using KendoGridBinder;
 using Microsoft.Practices.EnterpriseLibrary.Logging;
 using Semplest.Core.Models;
@@ -40,6 +41,146 @@ namespace Semplest.Core.Controllers
             }
             return View(model);
         }
+
+        [HttpPost]
+        [ActionName("CampaignSetup")]
+        [AcceptSubmitType(Name = "Command", Type = "GetCategories")]
+        public ActionResult GetCategories(CampaignSetupModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var scw = new ServiceClientWrapper();
+
+                    // create AdCopy array
+                    var promAds = model.Ads;
+                    List<string> liAddcopies = new List<string>();
+                    foreach (PromotionAd pAd in promAds)
+                    {
+                        liAddcopies.Add(pAd.AdText);
+                    }
+
+                    // get categories or classifications
+                    List<string> categories = scw.GetCategories(null, model.ProductGroup.ProductPromotionName, 
+                                                model.ProductGroup.Words, liAddcopies.ToArray(), model.Url);
+
+                    // create categories list that will be displayed in a multiselect list box
+                    if (categories != null && categories.Count > 0)
+                    {
+                        for (int i = 0; i < categories.Count; i++)
+                        {
+                            var cm = new CampaignSetupModel.CategoriesModel { Id = i, Name = categories[i] };
+                            model.AllCategories.Add(cm);
+                        }
+                    }
+                    else
+                    {
+                        var logEnty = new LogEntry { ActivityId = Guid.NewGuid(), Message = "Could not get Categories from web service" };
+                        Logger.Write(logEnty);
+                    }
+
+                    // save this some how while getting the keywords this is becoming null
+                    Session.Add("AllCategories", model.AllCategories);
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message + "\\r\\n" + ex.StackTrace;
+                //CreateDummyModel(model);
+                //ViewBag.AllCategories = model.AllCategories;
+                //Session.Add("AllCategories", model.AllCategories);
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("CampaignSetup")]
+        [AcceptSubmitType(Name = "Command", Type = "GetKeywords")]
+        public ActionResult GetKeywords(CampaignSetupModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (model.AllCategories.Count == 0)
+                    {
+                        model.AllCategories = (List<CampaignSetupModel.CategoriesModel>)Session["AllCategories"];
+                    }
+
+                    if (model.AllCategories.Count <= 0)
+                    {
+                    }
+
+                    var catList = new List<string>();
+
+                    foreach (CampaignSetupModel.CategoriesModel cat in model.AllCategories)
+                    {
+                        for (int i = 0; i < model.CategoryIds.Length; i++)
+                        {
+                            if (cat.Id == model.CategoryIds[i])
+                            {
+                                catList.Add(cat.Name);
+                            }
+                        }
+                    }
+
+                    var scw = new ServiceClientWrapper();
+                    // create AdCopy array
+                    var promAds = model.Ads;
+                    List<string> liAddcopies = new List<string>();
+                    foreach (PromotionAd pAd in promAds)
+                    {
+                        liAddcopies.Add(pAd.AdText);
+                    }
+
+                    // get keywords from the web service
+                    //List<string> keywords = scw.GetKeywords(catList, null, "coffee machine", null, null, "http://www.wholelattelove.com", null);
+                    List<string> keywords = scw.GetKeywords(catList, null, model.ProductGroup.ProductPromotionName,
+                                                    model.ProductGroup.Words, liAddcopies.ToArray(), model.Url, null);
+                    if (keywords != null && keywords.Count > 0)
+                    {
+                        int i = 0;
+                        foreach (string key in keywords)
+                        {
+                            var kwm = new CampaignSetupModel.KeywordsModel();
+                            kwm.Name = key;
+                            model.AllKeywords.Add(kwm);
+                        }
+                    }
+                    else
+                    {
+                        var logEnty = new LogEntry { ActivityId = Guid.NewGuid(), Message = "Could not get Keywords from web service" };
+                        Logger.Write(logEnty);
+                    }
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message + "\\r\\n" + ex.StackTrace;
+                return View(model);
+            }
+        }
+
+        #region Nested type: AcceptSubmitTypeAttribute
+
+        public class AcceptSubmitTypeAttribute : ActionMethodSelectorAttribute
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+
+            public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+            {
+                return controllerContext.RequestContext.HttpContext
+                           .Request.Form[Name] == Type;
+            }
+        }
+
+        #endregion
+
 
         [HttpPost]
         public JsonResult AdditionalLinks(KendoGridRequest request)
