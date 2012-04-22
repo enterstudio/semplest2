f95f8e7dbf5eeb6d3c5d19c5366ad4b5ddc90bc9
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-22 00:23:27 +0000

    git-svn-id: file:///semplestb/final_repo@703 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestSchedulerInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestSchedulerInterface.java
@@ -2,5 +2,5 @@ package semplest.services.client.interfaces;
 
 public interface SemplestSchedulerInterface extends ServiceInitialize
 {
-	public abstract Boolean NewSchedule(Integer ScheduleID,java.util.Date StartTime, Boolean IsDelete) throws Exception;
+	public abstract Boolean NewSchedule(Integer scheduleJobID,Integer ScheduleID,java.util.Date StartTime, Boolean IsDelete) throws Exception;
 }
