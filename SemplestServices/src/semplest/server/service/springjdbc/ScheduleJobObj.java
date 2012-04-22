4becaede2ec4013a8a577a1278b0394e00d10d0c
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-22 00:48:26 +0000

    schedule Job
    git-svn-id: file:///semplestb/final_repo@704 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestServices/src/semplest/server/service/springjdbc/ScheduleJobObj.java
@@ -0,0 +1,35 @@
+package semplest.server.service.springjdbc;
+
+import java.util.Date;
+
+public class ScheduleJobObj
+{
+	private Integer ScheduleJobPK;
+	private Integer ScheduleFK;
+	private Date ExecutionStartTime;
+	public Integer getScheduleJobPK()
+	{
+		return ScheduleJobPK;
+	}
+	public void setScheduleJobPK(Integer scheduleJobPK)
+	{
+		ScheduleJobPK = scheduleJobPK;
+	}
+	public Integer getScheduleFK()
+	{
+		return ScheduleFK;
+	}
+	public void setScheduleFK(Integer scheduleFK)
+	{
+		ScheduleFK = scheduleFK;
+	}
+	public Date getExecutionStartTime()
+	{
+		return ExecutionStartTime;
+	}
+	public void setExecutionStartTime(Date executionStartTime)
+	{
+		ExecutionStartTime = executionStartTime;
+	}
+
+}
--- SemplestServices/src/semplest/server/service/springjdbc/SetScheduleJobCompleteSP.java
+++ SemplestServices/src/semplest/server/service/springjdbc/SetScheduleJobCompleteSP.java
@@ -4,16 +4,21 @@ import java.sql.Types;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.SqlReturnResultSet;
 import org.springframework.jdbc.object.StoredProcedure;
 
 public class SetScheduleJobCompleteSP extends StoredProcedure 
 {
 	private static final String SPROC_NAME = "SetScheduleJobComplete";
+	private static final RowMapper<ScheduleJobObj> ScheduleJobObjMapper = new BeanPropertyRowMapper(ScheduleJobObj.class); 
 	
 	public SetScheduleJobCompleteSP()
 	{
 		super(BaseDB.jdbcTemplate.getDataSource(), SPROC_NAME );
+		declareParameter(new SqlReturnResultSet("nextScheduledJob", ScheduleJobObjMapper));   
 		declareParameter(new SqlParameter("ScheduleJobID", Types.INTEGER));
 		declareParameter(new SqlParameter("IsSucceedful", Types.BIT));
 		//declareParameter(new SqlOutParameter("titles", , new TitleMapper()));
@@ -21,11 +26,22 @@ public class SetScheduleJobCompleteSP extends StoredProcedure
 
 
 	}
-	public Map execute(Integer ScheduleID, boolean IsSuccessful) {
-        Map inputs = new HashMap();
+	/*
+	 * returns the next schedule job to run
+	 */
+	public ScheduleJobObj execute(Integer ScheduleID, boolean IsSuccessful) {
+        Map<String, Object> inputs = new HashMap<String, Object>();
         inputs.put("ScheduleJobID", ScheduleID);
         inputs.put("IsSuccessful", IsSuccessful);
-        return super.execute(inputs);
+        Map results  = super.execute(inputs);
+        if (results.get("nextScheduledJob") == null)   
+        {   
+             return null;   
+        }   
+        else  
+        {   
+            return (ScheduleJobObj) results.get("nextScheduledJob");   
+        } 
     }
 
 
