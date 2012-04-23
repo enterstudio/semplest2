f0135f7a3e44d58999aba100e0d70839caa79d5c
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-23 21:53:18 +0000

    git-svn-id: file:///semplestb/final_repo@779 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/SharedResources/Controllers/ProfileController.cs
+++ SemplestWeb/SemplestAll/SharedResources/Controllers/ProfileController.cs
@@ -38,6 +38,29 @@ namespace Semplest.SharedResources.Controllers
 
         }
 
+        public ActionResult LogIn()
+        { return View(); }
+
+        [HttpPost]
+        public ActionResult LogIn(Semplest.SharedResources.Models.ProfileModel pm)
+        {
+            using (SemplestEntities dbContext = new SemplestEntities())
+            {
+                var creds = dbContext.Credentials.Where(c => c.Username == pm.UserName && c.Password == pm.Password);
+                if (creds.Count() == 1)
+                {
+                    Session["userId"] = creds.First().UsersFK;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return View(pm);
+                }
+
+            };
+            //return RedirectToRoute("Default");
+
+        }
 
     }
 }
---
+++ SemplestWeb/SemplestAll/SharedResources/Helpers/RoleHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Mvc;
+namespace Semplest.SharedResources.Helpers
+{
+    public class AuthorizeRoleAttribute : AuthorizeAttribute
+    {
+        //public string UserName { get; set; }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            string userId = string.Empty;
+            if (HttpContext.Current.Session["UserId"] != null)
+                userId = HttpContext.Current.Session["UserId"].ToString();
+            return !string.IsNullOrEmpty(userId);
+        }
+
+        //protected override bool AuthorizeCore(HttpContextBase httpContext)
+        //{
+        //    //var userName = HttpContext.Current.Session["User"];
+        //    //if (userName != null)
+        //    //    UserName = userName.ToString();
+        //    //if (UserName.Equals("Sriram"))
+        //    return true;
+        //    //return base.AuthorizeCore(httpContext);
+        //}
+    }
+}
