292cd398dcb850cc79a9dc4553711ecc7e15e866
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-23 05:40:55 +0000

    git-svn-id: file:///semplestb/final_repo@747 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAll/SharedResources/Controllers/ProfileController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SemplestModel;
+
+namespace Semplest.SharedResources.Controllers
+{
+    public class ProfileController : Controller
+    {
+        //
+        // GET: /Profile/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Semplest.SharedResources.Models.ProfileModel pm)
+        {
+            using (SemplestEntities dbContext = new SemplestEntities())
+            {
+                var creds = dbContext.Credentials.Where(c => c.Username == pm.UserName && c.Password == pm.Password);
+                if (creds.Count() == 1)
+                {
+                    Session["userId"] = creds.First().UsersFK;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return View(pm);
+                }
+
+            };
+            //return RedirectToRoute("Default");
+
+        }
+
+
+    }
+}
