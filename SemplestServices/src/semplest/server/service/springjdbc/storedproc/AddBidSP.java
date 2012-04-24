1fffe1849bdcb1d37079242f506d028ecec4d0b0
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-24 16:14:29 +0000

    git-svn-id: file:///semplestb/final_repo@814 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestServices/src/semplest/server/service/springjdbc/storedproc/AddBidSP.java
@@ -0,0 +1,74 @@
+package semplest.server.service.springjdbc.storedproc;
+
+import java.sql.Types;
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.object.StoredProcedure;
+
+import semplest.server.service.springjdbc.BaseDB;
+
+public class AddBidSP extends StoredProcedure
+{
+	private static final String SPROC_NAME = "SetBidObject";
+	/*
+	 * @ProductGroupPK			INT,
+	@PromotionPK            INT,
+	@KeywordAdEngineID      INT,
+	@Keyword				NVARCHAR(250),
+	@MicroBidAmount			INT,
+	@ApprovalStatus			VARCHAR(30) = null,
+	@BidType				VARCHAR(25),
+	@FirstPageMicroCpc      INT = null,
+	@QualityScore           INT = null,
+	@IsEligibleForShowing	BIT = 1,
+	@IsBidActive			BIT = 1,
+	@IsNegative				BIT = 0,
+	@AdvertisingEngine		VARCHAR(50),
+	 */
+	public AddBidSP()
+	{
+		super(BaseDB.jdbcTemplate.getDataSource(), SPROC_NAME);
+		declareParameter(new SqlParameter("ProductGroupPK", Types.INTEGER));
+		declareParameter(new SqlParameter("PromotionPK", Types.INTEGER));
+		declareParameter(new SqlParameter("Keyword", Types.NVARCHAR));
+		declareParameter(new SqlParameter("MicroBidAmount", Types.INTEGER));
+		declareParameter(new SqlParameter("ApprovalStatus", Types.VARCHAR));
+		declareParameter(new SqlParameter("BidType", Types.VARCHAR));
+		declareParameter(new SqlParameter("FirstPageMicroCpc", Types.INTEGER));
+		declareParameter(new SqlParameter("QualityScore", Types.INTEGER));
+		declareParameter(new SqlParameter("IsEligibleForShowing", Types.BIT));
+		declareParameter(new SqlParameter("IsBidActive", Types.BIT));
+		declareParameter(new SqlParameter("IsNegative", Types.BIT));
+		declareParameter(new SqlParameter("AdvertisingEngine", Types.VARCHAR));
+		
+		declareParameter(new SqlOutParameter("ID", Types.INTEGER));
+		compile();
+	}
+
+	/*
+	 * returns the next schedule job to run
+	 */
+	public Integer execute(int ScheduleFK, Date ExecutionStartTime)
+	{
+		Map<String, Object> results = super.execute(ScheduleFK, ExecutionStartTime);
+		if (results.get("ID") == null)
+		{
+			return null;
+		}
+		else
+		{
+			Integer res = (Integer) results.get("ID");
+			if (res == null)
+			{
+				return null;
+			}
+			else
+			{
+				return res;
+			}
+		}
+	}
+}
