73e49d8d0ffdfac4b088e94dbab6f594b5221efe
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-24 11:37:16 +0000

    git-svn-id: file:///semplestb/final_repo@787 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/server/service/springjdbc/storedproc/AddScheduleJobSP.java
+++ SemplestServices/src/semplest/server/service/springjdbc/storedproc/AddScheduleJobSP.java
@@ -1,4 +1,4 @@
-package semplest.server.service.springjdbc;
+package semplest.server.service.springjdbc.storedproc;
 
 import java.sql.Types;
 import java.util.Date;
@@ -7,6 +7,8 @@ import java.util.Map;
 import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.object.StoredProcedure;
+
+import semplest.server.service.springjdbc.BaseDB;
 
 public class AddScheduleJobSP extends StoredProcedure
 {
