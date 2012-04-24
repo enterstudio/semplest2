6ab070e5525627dfa377d7f37125b01d452ad83f
lluis <lluis@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-24 13:06:26 +0000

    git-svn-id: file:///semplestb/final_repo@792 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestKeywords/src/semplest/keywords/lda/test.java
+++ SemplestKeywords/src/semplest/keywords/lda/test.java
@@ -17,24 +17,9 @@ public class test {
 	 * @throws ParserException 
 	 */
 	public static void main(String[] args) throws ClassNotFoundException, IOException, ParserException {
-		String a = null;
-		String b = null;
-		String c = null;
-		String d = "porsche gt3";
-		System.out.println(a+b+c);
-		System.out.println(stemvString(d));
+		TextUtils.changeKeywordMatchtoExact("/semplest/data/biddingTest/PiperHall/keywords.txt");
+		System.out.println("done");
 	}
-	private static String stemvString( String raws){
-		//Returns the stemmed version of a word
-	    String os = "";
-	    for( String w: raws.split("\\s+")){
-	    	String aux = dictUtils.getStemWord( w );
-	    	if(aux != null)
-	    		os = os + aux + " ";
-	    	else 
-	    		os = os + w + " "; 
-	    }
-	    return os;
-	  }
+
 
 }
