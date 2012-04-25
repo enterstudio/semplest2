5490a6ad6b38d373bca05cc4b8e2fe0b5fbcf30e
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-25 17:27:33 +0000

    Enum definitions
    git-svn-id: file:///semplestb/final_repo@866 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestNIOProtocol/src/semplest/server/protocol/ProtocolEnum.java
@@ -0,0 +1,72 @@
+package semplest.server.protocol;
+
+
+public class ProtocolEnum {
+	
+	
+	/*
+	 * ENUMS
+	 */
+	public static enum ScheduleFrequency
+	{
+		Now, Daily, Weekly, Monthly;
+		public static boolean existsFrequency(String freq)
+		{
+			if (freq != null)
+			{
+				for (ScheduleFrequency val : ScheduleFrequency.values())
+				{
+					if (val.name().equalsIgnoreCase(freq))
+					{
+						return true;
+
+					}
+				}
+
+			}
+			return false;
+		}
+	}
+	public static enum AdEngine
+	{
+		MSN,Google;
+		public static boolean existsFrequency(String adEngine)
+		{
+			if (adEngine != null)
+			{
+				for (AdEngine val : AdEngine.values())
+				{
+					if (val.name().equalsIgnoreCase(adEngine))
+					{
+						return true;
+
+					}
+				}
+
+			}
+			return false;
+		}
+	}
+	
+	public static enum MatchType
+	{
+		Exact,Broad,Phrase;
+		public static boolean existsFrequency(String matchType)
+		{
+			if (matchType != null)
+			{
+				for (MatchType val : MatchType.values())
+				{
+					if (val.name().equalsIgnoreCase(matchType))
+					{
+						return true;
+
+					}
+				}
+
+			}
+			return false;
+		}
+	}
+
+}
