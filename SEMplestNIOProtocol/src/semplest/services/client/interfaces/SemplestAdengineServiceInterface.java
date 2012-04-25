b8d8c4d335e59f345fcf0c9771e9688a35ac677e
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-25 12:53:54 +0000

    git-svn-id: file:///semplestb/final_repo@850 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestAdengineServiceInterface.java
@@ -0,0 +1,14 @@
+package semplest.services.client.interfaces;
+
+import java.util.ArrayList;
+
+public interface SemplestAdengineServiceInterface extends ServiceInitialize
+{
+	public abstract Boolean AddPromotionToAdEngine(Integer customerID, Integer productGroupID, Integer PromotionID, ArrayList<String> adEngineList ) throws Exception;
+	public abstract Boolean PausePromotion(Integer customerID,Integer promotionID, String adEngine) throws Exception;
+	public abstract Boolean PauseProductGroup(Integer customerID,Integer productGroupID, String adEngine) throws Exception;
+	public abstract Boolean DeleteAd(Integer customerID,Integer promotionID, Integer promotionAdID) throws Exception;
+	public abstract Boolean DeleteSiteLinkForAd(Integer customerID,Integer promotionID, Integer promotionAdID, Integer SiteLinkID) throws Exception;
+	public abstract Boolean UpdateGeoTargeting(Integer customerID,Integer productGroupID, Integer GeoTargetingID ) throws Exception;
+
+}
