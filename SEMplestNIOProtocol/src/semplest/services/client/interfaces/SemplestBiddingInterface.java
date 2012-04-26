8a636799ee10ea60f7e87594a4a07023dd9ab6fd
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-26 21:08:08 +0000

    git-svn-id: file:///semplestb/final_repo@961 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestBiddingInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestBiddingInterface.java
@@ -13,8 +13,9 @@ public interface SemplestBiddingInterface extends ServiceInitialize {
 
 	void getBidsUpdate(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
 
-	HashMap<String,Double> GetMonthlyBudgetPerSE(String accountID, Long campaignID,
-			Long adGroupID, ArrayList<String> searchEngine, Double TotalMonthlyBudget) throws Exception;
+	HashMap<String,Double> GetMonthlyBudgetPerSE(ArrayList<String> searchEngine, Double TotalMonthlyBudget) throws Exception;
+
+	
 
 
 
