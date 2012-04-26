d5828e28f8b8c47ef17c75af5f95688ffce0bedc
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-26 16:57:02 +0000

    Moving in exception handling
    git-svn-id: file:///semplestb/final_repo@933 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAll/SharedResources/Helpers/ExceptionHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SemplestModel;
+
+namespace SharedResources.Helpers
+{
+    public class ExceptionHelper : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+
+            //if (filterContext.ExceptionHandled)
+            //{
+            try
+            {
+                SemplestEntities _dbContext = new SemplestEntities();
+                SemplestModel.Error er = new SemplestModel.Error();
+                er.ErrorMessage = filterContext.Exception.ToString();
+                //filterContext.RequestContext.HttpContext.Session
+                if (HttpContext.Current.Session[Semplest.SharedResources.SEMplestConstants.SESSION_USERID] == null)
+                    er.UsersFK = 1;
+                else
+                    er.UsersFK = string.IsNullOrEmpty(HttpContext.Current.Session[Semplest.SharedResources.SEMplestConstants.SESSION_USERID].ToString()) ? 1 : int.Parse(HttpContext.Current.Session[Semplest.SharedResources.SEMplestConstants.SESSION_USERID].ToString());
+                er.TimeStamp = DateTime.Now;
+                _dbContext.Errors.Add(er);
+                _dbContext.SaveChanges();
+                //send email
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            // Log filterContext.Exception in some way.  
+            //}
+        }
+    }  
+}
