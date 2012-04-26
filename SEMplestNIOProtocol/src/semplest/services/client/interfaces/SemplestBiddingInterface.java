207518495845d09ff041497de3adb2a9e431259a
raj <raj@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-26 23:12:39 +0000

    Added Naive
    git-svn-id: file:///semplestb/final_repo@974 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestBiddingInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestBiddingInterface.java
@@ -10,13 +10,13 @@ public interface SemplestBiddingInterface extends ServiceInitialize {
 	HashMap<String,Double> getBid(String accountID, Long campaignID, Long adGroupID, ArrayList<String> keywords) throws Exception ;
 
 	void getBidsInitial(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
+	void getBidsInitialNaive(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
 
 	void getBidsUpdate(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
-
+	void getBidsUpdateNaive(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
+	
 	HashMap<String,Double> GetMonthlyBudgetPerSE(ArrayList<String> searchEngine, Double TotalMonthlyBudget) throws Exception;
 
 	
-
-
 
 }
