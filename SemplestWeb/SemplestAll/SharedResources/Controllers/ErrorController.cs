c8b30c2c0addc37d24072717026ca52eb01c7e18
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-04-26 16:46:19 +0000

    git-svn-id: file:///semplestb/final_repo@932 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAll/SharedResources/Controllers/ErrorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using SemplestModel;
+
+namespace SharedResources.Controllers
+{
+    public class ErrorModel
+    {
+        public DateTime TimeStamp { get; set; }
+        public string ErrorMessage { get; set; }
+        public string User { get; set; }
+
+    }
+
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Index()
+        {
+            SemplestEntities dbContext = new SemplestEntities();
+//            SortedList<DateTime, ErrorModel> list = new SortedList<DateTime, ErrorModel>();
+            List<ErrorModel> list = new List<ErrorModel>();
+            ErrorModel em = null;
+            foreach (Error er in dbContext.Errors.OrderByDescending(x => x.TimeStamp))
+            {
+                em = new ErrorModel();
+                em.ErrorMessage = er.ErrorMessage;
+                em.User = er.User.Credentials.First().Username;
+                em.TimeStamp = er.TimeStamp;
+
+                list.Add(em);
+            }
+            return View(list);
+        }
+
+    }
+}
