719b3570681bd9c6c3f95e23ceeeca1066909881
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-02 14:13:22 +0000

    git-svn-id: file:///semplestb/final_repo@1113 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMPlestDBScripts/StoredProcs/AddKeywordBidData.sql
@@ -0,0 +1,79 @@
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.AddKeywordBidData') AND type in (N'P', N'PC'))
+  DROP PROCEDURE dbo.AddKeywordBidData;
+GO
+
+-- +----------------------------------------------------------------------------------------------------------------+
+-- | Author  - Mitch                                                                                                |
+-- | Written - 4/20/2012																							|
+-- | Parms   - 																							|
+-- | Purpose - 						|
+-- +----------------------------------------------------------------------------------------------------------------+
+
+CREATE PROCEDURE dbo.AddKeywordBidData
+(
+
+	@PromotionID        BIGINT,
+	@Keyword				NVARCHAR(250),
+	@AdvertisingEngine		VARCHAR(50),
+	@BidType				VARCHAR(25),
+	@QualityScore           INT = null,
+	@ApprovalStatus			VARCHAR(30) = null,
+	@FirstPageMicroCpc      INT = null,
+	@IsEligibleForShowing	BIT = 1
+)
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+	DECLARE @ErrMsg VARCHAR(250), @keywordBidPK int, @currentTime datetime2, @BidTypeID int, 
+			@AdEngineID int,@keywordPK int
+	-- make sure keyword is in promotion
+	if not exists (select * from PromotionKeywordAssociation pka inner join Keyword k on k.KeywordPK = pka.KeywordFK
+					where k.Keyword = @Keyword and pka.PromotionFK = @PromotionID)
+	BEGIN
+		SELECT @ErrMsg = 'The Selected keyword does not exist for the Promotion'; 
+		RAISERROR (@ErrMsg, 16, 1);
+	END;	
+	set @currentTime = CURRENT_TIMESTAMP	
+	BEGIN TRANSACTION		
+	--check to see if the KeywordBid Exists for the MatchType
+	if NOT EXISTS (select * from KeywordBid kb 
+					inner join Keyword k on k.KeywordPK = kb.KeywordFK
+					inner join AdvertisingEngine ae on ae.AdvertisingEnginePK = kb.AdvertisingEngineFK
+					inner join BidType bt on bt.BidTypePK = kb.BidTypeFK 
+					where kb.PromotionFK = @PromotionID and k.Keyword = @Keyword and ae.AdvertisingEngine = @AdvertisingEngine and bt.BidType = @BidType)
+	BEGIN
+	--add Keyword /matchtype with -1 microbid
+		
+		select @BidTypeID = bt.BidTypePK from BidType bt where bt.BidType = @BidType 
+		select @AdEngineID = ae.AdvertisingEnginePK from AdvertisingEngine ae where ae.AdvertisingEngine = @AdvertisingEngine  
+		--
+		insert into KeywordBid(KeywordFK,AdvertisingEngineFK,PromotionFK,StartDate,EndDate,IsActive, BidTypeFK, MicroBidAmount, IsDefaultValue, CreatedDate)
+			select k.KeywordPK,@AdEngineID,@PromotionID,@currentTime,@currentTime,0,@BidTypeID,-1,0,@currentTime from Keyword k where k.Keyword = @Keyword
+		SET @keywordBidPK = @@IDENTITY
+		insert into KeywordBidData(KeywordBidFK,QualityScore,ApprovalStatus,FirstPageMicroCPC,IsEligibleForShowing,CreatedDate)
+			VALUES (@keywordBidPK, @QualityScore, @ApprovalStatus, @FirstPageMicroCpc, @IsEligibleForShowing, @currentTime)
+	END	
+	ELSE --keywordBid already exists for match type				 
+	BEGIN
+		select @keywordBidPK = kb.KeywordBidPK from KeywordBid kb 
+					inner join Keyword k on k.KeywordPK = kb.KeywordFK
+					inner join AdvertisingEngine ae on ae.AdvertisingEnginePK = kb.AdvertisingEngineFK
+					inner join BidType bt on bt.BidTypePK = kb.BidTypeFK 
+					where kb.PromotionFK = @PromotionID and k.Keyword = @Keyword and ae.AdvertisingEngine = @AdvertisingEngine and bt.BidType = @BidType
+		insert into KeywordBidData(KeywordBidFK,QualityScore,ApprovalStatus,FirstPageMicroCPC,IsEligibleForShowing,CreatedDate)
+			VALUES (@keywordBidPK, @QualityScore, @ApprovalStatus, @FirstPageMicroCpc, @IsEligibleForShowing, @currentTime)
+	END
+	
+	COMMIT TRANSACTION	
+	
+END TRY
+BEGIN CATCH
+ IF XACT_STATE() != 0 OR @@TRANCOUNT > 0
+    ROLLBACK TRANSACTION;
+	DECLARE @ErrMessage	nvarchar(4000),
+          @ErrorSeverity	int,
+          @ErrorState		int;
+	SELECT @ErrMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	RAISERROR (@ErrMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
+GO
--- SEMPlestDBScripts/StoredProcs/SetBidObject.sql
+++ SEMPlestDBScripts/StoredProcs/SetBidObject.sql
@@ -11,19 +11,11 @@ GO
 
 CREATE PROCEDURE dbo.SetBidObject
 (
-
-	@ProductGroupPK			INT,
 	@PromotionPK            INT,
 	@KeywordAdEngineID      BIGINT,
 	@Keyword				NVARCHAR(250),
 	@MicroBidAmount			INT,
-	@ApprovalStatus			VARCHAR(30) = null,
 	@BidType				VARCHAR(25),
-	@FirstPageMicroCpc      INT = null,
-	@QualityScore           INT = null,
-	@IsEligibleForShowing	BIT = 1,
-	@IsBidActive			BIT = 1,
-	@IsNegative				BIT = 0,
 	@AdvertisingEngine		VARCHAR(50),
 	@ID int output
 )
@@ -46,6 +38,14 @@ BEGIN TRY
 		SELECT @ErrMsg = 'The Bid Type was not found.'; 
 		RAISERROR (@ErrMsg, 16, 1);
 	END;
+	--check to see if keyword is active and not deleted for the promo
+	if EXISTS (select * from PromotionKeywordAssociation pka
+				inner join Keyword k on k.KeywordPK = pka.KeywordFK
+				where k.Keyword = @Keyword and (pka.IsActive = 0 OR pka.IsDeleted = 1))
+	BEGIN
+		SELECT @ErrMsg = 'The Selected keyword is either Inactive or Deleted for the Promotion'; 
+		RAISERROR (@ErrMsg, 16, 1);
+	END;			
 	select @AdEngineID = a.AdvertisingEnginePK from AdvertisingEngine a where a.AdvertisingEngine = @AdvertisingEngine
 	select @BidTypeID = bt.BidTypePK from BidType bt where bt.BidType = @BidType
 	SET @currentTime = CURRENT_TIMESTAMP
@@ -56,7 +56,7 @@ BEGIN TRY
 				inner join Keyword k on k.KeywordPK = pka.KeywordFK
 				inner join KeywordBid kb on kb.KeywordFK = k.KeywordPK
 				inner join AdvertisingEngine a on a.AdvertisingEnginePK = kb.AdvertisingEngineFK
-				where pka.PromotionFK = @PromotionPK and a.AdvertisingEnginePK = @AdEngineID --and pka.IsActive = 1 
+				where pka.PromotionFK = @PromotionPK and a.AdvertisingEnginePK = @AdEngineID 
 					 and kb.KeywordBidPK = @BidTypeID
 					 and (k.Keyword = @Keyword or kb.KeywordAdEngineID = @KeywordAdEngineID))
 	BEGIN
@@ -69,23 +69,9 @@ BEGIN TRY
 					where pka.PromotionFK = @PromotionPK and pka.IsActive = 1 and a.AdvertisingEnginePK = @AdEngineID
 						 and kb.KeywordBidPK = @BidTypeID
 						 and (k.Keyword = @Keyword or kb.KeywordAdEngineID = @KeywordAdEngineID)
-		--Check to see if setting inactive				 
-		if (@IsBidActive = 0)
-		BEGIN
-			update KeywordBid set EndDate = @currentTime, IsActive = 0
-			from PromotionKeywordAssociation pka
-				inner join Keyword k on k.KeywordPK = pka.KeywordFK
-				inner join KeywordBid kb on kb.KeywordFK = k.KeywordPK
-				inner join AdvertisingEngine a on a.AdvertisingEnginePK = kb.AdvertisingEngineFK
-				where pka.PromotionFK = @PromotionPK and a.AdvertisingEnginePK = @AdEngineID --and pka.IsActive = 1 
-					 and kb.KeywordBidPK = @BidTypeID
-					 and (k.Keyword = @Keyword or kb.KeywordAdEngineID = @KeywordAdEngineID)		 		 
-		END
-		ELSE  --update the Bid if MicroBidAmount if different
-		BEGIN
+		
 			if (@MicroBidAmount != @currentMicroBidAmt)
 			BEGIN
-				
 				 --update the last bid with an end Date and set inactive
 				 UPDATE KeywordBid set EndDate = @currentTime, IsActive = 0 WHERE KeywordBidPK = @keywordBidPK
 				 --add new active keyword bid
@@ -95,33 +81,16 @@ BEGIN TRY
 				SET @keywordBidPK = @@IDENTITY	 
 				--make sure the associaition is active
 				update PromotionKeywordAssociation set IsActive = 1 where PromotionFK = @PromotionPK and KeywordFK = @keywordPK	
-			END	
-		END			 
+			END		 
 	END	
 	
-	ELSE --Association DOES NOT EXIST ADD
+	ELSE --New Bid on Keyword
 	  BEGIN
-			IF NOT EXISTS (Select * from Keyword k where k.Keyword = @Keyword)
-			BEGIN
-				INSERT into Keyword(Keyword,CreatedDate) values (@Keyword,@currentTime)
-				SET @keywordPK = @@IDENTITY
-			END 
-			ELSE
-			BEGIN
-				Select @keywordPK = k.KeywordPK from Keyword k where k.Keyword = @Keyword
-			END
-			insert into PromotionKeywordAssociation(KeywordFK,PromotionFK,CreatedDate,IsActive,IsDeleted,IsNegative)
-			values (@keywordPK,@PromotionPK,@currentTime,1,0,@IsNegative)
 			--create the keyword bid
 			insert into KeywordBid(KeywordFK,AdvertisingEngineFK,PromotionFK,StartDate,EndDate,IsActive,BidTypeFK,MicroBidAmount,KeywordAdEngineID)
-				VALUES (@keywordPK,@AdEngineID,@PromotionPK,@currentTime,null,@IsBidActive,@BidTypeID,@MicroBidAmount,@KeywordAdEngineID)
+				VALUES (@keywordPK,@AdEngineID,@PromotionPK,@currentTime,null,1,@BidTypeID,@MicroBidAmount,@KeywordAdEngineID)
 			SET @keywordBidPK = @@IDENTITY	
 	  END
-	if (@QualityScore is not null or @ApprovalStatus is not null or @FirstPageMicroCpc is not null or @IsEligibleForShowing is not null)
-	BEGIN
-		INSERT INTO KeywordInitialBidData(KeywordBidFK,QualityScore,ApprovalStatus,FirstPageMicroCPC,IsEligibleForShowing,CreatedDate)
-		values (@keywordBidPK, @QualityScore,@ApprovalStatus,@FirstPageMicroCpc,@IsEligibleForShowing,@currentTime)
-	END
 	   		
 	COMMIT TRANSACTION	
 	
