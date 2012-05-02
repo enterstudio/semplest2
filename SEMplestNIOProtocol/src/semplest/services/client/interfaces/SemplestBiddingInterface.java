88c7afca4ffb54d924c78ccbbb07d6a445b5da58
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-02 16:24:34 +0000

    added setBidsInitial
    git-svn-id: file:///semplestb/final_repo@1117 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestBiddingInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestBiddingInterface.java
@@ -7,6 +7,9 @@ import semplest.server.protocol.adengine.KeywordDataObject;
 
 public interface SemplestBiddingInterface extends ServiceInitialize {
 	
+	void setBidsInitial(Integer promotionID, String searchEngine) throws Exception;
+
+	
 	HashMap<String,Double> getBid(String accountID, Long campaignID, Long adGroupID, ArrayList<String> keywords) throws Exception ;
 
 	void getBidsInitial(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
@@ -15,8 +18,6 @@ public interface SemplestBiddingInterface extends ServiceInitialize {
 	void getBidsUpdate(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
 	void getBidsUpdateNaive(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
 	
-	HashMap<String,Double> GetMonthlyBudgetPerSE(ArrayList<String> searchEngine, Double TotalMonthlyBudget) throws Exception;
-
-	
+	HashMap<String,Double> GetMonthlyBudgetPerSE(ArrayList<String> searchEngine, Double TotalMonthlyBudget) throws Exception;	
 
 }
