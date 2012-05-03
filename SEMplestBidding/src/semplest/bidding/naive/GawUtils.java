5db37d0e0c2db6368b954b4e5b0c94737a8f8334
raj <raj@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-03 20:10:03 +0000

    Init commit.
    git-svn-id: file:///semplestb/final_repo@1186 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestBidding/src/semplest/bidding/naive/GawUtils.java
@@ -0,0 +1,25 @@
+package semplest.bidding.naive;
+
+import com.google.api.adwords.v201109.o.*;
+import com.google.api.adwords.v201109.cm.*;
+import com.google.api.adwords.lib.AdWordsUser;
+import com.google.api.adwords.lib.AdWordsService;
+
+// Gets traffic estimates from Google
+public class GawUtils {
+
+// String[] campaign = {"ParkWinters","1851386728","80759052","3442703292"}; 
+// String[] campaign = {"WeddingFlowers","9886423251","94891718","3200406038"};
+  static String[] campaign = {
+    "TovahPhoto","8982168071","76709721","3582397881"};
+  static String clientId  = campaign[1];
+  static String cId       = campaign[2];
+  static String agId      = campaign[3];
+
+  public static AdWordsUser getUser( String clid ){
+    AdWordsUser user = new AdWordsUser("adwords@semplest.com","ic0system",
+        clid, "Icosystem", "2H8l6aUm6K_Q44vDvxs3Og");
+    return user;
+  }
+}
+
