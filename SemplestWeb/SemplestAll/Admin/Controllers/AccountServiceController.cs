8fb925fcf9c1a81632da1b88622e51b268a43be0
tutor <tutor@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-04 21:46:21 +0000

    git-svn-id: file:///semplestb/final_repo@1259 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/Admin/Controllers/AccountServiceController.cs
+++ SemplestWeb/SemplestAll/Admin/Controllers/AccountServiceController.cs
@@ -20,42 +20,42 @@ namespace Semplest.Admin.Controllers
         public ActionResult Index(int id)
         {
             SemplestEntities dbcontext = new SemplestEntities();
-            
-            var viewModel = 
+
+            var viewModel =
                from u in dbcontext.Users
                join c in dbcontext.Customers on u.CustomerFK equals c.CustomerPK
                join caa in dbcontext.CustomerAddressAssociations on c.CustomerPK equals caa.CustomerFK
                join a in dbcontext.Addresses on caa.AddressFK equals a.AddressPK
                join sc in dbcontext.StateCodes on a.StateAbbrFK equals sc.StateAbbrPK
                join at in dbcontext.AddressTypes on caa.AddressTypeFK equals at.AddressTypePK
-               join cpa in dbcontext.CustomerPhoneAssociations on c.CustomerPK equals cpa.CustomerFK 
-               join p in dbcontext.Phones on cpa.PhoneFK equals p.PhonePK 
-               join b in dbcontext.BillTypes on c.BillTypeFK equals b.BillTypePK 
-               
-               where (c.CustomerPK==id)
-               select new AccountServiceModel 
+               join cpa in dbcontext.CustomerPhoneAssociations on c.CustomerPK equals cpa.CustomerFK
+               join p in dbcontext.Phones on cpa.PhoneFK equals p.PhonePK
+               join b in dbcontext.BillTypes on c.BillTypeFK equals b.BillTypePK
+
+               where (c.CustomerPK == id)
+               select new AccountServiceModel
                {
-                    AccountNumber = c.CustomerPK ,
-                    Customer  = c.Name ,
-                    FirstName  = u.FirstName  ,
-                    LastName  = u.LastName ,
-                    Address1  = a.Address1 ,
-                    Address2  = a.Address2 ,
-                    City  = a.City ,
-                    State  = sc.StateAbbr ,
-                    Zip = a.ZipCode ,
-                    Phone  = p.Phone1 ,
-                    Email  = u.Email ,
-                    BillType  = b.BillType1,
-                     isActive = u.IsActive 
+                   AccountNumber = c.CustomerPK,
+                   Customer = c.Name,
+                   FirstName = u.FirstName,
+                   LastName = u.LastName,
+                   Address1 = a.Address1,
+                   Address2 = a.Address2,
+                   City = a.City,
+                   State = sc.StateAbbr,
+                   Zip = a.ZipCode,
+                   Phone = p.Phone1,
+                   Email = u.Email,
+                   BillType = b.BillType1,
+                   isActive = u.IsActive
                };
 
             var viewModel2 =
-                from e in dbcontext.Employees 
-                join eca in dbcontext.EmployeeCustomerAssociations on e.EmployeePK equals eca.EmployeeFK 
-                join et in dbcontext.EmployeeTypes on e.EmployeeTypeFK equals et.EmployeeTypeID 
-                join u in dbcontext.Users on e.UsersFK equals u.UserPK 
-                where (eca.CustomerFK==id)
+                from e in dbcontext.Employees
+                join eca in dbcontext.EmployeeCustomerAssociations on e.EmployeePK equals eca.EmployeeFK
+                join et in dbcontext.EmployeeTypes on e.EmployeeTypeFK equals et.EmployeeTypeID
+                join u in dbcontext.Users on e.UsersFK equals u.UserPK
+                where (eca.CustomerFK == id)
                 select new EmployeeCustomerAssociaitionModel
             {
                 AccountNumber = eca.CustomerFK,
@@ -66,10 +66,48 @@ namespace Semplest.Admin.Controllers
                 EmployeeUserPK = u.UserPK
             };
 
+
+
             AccountServiceWithEmployeeModel x = new AccountServiceWithEmployeeModel();
-            x.AccountServiceModel = viewModel.Single(c=>c.AccountNumber==id);
+            var parent =
+         from ch in dbcontext.CustomerHierarchies
+         where (ch.CustomerFK == id)
+         select ch;
+
+
+            if (parent.FirstOrDefault().CustomerFK == parent.FirstOrDefault().CustomerParentFK) //self
+            {
+                x.ParentName = "Self";
+
+            }
+            else
+
+
+                if (parent.FirstOrDefault().CustomerParentFK == null) //parent
+                {
+                    
+                    x.ParentName = "This is a parent";
+
+                }
+                else
+                {
+                    //parent
+                    var findparent = from p in dbcontext.Customers
+                                     where p.CustomerPK==(parent.FirstOrDefault().CustomerParentFK)
+                                     select p;
+                    x.ParentName = findparent.FirstOrDefault().Name;
+
+                }
+
+
+            x.AccountServiceModel = viewModel.Single(c => c.AccountNumber == id);
             x.EmployeeCustomerAssociaitionModel = viewModel2;
-            
+
+
+
+
+
+
 
             //from u in dbcontext.Users
             //join c in dbcontext.Customers on u.CustomerFK equals c.CustomerPK
