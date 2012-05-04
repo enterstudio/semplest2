6d28858de19d78682533e71867f9b6bb10b6b22f
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-04 15:31:45 +0000

    git-svn-id: file:///semplestb/final_repo@1228 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMPlestDBScripts/StoredProcs/UpdateDefaultBidForKeywords.sql
@@ -0,0 +1,48 @@
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.UpdateDefaultBidForKeywords') AND type in (N'P', N'PC'))
+  DROP PROCEDURE dbo.UpdateDefaultBidForKeywords;
+GO
+
+-- +----------------------------------------------------------------------------------------------------------------+
+-- | Author  - Mitch                                                                                                |
+-- | Written - 4/20/2012																							|
+-- | Parms   - 																							|
+-- | Purpose - 						|
+-- +----------------------------------------------------------------------------------------------------------------+
+
+CREATE PROCEDURE dbo.UpdateDefaultBidForKeywords
+(
+	@PromotionPK            INT,
+	@AdvertisingEngine		VARCHAR(50)
+)
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+	DECLARE @ErrMsg VARCHAR(250), @MicroDefaultBid bigint
+
+	--validate data
+	--AdEngine
+	IF NOT EXISTS (select * from AdvertisingEngine a where a.AdvertisingEngine = @AdvertisingEngine)
+	BEGIN
+		SELECT @ErrMsg = 'The Advertising Engine was not found.'; 
+		RAISERROR (@ErrMsg, 16, 1);
+	END;
+	select @MicroDefaultBid = aep.MicroDefaultBid from AdvertisingEnginePromotion aep
+	inner join AdvertisingEngineAccount aea on aea.AdvertisingEngineAccountPK = aep.AdvertisingEngineAccountFK
+	inner join AdvertisingEngine ae on ae.AdvertisingEnginePK = aea.AdvertisingEngineFK
+	where ae.AdvertisingEngine = @AdvertisingEngine and aep.PromotionFK = @PromotionPK
+	
+	update KeywordBid set MicroBidAmount = @MicroDefaultBid
+	from KeywordBid kb 
+	inner join AdvertisingEngine ae on ae.AdvertisingEnginePK = kb.AdvertisingEngineFK
+	where kb.PromotionFK = @PromotionPK and  ae.AdvertisingEngine = @AdvertisingEngine and kb.IsDefaultValue = 1 and kb.MicroBidAmount != -1
+		 
+	
+END TRY
+BEGIN CATCH
+	DECLARE @ErrMessage	nvarchar(4000),
+          @ErrorSeverity	int,
+          @ErrorState		int;
+	SELECT @ErrMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	RAISERROR (@ErrMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
+GO
