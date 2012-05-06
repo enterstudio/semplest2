17e06d7f4c54ea689ce8ddc2fa6059d96a50b9ba
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-06 20:36:40 +0000

    added reporting
    git-svn-id: file:///semplestb/final_repo@1284 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAll/Core/Models/ReportIndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Semplest.Core.Models
+{
+    public class ReportIndexModel
+    {
+        public IEnumerable<SemplestModel.ProductGroup> ProductGroups;
+        public IEnumerable<SemplestModel.AdvertisingEngine> AdvertisingEngines;
+
+    }
+}
