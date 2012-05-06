b67e07fae4423faa881713fe71ae805e3048a989
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-06 20:36:21 +0000

    added reporting
    git-svn-id: file:///semplestb/final_repo@1283 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/SharedResources/Helpers/RoleHelper.cs
+++ SemplestWeb/SemplestAll/SharedResources/Helpers/RoleHelper.cs
@@ -10,12 +10,12 @@ namespace Semplest.SharedResources.Helpers
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
 
-            httpContext.Session[SEMplestConstants.SESSION_USERID] = 1;
-            return true;
-            //string userId = string.Empty;
-            //if (HttpContext.Current.Session["UserId"] != null)
-            //    userId = HttpContext.Current.Session["UserId"].ToString();
-            //return !string.IsNullOrEmpty(userId);
+            //httpContext.Session[SEMplestConstants.SESSION_USERID] = 1;
+            //return true;
+            string userId = string.Empty;
+            if (HttpContext.Current.Session["UserId"] != null)
+                userId = HttpContext.Current.Session["UserId"].ToString();
+            return !string.IsNullOrEmpty(userId);
         }
 
         //protected override bool AuthorizeCore(HttpContextBase httpContext)
