5f39df3d3b2ba64eb8697afe7aea82cc47a7f2f1
lluis <lluis@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-07 14:50:48 +0000

    git-svn-id: file:///semplestb/final_repo@1299 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/other/Money.java
+++ SemplestServices/src/semplest/other/Money.java
@@ -22,7 +22,7 @@ public class Money implements Comparable<Money>, Serializable {
 		milliCents = 0;
 	}
 	
-	Money(long milliCents) {
+	public Money(long milliCents) {
 		this.milliCents = milliCents;
 	}
 	
