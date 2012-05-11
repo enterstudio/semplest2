23e1e0e5fab78e05a82d8fef6b5b4ea5d5d943ce
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-11 16:26:46 +0000

    git-svn-id: file:///semplestb/final_repo@1517 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestBiddingInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestBiddingInterface.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import semplest.server.protocol.adengine.AdEngineInitialData;
+import semplest.server.protocol.adengine.BudgetObject;
 import semplest.server.protocol.adengine.KeywordDataObject;
 
 public interface SemplestBiddingInterface extends ServiceInitialize {
 	
 	HashMap<String, Double> GetMonthlyBudgetPercentPerSE(Integer promotionID, ArrayList<String> searchEngine) throws Exception;
 	HashMap<String, AdEngineInitialData> getInitialValues(Integer promotionID, ArrayList<String> searchEngine) throws Exception;
-	void setBidsInitial(Integer promotionID, String searchEngine) throws Exception;
-	void setBidsUpdate(Integer promotionID, String searchEngine) throws Exception;
+	void setBidsInitial(Integer promotionID, String searchEngine, BudgetObject budgetData) throws Exception;
+	void setBidsUpdate(Integer promotionID, String searchEngine, BudgetObject budgetData) throws Exception;
 
 	
 	HashMap<String,Double> getBid(String accountID, Long campaignID, Long adGroupID, ArrayList<String> keywords) throws Exception ;
@@ -21,6 +22,7 @@ public interface SemplestBiddingInterface extends ServiceInitialize {
 
 	void getBidsUpdate(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
 	void getBidsUpdateNaive(String accountID, Long campaignID, Long adGroupID, String searchEngine) throws Exception;
+
 	
 	
 	
