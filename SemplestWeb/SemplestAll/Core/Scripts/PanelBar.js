3ab2d9fb52e98d2fb0c3b0cbbcecb3065700cb97
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-12 04:07:50 +0000

    SideBar
    git-svn-id: file:///semplestb/final_repo@1567 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/Core/Scripts/PanelBar.js
+++ SemplestWeb/SemplestAll/Core/Scripts/PanelBar.js
@@ -4,6 +4,9 @@
             panelBar.append({ encoded: false, text: data[mainGroup].Name }, panelBar.select(data[mainGroup].Name));
             for (var subitem in data[mainGroup].SubItems) {
                 panelBar.append({ text: data[mainGroup].SubItems[subitem].Name, url: data[mainGroup].SubItems[subitem].Url }, panelBar.element.children("li")[mainGroup]);
+                for (var subitem1 in data[mainGroup].SubItems[subitem].SubItems) {
+                    panelBar.append({ text: data[mainGroup].SubItems[subitem].SubItems[subitem1].Name, url: data[mainGroup].SubItems[subitem].SubItems[subitem1].Url }, panelBar.element.children("li")[mainGroup]);
+                }
             }
         }
     });
