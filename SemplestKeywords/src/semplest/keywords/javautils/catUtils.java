e1c004e971a748bb292a7103491242501c267a26
raj <raj@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-15 16:35:17 +0000

    Added comments
    git-svn-id: file:///semplestb/final_repo@1676 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestKeywords/src/semplest/keywords/javautils/catUtils.java
+++ SemplestKeywords/src/semplest/keywords/javautils/catUtils.java
@@ -150,6 +150,10 @@ public class catUtils
   public static HashMap<String,String> catId( String file ){
     return ioUtils.readPair( file );   
   }
+  // O(n) time
+  // [Note:] Would be faster to sort O(n log n), 
+  // find first and last indices O(log n), if repeated often. 
+  // O(n) seems fast enougn (about 100 ms for 0.3 Million cats)
   public static String[] descendants(HashMap<String,String> cids, String c){
     ArrayList<String> res = new ArrayList<String>();
     for( Map.Entry<String,String> e: cids.entrySet())
