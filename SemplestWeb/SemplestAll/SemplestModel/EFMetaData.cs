7b5a8c8607223ca3d2d62e37a6b41c56b791a3c0
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-15 01:30:22 +0000

    Metadata
    git-svn-id: file:///semplestb/final_repo@1666 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/SemplestModel/EFMetaData.cs
+++ SemplestWeb/SemplestAll/SemplestModel/EFMetaData.cs
@@ -28,17 +28,17 @@ namespace SemplestModel
         [DisplayName("Min Salesperson Commission Percentage:")]
         public decimal MinSalespersonCommissionPercentage { get; set; }
         [DisplayName("Max Salesperson Commission Percentage:")]
-        public Nullable<decimal> MaxSalespersonCommissionPercentage { get; set; }
+        public decimal? MaxSalespersonCommissionPercentage { get; set; }
         [DisplayName("SEMplese Banner Image:")]
         public string DefaultSemplestBannerImageUrl { get; set; }
         [DisplayName("SEMplest Style Sheet:")]
         public string DefaultSemplestStyleSheetUrl { get; set; }
         [DisplayName("Maximum # of sitelinks:")]
-        public Nullable<int> MaxNumberOfSitelinks { get; set; }
+        public int? MaxNumberOfSitelinks { get; set; }
         [DisplayName("Last account number used:")]
-        public Nullable<int> LastAccountNumberUsed { get; set; }
+        public int? LastAccountNumberUsed { get; set; }
         [DisplayName("Last SEMplest Emplyee ID used:")]
-        public Nullable<int> LastSEMplestEmployeeIDused { get; set; }
+        public int? LastSEMplestEmployeeIDused { get; set; }
         public string DefaultEmailContactUs { get; set; }
         public string DefalutEmailContactMe { get; set; }
         [DisplayName("Default Product Group Name:")]
