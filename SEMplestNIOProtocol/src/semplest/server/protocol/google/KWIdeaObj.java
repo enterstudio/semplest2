346635261d2029e566053f9ad8e7e4cc72fdddd1
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-18 16:22:41 +0000

    Objects for google keyword idea implementation
    git-svn-id: file:///semplestb/final_repo@1755 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestNIOProtocol/src/semplest/server/protocol/google/KWIdeaObj.java
@@ -0,0 +1,54 @@
+package semplest.server.protocol.google;
+
+import com.google.api.adwords.v201109.cm.Keyword;
+
+
+public class KWIdeaObj {
+	
+	Keyword kw;
+	Long averageMonthlySearches;
+	Double competition;
+	
+	
+	public KWIdeaObj(Keyword kw, Long averageMonthlySearches, Double competition) {
+		this.kw = kw;
+		this.averageMonthlySearches = averageMonthlySearches;
+		this.competition = competition;
+	}
+
+
+	public Keyword getKw() {
+		return kw;
+	}
+
+
+	public void setKw(Keyword kw) {
+		this.kw = kw;
+	}
+
+
+	public Long getAverageMonthlySearches() {
+		return averageMonthlySearches;
+	}
+
+
+	public void setAverageMonthlySearches(Long averageMonthlySearches) {
+		this.averageMonthlySearches = averageMonthlySearches;
+	}
+
+
+	public Double getCompetition() {
+		return competition;
+	}
+
+
+	public void setCompetition(Double competition) {
+		this.competition = competition;
+	}
+	
+	
+	
+}
+
+
+
