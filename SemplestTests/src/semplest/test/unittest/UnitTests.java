b4c146cffba430f95f6d87d132a2d8bd0569bdc6
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-18 22:27:02 +0000

    git-svn-id: file:///semplestb/final_repo@1806 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestTests/src/semplest/test/unittest/UnitTests.java
+++ SemplestTests/src/semplest/test/unittest/UnitTests.java
@@ -65,7 +65,7 @@ public class UnitTests {
 		Date date = new Date();
 		now = dateFormat.format(date);
 		String reportName = "UnitTestReport" + now + ".txt";
-		String reportPath = "\\semplest\\TestReports\\UnitTest\\" + reportName;
+		String reportPath = "/semplest/TestReports/UnitTest/" + reportName;
 		
 		try{
 			//Create Report Header						
