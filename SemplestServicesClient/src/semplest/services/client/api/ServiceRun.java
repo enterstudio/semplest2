4178f7a70fe55f8af69554a52f8a3770e78ee8db
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-19 23:10:49 +0000

    git-svn-id: file:///semplestb/final_repo@1817 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServicesClient/src/semplest/services/client/api/ServiceRun.java
+++ SemplestServicesClient/src/semplest/services/client/api/ServiceRun.java
@@ -5,30 +5,61 @@ import java.util.HashMap;
 
 import javax.ws.rs.core.MultivaluedMap;
 
-import semplest.server.protocol.TaskOutput;
+import org.apache.log4j.Logger;
 
+import semplest.server.protocol.TaskOutput;
 
 import com.google.gson.Gson;
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 public class ServiceRun
 {
 	private static Gson gson = new Gson();
+	private static final Logger logger = Logger.getLogger(ServiceRun.class);
+
 	public String runMethod(String baseURL, String serviceName, String methodName, String jsonStr, String timeoutMS) throws Exception
 	{
-		Client client = Client.create();
-		MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
-		queryParams.add("jsonStr", jsonStr);
-		queryParams.add("service", serviceName);
-		queryParams.add("method", methodName);
-		if (timeoutMS != null)
+		ClientResponse response = null;
+		Client client = null;
+		String ret = null;
+		try
 		{
-			queryParams.add("timeout", timeoutMS);
+			client = Client.create();
+			MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
+			queryParams.add("jsonStr", jsonStr);
+			queryParams.add("service", serviceName);
+			queryParams.add("method", methodName);
+			if (timeoutMS != null)
+			{
+				queryParams.add("timeout", timeoutMS);
+			}
+			WebResource webResource = client.resource(baseURL);
+			webResource = webResource.queryParams(queryParams);
+			// client response
+			response = webResource.get(ClientResponse.class);
+			ret = response.getEntity(String.class);
+			logger.debug(response.getClientResponseStatus().getStatusCode() + ":" + ret);
 		}
-		WebResource webResource = client.resource(baseURL);
-		String ret = webResource.queryParams(queryParams).get(String.class);
+		catch (Exception e)
+		{
+			throw e;
+		}
+		finally
+		{
+			if (response != null)
+			{
+				response.close();
+			}
+			if (client != null)
+			{
+				client.destroy();
+			}
+
+		}
+
 		HashMap<String, String> resultMap = gson.fromJson(ret, HashMap.class);
 		if (resultMap.containsKey("errorID"))
 		{
@@ -39,10 +70,10 @@ public class ServiceRun
 			return resultMap.get("result");
 		}
 	}
-	
-	public Class<?> getReturnType(Class c,String method)
+
+	public Class<?> getReturnType(Class c, String method)
 	{
-		Method[] allMethods= c.getDeclaredMethods();
+		Method[] allMethods = c.getDeclaredMethods();
 		for (Method m : allMethods)
 		{
 			if (m.getName().equals(method))
@@ -52,26 +83,28 @@ public class ServiceRun
 		}
 		return null;
 	}
-	public TaskOutput RunTask(Class taskClass, String baseurl, String SERVICEOFFERED, String method, String jsonParameters,String timeoutMS) throws Exception
+
+	public TaskOutput RunTask(Class taskClass, String baseurl, String SERVICEOFFERED, String method, String jsonParameters, String timeoutMS)
+			throws Exception
 	{
-		
+
 		TaskOutput out = new TaskOutput();
 		String returnData = runMethod(baseurl, SERVICEOFFERED, method, jsonParameters, timeoutMS);
-		Class returnType = getReturnType(taskClass,method);
+		Class returnType = getReturnType(taskClass, method);
 		if (returnType != null)
-		{	
+		{
 			Object result = gson.fromJson(returnData, returnType);
 			out.setIsSuccessful(true);
 			out.setResult(result);
 			out.setReturnType(returnType);
 		}
-		else 
+		else
 		{
 			out.setIsSuccessful(false);
 			out.setErrorMessage("Error running RunTask " + SERVICEOFFERED + ":" + method + ": " + jsonParameters);
 		}
 		return out;
-		
+
 	}
 
 }
