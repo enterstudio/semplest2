71adbf0557e458d7b3b4d78cf04b4388a7dfb689
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-05-20 12:54:26 +0000

    Reporting
    git-svn-id: file:///semplestb/final_repo@1828 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/Core/Models/ReportIndexModel.cs
+++ SemplestWeb/SemplestAll/Core/Models/ReportIndexModel.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SemplestModel;
 
 namespace Semplest.Core.Models
 {
-    public class ReportIndexModel
+    public class ReportIndexModel : ModelBase
     {
+        public ReportIndexModel()
+        {
+            using (var entities = new SemplestEntities())
+                Configuration = entities.Configurations.FirstOrDefault();
+        }
         public IEnumerable<SemplestModel.ProductGroup> ProductGroups;
         public IEnumerable<SemplestModel.AdvertisingEngine> AdvertisingEngines;
 
