22c02e049e5f9a0db0a9b31041a466692c61f463
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-01 21:52:16 +0000

    implemented UpdateBudget method in AdEngine Service for Google
    git-svn-id: file:///semplestb/final_repo@2104 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestAdengineServiceInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestAdengineServiceInterface.java
@@ -5,21 +5,21 @@ import java.util.List;
 
 public interface SemplestAdengineServiceInterface extends ServiceInitialize
 {
-	// implemented
+	// implemented (at least for Google)
 	public abstract Boolean AddPromotionToAdEngine(Integer customerID, Integer productGroupID, Integer PromotionID, ArrayList<String> adEngineList) throws Exception;
 	public abstract Boolean ExecuteBidProcess(Integer PromotionID, ArrayList<String> adEngine) throws Exception;
 	public abstract Boolean UpdateGeoTargeting(Integer PromotionID, List<String> adEngines) throws Exception;
 	public abstract Boolean AddAd(Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
 	public abstract Boolean DeleteAd(Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
-	public abstract Boolean UpdateAd(Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
+	public abstract Boolean UpdateAd(Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;	
+	public abstract Boolean UpdateBudget(Integer promotionID, Double changeInBudget, List<String> adEngines) throws Exception;
 	
-	// not yet implemented	
-	public abstract Boolean PausePromotion(Integer customerID,Integer promotionID, List<String> adEngines) throws Exception;
+	// not yet implemented		
 	public abstract Boolean PauseProductGroup(Integer customerID,Integer productGroupID, List<String> adEngines) throws Exception;		
 	public abstract Boolean DeleteSiteLinkForAd(Integer customerID,Integer promotionID, Integer promotionAdID, Integer SiteLinkID, List<String> adEngines) throws Exception;
 	public abstract Boolean AddSiteLinkForAd(Integer customerID,Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
-	public abstract Boolean UpdateSiteLinkForAd(Integer customerID,Integer promotionID, Integer promotionAdID, Integer SiteLinkID, String siteLink, List<String> adEngines) throws Exception;		
-	public abstract Boolean UpdateBudget(Integer promotionID, Double changeInBudget, List<String> adEngines) throws Exception;
+	public abstract Boolean UpdateSiteLinkForAd(Integer customerID,Integer promotionID, Integer promotionAdID, Integer SiteLinkID, String siteLink, List<String> adEngines) throws Exception;
+	public abstract Boolean PausePromotion(Integer customerID,Integer promotionID, List<String> adEngines) throws Exception;	
 	public abstract Boolean ChangePromotionStartDate(Integer promotionID, java.util.Date newStartDate, List<String> adEngines) throws Exception;
 	public abstract Boolean DeleteKeyword(Integer promotionID, String Keyword, List<String> adEngines) throws Exception;	
 }
