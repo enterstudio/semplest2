7e92badbd2a4544ffca9e3f8cf81404ecba5fdc6
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-04 13:00:00 +0000

    git-svn-id: file:///semplestb/final_repo@2164 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMPlestDBScripts/StoredProcs/UpdateRemainingBudgetInCycle.sql
+++ SEMPlestDBScripts/StoredProcs/UpdateRemainingBudgetInCycle.sql
@@ -28,24 +28,41 @@ BEGIN TRY
 		SELECT @ErrMsg = 'The Promotion was not found.'; 
 		RAISERROR (@ErrMsg, 16, 1);
 	END;
-	Declare @costTable Table(KeywordFK int, Cost Bigint);
-	--get general info
-	insert into @costTable(KeywordFK, Cost)
-	select kb.KeywordFK, Sum(aerd.MicroCost) [TotalMicroSpent] from KeywordBid kb 
-	inner join AdvertisingEngineReportData aerd on aerd.KeywordBidFK = kb.KeywordBidPK
-	inner join AdvertisingEngine ae on ae.AdvertisingEnginePK = kb.AdvertisingEngineFK
-	where kb.PromotionFK = @PromotionPK
-	and aerd.TransactionDate >= @StartDate and aerd.TransactionDate <= @EndDate
-	group by kb.KeywordFK	
-	having Sum(aerd.MicroCost) > 0
+	Declare @ReportDataTable Table(KeywordBidFK int, TransactionDate datetime2, MicroCost bigint)
+	--Table of transactions not yet applied to promotion from all AdEngines
+	insert into @ReportDataTable(KeywordBidFK,TransactionDate,MicroCost)
+	select aerd.KeywordBidFK, aerd.TransactionDate, aerd.MicroCost from KeywordBid kb 
+		inner join AdvertisingEngineReportData aerd on aerd.KeywordBidFK = kb.KeywordBidPK
+		inner join AdvertisingEngine ae on ae.AdvertisingEnginePK = kb.AdvertisingEngineFK
+		where kb.PromotionFK = @PromotionPK
+		and aerd.TransactionDate >= @StartDate and aerd.TransactionDate <= @EndDate and aerd.CostAppliedToPromotionDate is null
+		
+	--Declare @costTable Table(TransactionDate datetime2,KeywordFK int, Cost Bigint);
+	--get the transactions to apply as a cost to the promotion
+	--insert into @costTable(KeywordFK, Cost)
+	--	select kb.KeywordFK, Sum(aerd.MicroCost) [TotalMicroSpent] from KeywordBid kb 
+	--	inner join AdvertisingEngineReportData aerd on aerd.KeywordBidFK = kb.KeywordBidPK
+	--	inner join AdvertisingEngine ae on ae.AdvertisingEnginePK = kb.AdvertisingEngineFK
+	--	where kb.PromotionFK = @PromotionPK
+	--	and aerd.TransactionDate >= @StartDate and aerd.TransactionDate <= @EndDate and aerd.CostAppliedToPromotionDate is null
+	--group by kb.KeywordFK	
+	--having Sum(aerd.MicroCost) > 0
 	--Total cost update a given Date
-	select @Cost = SUM(cost) from @costTable
+	select @Cost = SUM(MicroCost) from @ReportDataTable
 	--Total amount paid until date
 	select @paid = SUM(pp.Amount) * 1000000 from PromotionPayment pp 
 	where pp.IsValid = 1 and pp.BudgetToAddDate <= @EndDate and pp.PromotionFK = @PromotionPK
 	--update the remaining budget
-	update Promotion set RemainingBudgetInCycle = (@paid -@Cost)/1000000, EditedDate = CURRENT_TIMESTAMP
-	from Promotion p where p.PromotionPK = @PromotionPK
+	BEGIN TRANSACTION
+	
+	 update Promotion set RemainingBudgetInCycle = (@paid -@Cost)/1000000, EditedDate = CURRENT_TIMESTAMP
+	 from Promotion p where p.PromotionPK = @PromotionPK
+	
+	 update AdvertisingEngineReportData set CostAppliedToPromotionDate = CURRENT_TIMESTAMP
+	 from AdvertisingEngineReportData aerd
+	 inner join @ReportDataTable rd on rd.KeywordBidFK = aerd.KeywordBidFK and rd.TransactionDate = aerd.TransactionDate
+	
+	COMMIT TRANSACTION
 	
 	Set @ID = @PromotionPK
 	return @ID
@@ -53,6 +70,8 @@ BEGIN TRY
 	
 END TRY
 BEGIN CATCH
+IF XACT_STATE() != 0 OR @@TRANCOUNT > 0
+    ROLLBACK TRANSACTION;
 	DECLARE @ErrMessage	nvarchar(4000),
           @ErrorSeverity	int,
           @ErrorState		int;
