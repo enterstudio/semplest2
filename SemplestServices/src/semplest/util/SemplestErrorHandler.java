bb1770a004d7b1123ce710e97f8b026e4cccce5b
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-06 21:05:20 +0000

    Made Error Handlers Static
    git-svn-id: file:///semplestb/final_repo@2269 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/util/SemplestErrorHandler.java
+++ SemplestServices/src/semplest/util/SemplestErrorHandler.java
@@ -5,7 +5,7 @@ import semplest.server.service.springjdbc.SemplestDB;
 
 public class SemplestErrorHandler {	
 
-	public void logToDatabase(Exception e){		 
+	public static void logToDatabase(Exception e){		 
 		String serviceName = SEMplestService.properties.getProperty("YAJSW.servicename");
 		SemplestDB.logError(e, serviceName);
 	}
