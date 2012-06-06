f782970e1391d1d71b95ff1e92c43a66849d2b78
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-06 14:50:13 +0000

    added todo and other instructions
    git-svn-id: file:///semplestb/final_repo@2250 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestAdengineServiceInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestAdengineServiceInterface.java
@@ -5,12 +5,18 @@ import java.util.List;
 
 public interface SemplestAdengineServiceInterface extends ServiceInitialize
 {
-	Boolean AddPromotionToAdEngine(Integer customerID, Integer productGroupID, Integer PromotionID, ArrayList<String> adEngineList) throws Exception;
-	Boolean ExecuteBidProcess(Integer PromotionID, ArrayList<String> adEngine) throws Exception;
-	Boolean UpdateGeoTargeting(Integer PromotionID, List<String> adEngines) throws Exception;
-	Boolean AddAd(Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
-	Boolean DeleteAdEngineAd(Integer customerID, Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
+	// worker method (TODO: these must throw exception, NOT return Boolean)
+	Boolean AddPromotionToAdEngine(Integer customerID, Integer productGroupID, Integer PromotionID, ArrayList<String> adEngineList) throws Exception; 
 	Boolean DeleteAd(Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
+	Boolean ExecuteBidProcess(Integer PromotionID, ArrayList<String> adEngine) throws Exception;
+	
+	// schedule (TODO: these should have names ending in Schedule)
+	//Boolean AddPromotionToAdEngineSchedule(Integer customerID, Integer productGroupID, Integer PromotionID, ArrayList<String> adEngineList) throws Exception;
+	Boolean UpdateGeoTargeting(Integer PromotionID, List<String> adEngines) throws Exception;
+	Boolean DeleteAdEngineAd(Integer customerID, Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
+	
+	// should be scheduled (TODO: these must throw exception, NOT return Boolean) 
+	Boolean AddAd(Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;
 	Boolean UpdateAd(Integer promotionID, Integer promotionAdID, List<String> adEngines) throws Exception;	
 	Boolean UpdateBudget(Integer promotionID, Double changeInBudget, List<String> adEngines) throws Exception;
 	Boolean PausePromotion(Integer promotionID, List<String> adEngines) throws Exception;
