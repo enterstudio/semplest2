2c64d1b44bd101cbc40875083fd86081a2bc7d38
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-06 20:49:47 +0000

    git-svn-id: file:///semplestb/final_repo@2268 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/util/SemplestErrorHandler.java
+++ SemplestServices/src/semplest/util/SemplestErrorHandler.java
@@ -3,14 +3,10 @@ package semplest.util;
 import semplest.server.service.SEMplestService;
 import semplest.server.service.springjdbc.SemplestDB;
 
-public class SemplestErrorHandler {
-	private String serviceName;
-	
-	public SemplestErrorHandler(){
-		//serviceName = SEMplestService.properties.getProperty("YAJSW.servicename");
-	}
+public class SemplestErrorHandler {	
 
 	public void logToDatabase(Exception e){		 
-		//SemplestDB.logError(e, serviceName);
+		String serviceName = SEMplestService.properties.getProperty("YAJSW.servicename");
+		SemplestDB.logError(e, serviceName);
 	}
 }
