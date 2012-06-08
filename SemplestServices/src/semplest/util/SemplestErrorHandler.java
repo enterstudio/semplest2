7ce77389855383f5b464fe8f32d2a2c714434b85
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-08 20:12:35 +0000

    git-svn-id: file:///semplestb/final_repo@2353 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/util/SemplestErrorHandler.java
+++ SemplestServices/src/semplest/util/SemplestErrorHandler.java
@@ -1,14 +1,23 @@
 package semplest.util;
 
+import org.apache.log4j.Logger;
+
 import semplest.server.service.SEMplestService;
 import semplest.server.service.springjdbc.SemplestDB;
 
 public class SemplestErrorHandler {	
 
+	private static final Logger logger = Logger.getLogger(SEMplestService.class);
+	
 	public static void logToDatabase(Exception e){		 
-		if(SEMplestService.properties == null) return;
-		
-		String serviceName = SEMplestService.properties.getProperty("YAJSW.servicename");
-		SemplestDB.logError(e, serviceName);
+		try{
+			if(SEMplestService.properties == null) return;
+			
+			String serviceName = SEMplestService.properties.getProperty("YAJSW.servicename");
+			SemplestDB.logError(e, serviceName);
+		}
+		catch(Exception e1){
+			logger.debug("Error logging error to the databse - " + e1.getMessage());
+		}
 	}
 }
