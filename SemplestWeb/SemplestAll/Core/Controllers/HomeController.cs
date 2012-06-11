e87909b82cbaab28cfa6fa1dd8e33659761fd21f
tutor <tutor@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-11 14:56:00 +0000

    added back the  AddProductGroupName
    git-svn-id: file:///semplestb/final_repo@2416 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/Core/Controllers/HomeController.cs
+++ SemplestWeb/SemplestAll/Core/Controllers/HomeController.cs
@@ -135,6 +135,34 @@ namespace SemplestWebApp.Controllers
         }
 
         [HttpPost]
+        public ActionResult AddProductGroupName(FormCollection fc)
+        {
+            try
+            {
+                string productgroupname = fc["newproductgroupname"].ToString().Trim();
+                Credential c = ((Credential)(Session[Semplest.SharedResources.SEMplestConstants.SESSION_USERID]));
+                if (productgroupname == null || productgroupname == "") throw new Exception("Blank");
+                SemplestEntities dbContext = new SemplestEntities();
+                var productgroupexists = from pg in dbContext.ProductGroups
+                                         where pg.CustomerFK.Equals(c.User.CustomerFK.Value) && pg.ProductGroupName.Equals(productgroupname)
+                                         select pg;
+
+                if (productgroupexists.Count() > 0) throw new Exception("Duplicate");
+
+                dbContext.ProductGroups.AddObject(new ProductGroup { CustomerFK = c.User.CustomerFK.Value, ProductGroupName = productgroupname, StartDate = DateTime.Now, IsActive = true });
+                dbContext.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return RedirectToAction("Index2");
+        }
+
+
+        [HttpPost]
         [ActionName("SearchKeywords")]
         [AcceptSubmitType(Name = "Command", Type = "GetKeywords")]
         public ActionResult GetKeywords(SearchKeywordsModel model)
