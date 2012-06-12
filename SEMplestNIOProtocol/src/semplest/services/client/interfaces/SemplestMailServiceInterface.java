ebcb39958398807f0d10af808d9753abc1194578
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-12 19:46:42 +0000

    git-svn-id: file:///semplestb/final_repo@2452 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestMailServiceInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestMailServiceInterface.java
@@ -2,5 +2,5 @@ package semplest.services.client.interfaces;
 
 public interface SemplestMailServiceInterface extends ServiceInitialize
 {
-	public abstract Boolean SendEmail(String subject,String from,  String recipient, String msgTxt) throws Exception;
+	public abstract Boolean SendEmail(String subject,String from,  String recipient, String msgTxt, String msgType) throws Exception;
 }
