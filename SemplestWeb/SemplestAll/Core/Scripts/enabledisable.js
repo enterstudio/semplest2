960f21f15bf98630f523545cb2aa3a6c29b28318
manik <manik@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-16 03:33:34 +0000

    open all tabs and enable disable script added
    git-svn-id: file:///semplestb/final_repo@2591 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAll/Core/Scripts/enabledisable.js
@@ -0,0 +1,32 @@
+﻿function disableIsLaunched() {
+    $('.k-textbox').attr('disabled', 'disabled');
+    $('.k-dropdownlist').attr('disabled', 'disabled');
+    $('input').attr('disabled', true);
+}
+function disableIsLaunchedCompleted() {
+    $('.k-textbox').attr('disabled', 'disabled');
+    $('.k-dropdownlist').attr('disabled', 'disabled');
+    $('.k-button').attr('disabled', 'disabled');
+    $('input').attr('disabled', true);
+}
+function enableIsLaunched() {
+    $('.k-textbox').attr('disabled', 'disabled');
+    $('.k-dropdownlist').attr('disabled', 'disabled');
+    $('input').attr('disabled', true);
+}
+function openAllTabs() {
+    tabStrip.enable(tab.next(), tab.next().hasClass("k-state-disabled"));
+    tabStrip.append({
+        text: "Negative Keywords",
+        contentUrl: '/Campaign/NegativeKeywords'
+    }, tabStrip.tabGroup.children("li:last")).select();
+    tabStrip.append({
+        text: "Additional Links",
+        contentUrl: '/Campaign/AdditionalLinks'
+    }, tabStrip.tabGroup.children("li:last")).select();
+    tabStrip.append({
+        text: "BillingLaunch",
+        contentUrl: '/Campaign/BillingLaunch'
+    }, tabStrip.tabGroup.children("li:last")).select();
+   
+}
