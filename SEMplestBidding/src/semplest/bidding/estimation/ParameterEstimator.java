558e5f6c401886612b24af0a9aab04fe5ca15a17
alex <alex@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-21 21:57:53 +0000

    Added ability to add a constraint to the Nelder-Mead solver.
    git-svn-id: file:///semplestb/final_repo@2734 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestBidding/src/semplest/bidding/estimation/ParameterEstimator.java
+++ SEMplestBidding/src/semplest/bidding/estimation/ParameterEstimator.java
@@ -1,81 +1,87 @@
 package semplest.bidding.estimation;
 
+import flanagan.math.Minimisation;
+import flanagan.math.MinimisationFunction;
 
-import flanagan.math.*;
+class LeastSquares implements MinimisationFunction {
 
-class LeastSquares implements MinimisationFunction{
+  private ParametricFunction pf = null;
 
-	private ParametricFunction pf = null;
+  private double[][] input = null;
+  private double[] output = null;
 
-	private double [][] input = null;
-	private double [] output = null;
-	
-	public LeastSquares(ParametricFunction pf, double [][] input, double [] output){
-		this.pf=pf;
-		this.input=input;
-		this.output=output;
-	}
-	
-	@Override
-	public double function(double[] param) {
-		double sum = 0;
-		
-		try {
-			for(int i=0; i<input.length; i++){
-				sum+=Math.pow(pf.function(input[i], param)-output[i],2);
-				if(Double.isNaN(sum)){
-					System.out.println(sum+": x="+input[0][0]+" " + param[0]+ " "+ param[1]+" "+param[2]+" "+param[3]);
-					break;
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return sum;
-	}
+  public LeastSquares(ParametricFunction pf, double[][] input, double[] output) {
+    this.pf = pf;
+    this.input = input;
+    this.output = output;
+  }
+
+  @Override
+  public double function(double[] param) {
+    double sum = 0;
+    try {
+      for (int i = 0; i < input.length; i++) {
+        double fVal = pf.function(input[i], param);
+        sum += Math.pow(fVal - output[i], 2);
+        if (Double.isNaN(sum)) {
+          System.out.println(sum + ": x=" + input[0][0] + " " + param[0] + " "
+              + param[1] + " " + param[2] + " " + param[3]);
+          break;
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return sum;
+  }
 }
 
-
 public class ParameterEstimator {
-	
-	private LeastSquares lsq;
-	private Minimisation min = new Minimisation();
 
-	
-	private double [] startPoint = null;
-	private double [] stepSize = null;
-	double fTol = 1e-15;
-	
-	public ParameterEstimator(ParametricFunction pf, double [][] input, double [] output){
-		lsq = new LeastSquares(pf, input, output);
-	}
-	
-	public void estimateParams(){				
-	    // Nelder and Mead minimisation procedure
-	    min.nelderMead(lsq, startPoint, stepSize, fTol);
-	}
-	
-	public void suppressNoConvergenceMessage(){
-		min.suppressNoConvergenceMessage();
-	}
-	
+  private LeastSquares lsq;
+  private Minimisation min = new Minimisation();
 
-	public void setStartPoint(double [] startPoint){
-		this.startPoint=startPoint;
-	}
-	
-	public void setStepSize(double [] stepSize){
-		this.stepSize=stepSize;
-	}
-	
-	public double getMinimum(){
-		return min.getMinimum();
-	}
-	
-	public double [] getParamValues(){
-		return min.getParamValues();
-	}
-	
-	
+  private double[] startPoint = null;
+  private double[] stepSize = null;
+  double fTol = 1e-15;
+  int maxIter = 10000;
 
+  public ParameterEstimator(ParametricFunction pf, double[][] input,
+      double[] output) {
+    lsq = new LeastSquares(pf, input, output);
+  }
+
+  public void estimateParams() {
+    // Nelder and Mead minimisation procedure
+    min.nelderMead(lsq, startPoint, stepSize, fTol, maxIter);
+  }
+
+  public void suppressNoConvergenceMessage() {
+    min.suppressNoConvergenceMessage();
+  }
+
+  public void setStartPoint(double[] startPoint) {
+    this.startPoint = startPoint;
+  }
+
+  public void setStepSize(double[] stepSize) {
+    this.stepSize = stepSize;
+  }
+
+  public boolean getConvergenceStatus() {
+    return min.getConvStatus();
+  }
+
+  public double getMinimum() {
+    return min.getMinimum();
+  }
+
+  public double[] getParamValues() {
+    return min.getParamValues();
+  }
+
+  // add a constraint to the minimizer
+  public void addConstraint(int paramIndex, int conDir, double constraint) {
+    min.addConstraint(paramIndex, conDir, constraint);
+  }
 }
