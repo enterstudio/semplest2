27832b9ce7ee7398041ace7f647fd9dc71ae1d26
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-25 16:22:33 +0000

    git-svn-id: file:///semplestb/final_repo@2769 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/service/bidding/BidGeneratorObj.java
+++ SemplestServices/src/semplest/service/bidding/BidGeneratorObj.java
@@ -337,8 +337,8 @@ public class BidGeneratorObj {
 			
 			ArrayList<BidElement> bidsMatchTypeMSN_Temp = new ArrayList<BidElement>();
 			for(Keyword w : kwList){
-				logger.info("Keyword: "+w.getText()+" "+w.getBroadMatchBid().getAmount()+" "+w.getExactMatchBid().getAmount()+" "+w.getPhraseMatchBid().getAmount());
-				logger.info("Keyword ID: "+w.getId());
+				logger.info("Keyword: "+w.getText()+" "+w.getBroadMatchBid().getAmount()+" "+
+						w.getExactMatchBid().getAmount()+" "+w.getPhraseMatchBid().getAmount()+", Keyword ID: "+w.getId());
 				
 				String matchType = ProtocolEnum.SemplestMatchType.Exact.name(); 
 				if(w.getExactMatchBid().getAmount()==null || w.getExactMatchBid().getAmount() < 0.0001){
