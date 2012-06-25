6fa177a3e4bde84312afa123b62d20871225e836
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-25 18:47:40 +0000

    encryption stub
    git-svn-id: file:///semplestb/final_repo@2774 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAll/SharedResources/Helpers/EncryptionHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SharedResources.Helpers;
+
+namespace Semplest.SharedResources.Encryption
+{
+    public sealed class EncryptionHelper
+    {
+        private static EncryptionHelper _instance;
+        private static string _encryptionKey;
+
+        private EncryptionHelper() { }
+
+        public static bool VerifyPassword(string s)
+        {
+            if (string.IsNullOrEmpty(_encryptionKey))
+                Initalize();
+            return false;
+        }
+
+        public string DecryptString(string s)
+        {
+            if (string.IsNullOrEmpty(_encryptionKey))
+                Initalize();
+            return string.Empty;
+        }
+
+        public string EncryptString(string s)
+        {
+            if (string.IsNullOrEmpty(_encryptionKey))
+                Initalize();
+            return string.Empty;
+        }
+
+        private static void Initalize()
+        {
+            using (SemplestModel.Semplest dbContext = new SemplestModel.Semplest())
+            {
+                _encryptionKey = dbContext.Configurations.FirstOrDefault().SemplestEncryptionkey;
+            }
+        }
+    }
+}
