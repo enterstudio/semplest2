aca562dd4c1d1297ac255e485fe85e9307c10780
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-25 19:46:06 +0000

    Controller to test encryption
    git-svn-id: file:///semplestb/final_repo@2777 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAll/Core/Controllers/EncryptionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semplest.Core.Controllers
+{
+    public class EncryptionController : Controller
+    {
+        //
+        // GET: /Encryption/
+
+        public ActionResult Index()
+        {
+            bool retval = Semplest.SharedResources.Encryption.EncryptionHelper.VerifyPassword("blah");
+            return View();
+        }
+
+    }
+}
