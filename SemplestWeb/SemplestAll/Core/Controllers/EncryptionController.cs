1b6413968781ab2290f1ac7fe2db5f6201be82d5
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-26 14:28:51 +0000

    test encryption code
    git-svn-id: file:///semplestb/final_repo@2784 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestWeb/SemplestAll/Core/Controllers/EncryptionController.cs
+++ SemplestWeb/SemplestAll/Core/Controllers/EncryptionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Semplest.SharedResources.Encryption;
 
 namespace Semplest.Core.Controllers
 {
@@ -13,7 +14,11 @@ namespace Semplest.Core.Controllers
 
         public ActionResult Index()
         {
-            bool retval = Semplest.SharedResources.Encryption.EncryptionHelper.VerifyPassword("blah");
+            EncryptionHelper aes = Semplest.SharedResources.Encryption.EncryptionHelper.getInstance();
+            String test = "01tsemplest";
+            String en = aes.EncryptString(test);
+            String de = aes.DecryptString(en);
+
             return View();
         }
 
