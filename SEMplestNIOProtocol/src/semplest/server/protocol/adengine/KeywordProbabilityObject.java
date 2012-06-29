379b836fd2a2f8e99e2e603f893698703ef668d8
lluis <lluis@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-06-29 21:18:48 +0000

    git-svn-id: file:///semplestb/final_repo@2923 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/server/protocol/adengine/KeywordProbabilityObject.java
+++ SEMplestNIOProtocol/src/semplest/server/protocol/adengine/KeywordProbabilityObject.java
@@ -76,24 +76,18 @@ public class KeywordProbabilityObject
 	{
 		this.isDeleted = isDeleted;
 	}
+	
+	
+	
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((isActive == null) ? 0 : isActive.hashCode());
-		result = prime * result + ((isDeleted == null) ? 0 : isDeleted.hashCode());
-		result = prime * result + ((isNegative == null) ? 0 : isNegative.hashCode());
-		result = prime * result + ((isTargetGoogle == null) ? 0 : isTargetGoogle.hashCode());
-		result = prime * result + ((isTargetMSN == null) ? 0 : isTargetMSN.hashCode());
 		result = prime * result + ((keyword == null) ? 0 : keyword.hashCode());
-		result = prime * result + ((keywordPK == null) ? 0 : keywordPK.hashCode());
-		result = prime * result + ((semplestProbability == null) ? 0 : semplestProbability.hashCode());
 		return result;
 	}
 	@Override
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -101,61 +95,10 @@ public class KeywordProbabilityObject
 		if (getClass() != obj.getClass())
 			return false;
 		KeywordProbabilityObject other = (KeywordProbabilityObject) obj;
-		if (isActive == null)
-		{
-			if (other.isActive != null)
-				return false;
-		}
-		else if (!isActive.equals(other.isActive))
-			return false;
-		if (isDeleted == null)
-		{
-			if (other.isDeleted != null)
-				return false;
-		}
-		else if (!isDeleted.equals(other.isDeleted))
-			return false;
-		if (isNegative == null)
-		{
-			if (other.isNegative != null)
-				return false;
-		}
-		else if (!isNegative.equals(other.isNegative))
-			return false;
-		if (isTargetGoogle == null)
-		{
-			if (other.isTargetGoogle != null)
-				return false;
-		}
-		else if (!isTargetGoogle.equals(other.isTargetGoogle))
-			return false;
-		if (isTargetMSN == null)
-		{
-			if (other.isTargetMSN != null)
-				return false;
-		}
-		else if (!isTargetMSN.equals(other.isTargetMSN))
-			return false;
-		if (keyword == null)
-		{
+		if (keyword == null) {
 			if (other.keyword != null)
 				return false;
-		}
-		else if (!keyword.equals(other.keyword))
-			return false;
-		if (keywordPK == null)
-		{
-			if (other.keywordPK != null)
-				return false;
-		}
-		else if (!keywordPK.equals(other.keywordPK))
-			return false;
-		if (semplestProbability == null)
-		{
-			if (other.semplestProbability != null)
-				return false;
-		}
-		else if (!semplestProbability.equals(other.semplestProbability))
+		} else if (!keyword.equals(other.keyword))
 			return false;
 		return true;
 	}
