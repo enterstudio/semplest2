79b9fe0c7b6ff98dd71f38160f26d543c7ad01c6
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-03 14:23:53 +0000

    git-svn-id: file:///semplestb/final_repo@2969 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestTests/src/semplest/test/other/InstallationSetup.java
+++ SemplestTests/src/semplest/test/other/InstallationSetup.java
@@ -13,65 +13,112 @@ import java.util.Properties;
 
 
 public class InstallationSetup {
-
-	private HashMap<String, String> properties = new HashMap<String, String>();
+	
+	//Configurations
+	//DEV
+	private final static String DevJdbc = "jdbc:jtds:sqlserver://172.18.9.23/semplest";
+	private final static String DevEsbServer = "VMDEVJAVA1";
+	private final static String DevServiceServer = "VMDEVJAVA2";
+	private final static String DevKeywordServer = "VMDEVJAVA3";
+	
+	//TEST
+	private final static String TestJdbc = "jdbc:jtds:sqlserver://172.18.9.35/semplestTest";
+	private final static String TestEsbServer = "VMJAVA1";
+	private final static String TestServiceServer = "VMJAVA2";
+	private final static String TestKeywordServer = "VMJAVA3";
+	
+	//EXP
+	private final static String ExpJdbc = "jdbc:jtds:sqlserver://172.18.9.23/semplest_testing";
+	private final static String ExpEsbServer = "172.18.9.26";
+	private final static String ExpServiceServer = "VMDEVJAVA3";
+	private final static String ExpKeywordServer = "VMDEVJAVA2";
+	
+	//UAT
+	private final static String UatJdbc = "jdbc:jtds:sqlserver://172.18.9.35/semplest";
+	private final static String UatEsbServer = "NY-semplestDev2";
+	private final static String UatServiceServer = "NY-semplestDev2";
+	private final static String UatKeywordServer = "NY-semplestDev2";
+	
+	private enum SERVER_BOX {DEV, TEST, EXP, UAT};
+	
+	private static HashMap<SERVER_BOX, ServerConfiguration> config = new HashMap<SERVER_BOX, ServerConfiguration>();
 	
 	public static void main(String args[]){		
 		
 		InstallationSetup is = new InstallationSetup();
+		
+		is.init();
+		
 		try {
 			InetAddress ownIP = InetAddress.getLocalHost();
 			String hostName = ownIP.getHostName();			
 			
-			if(hostName.equals("VMDEVJAVA1")){
+			if(hostName.equals(DevEsbServer)){
 				//DEV Box ESB
-				is.EsbDev();
+				is.setEsb(SERVER_BOX.DEV);
 			}
 			
-			if(hostName.equals("VMDEVJAVA2")){
+			if(hostName.equals(DevServiceServer)){
 				//DEV Box Services
-				is.ServiceDev();
-				
-				//Exp Keyword Service for 172.18.9.26 ESB
-				is.KeywordExp();
+				is.setServices(SERVER_BOX.DEV);
 			}
 			
-			if(hostName.equals("VMJAVA1")){
-				//TEST Box ESB
-				is.EsbTest();
-			}
-			
-			if(hostName.equals("VMJAVA2")){
-				//TEST Box Services
-				is.ServiceTest();
-			}			
-			
-			if(hostName.equals("VMDEVJAVA3")){
+			if(hostName.equals(DevKeywordServer)){
 				//DEV Box Keyword Service
-				is.KeywordDev();
-				
-				//Exp Services for 172.18.9.26 ESB
-				is.ServiceExp();
+				is.setKeywordService(SERVER_BOX.DEV);
 			}
 			
-			if(hostName.equals("VMJAVA3")){
-				//TEST Box Keyword Service
-				is.KeywordTest();
+			if(hostName.equals(TestEsbServer)){
+				//TEST Box ESB
+				is.setEsb(SERVER_BOX.TEST);
+			}
+			
+			if(hostName.equals(TestServiceServer)){
+				//TEST Box Services
+				is.setServices(SERVER_BOX.TEST);
 			}			
 			
-			if(ownIP.getHostAddress().equalsIgnoreCase("172.18.9.26")){
+			if(hostName.equals(TestKeywordServer)){
+				//TEST Box Keyword Service
+				is.setKeywordService(SERVER_BOX.TEST);
+			}			
+			
+			if(ownIP.getHostAddress().equalsIgnoreCase(ExpEsbServer)){
 				//Exp Box ESB
-				is.EsbExp();
+				is.setEsb(SERVER_BOX.EXP);
 			}
 			
+			if(hostName.equals(ExpServiceServer)){				
+				//Exp Services
+				is.setServices(SERVER_BOX.EXP);
+			}
+			
+			if(hostName.equals(ExpKeywordServer)){				
+				//Exp Keyword Service
+				is.setKeywordService(SERVER_BOX.EXP);
+			}
+			
+			if(hostName.equals(config.get(SERVER_BOX.UAT).esbServer)){				
+				//UAT Box ESB
+				is.setEsb(SERVER_BOX.UAT);
+			}
+			
+			if(hostName.equals(config.get(SERVER_BOX.UAT).serviceServer)){				
+				//UAT Box Services
+				is.setServices(SERVER_BOX.UAT);
+			}
+			
+			if(hostName.equals(config.get(SERVER_BOX.UAT).keywordServer)){				
+				//UAT Box Keyword Service
+				is.setKeywordService(SERVER_BOX.UAT);
+			}
 			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public void EsbDev() throws Exception{
+	public void setEsb(SERVER_BOX box) throws Exception{
 		String esbProps = "C:\\SEMplestESB\\properties\\system.properties";
 		
 		Properties properties = new Properties();
@@ -79,132 +126,45 @@ public class InstallationSetup {
 		properties.load(in);
 		in.close();		
 		
-		properties.setProperty("BrokerIP", "VMDEVJAVA1");		
+		String esb = config.get(box).esbServer;
+		properties.setProperty("BrokerIP", esb);		
 		
 		FileOutputStream out = new FileOutputStream(esbProps);	
-		properties.store(out,"ESB on Dev Box. Updated by InstallationSetup (Nan).");
-		out.close();
+		String comment = "ESB on " + box.name() + " Box. Updated by InstallationSetup (Nan).";
+		properties.store(out,comment);
+		out.close();		
 	}
 	
-	public void EsbTest() throws Exception{
-		String esbProps = "C:\\SEMplestESB\\properties\\system.properties";
+	public void setServices(SERVER_BOX box){
+		String serviceName;		
+		String comment = "Service on " + box.name() + " Box. Updated by InstallationSetup (Nan).";		
+		String jdbc = config.get(box).jdbc;
 		
-		Properties properties = new Properties();
-		FileInputStream in = new FileInputStream(esbProps);
-		properties.load(in);
-		in.close();		
+		serviceName = "SemplestAdengineService";
+		setProps(serviceName, jdbc, comment);
 		
-		properties.setProperty("BrokerIP", "VMJAVA1");		
+		serviceName = "SemplestBidGeneratorService";
+		setProps(serviceName, jdbc, comment);
 		
-		FileOutputStream out = new FileOutputStream(esbProps);	
-		properties.store(out,"ESB on Test Box. Updated by InstallationSetup (Nan).");
-		out.close();
+		serviceName = "SemplestChaseOrbitalGatewayService";
+		setProps(serviceName, jdbc, comment);
+		
+		serviceName = "SemplestMailService";
+		setProps(serviceName, jdbc, comment);
+		
+		serviceName = "SemplestSchedulerService";
+		setProps(serviceName, jdbc, comment);
 	}
 	
-	public void EsbExp() throws Exception{
-		String esbProps = "C:\\SEMplestESB\\properties\\system.properties";
+	public void setKeywordService(SERVER_BOX box){
+		String serviceName = "SemplestKeywordGeneratorService";		
+		String comment = "Service on " + box.name() + " Box. Updated by InstallationSetup (Nan).";		
+		String jdbc = config.get(box).jdbc;
 		
-		Properties properties = new Properties();
-		FileInputStream in = new FileInputStream(esbProps);
-		properties.load(in);
-		in.close();		
-		
-		properties.setProperty("BrokerIP", "172.18.9.26");		
-		
-		FileOutputStream out = new FileOutputStream(esbProps);	
-		properties.store(out,"ESB on Experiment Box. Updated by InstallationSetup (Nan).");
-		out.close();
+		setProps(serviceName, jdbc, comment);
 	}
 	
-	public void ServiceDev(){
-		String service;
-		
-		service = "SemplestAdengineService";
-		setServicePropsDev(service);
-		
-		service = "SemplestBidGeneratorService";
-		setServicePropsDev(service);
-		
-		service = "SemplestChaseOrbitalGatewayService";
-		setServicePropsDev(service);
-		
-		service = "SemplestMailService";
-		setServicePropsDev(service);
-		
-		service = "SemplestSchedulerService";
-		setServicePropsDev(service);
-	}
-	
-	public void ServiceTest(){
-		String service;
-		
-		service = "SemplestAdengineService";
-		setServicePropsTest(service);
-		
-		service = "SemplestBidGeneratorService";
-		setServicePropsTest(service);
-		
-		service = "SemplestChaseOrbitalGatewayService";
-		setServicePropsTest(service);
-		
-		service = "SemplestMailService";
-		setServicePropsTest(service);
-		
-		service = "SemplestSchedulerService";
-		setServicePropsTest(service);
-	}
-	
-	public void ServiceExp(){
-		String service;
-		
-		service = "SemplestAdengineService";
-		setServicePropsExp(service);
-		
-		service = "SemplestBidGeneratorService";
-		setServicePropsExp(service);
-		
-		service = "SemplestChaseOrbitalGatewayService";
-		setServicePropsExp(service);
-		
-		service = "SemplestMailService";
-		setServicePropsExp(service);
-		
-		service = "SemplestSchedulerService";
-		setServicePropsExp(service);
-	}
-	
-	public void KeywordDev(){
-		String service = "SemplestKeywordGeneratorService";
-		setServicePropsDev(service);
-	}
-	
-	public void KeywordTest(){
-		String service = "SemplestKeywordGeneratorService";
-		setServicePropsTest(service);
-	}
-	
-	public void KeywordExp(){
-		try{
-			String path = "C:\\SemplestKeywordGeneratorService\\bin\\system.properties";
-			Properties properties = new Properties();
-			FileInputStream in = new FileInputStream(path);
-			properties.load(in);
-			in.close();		
-			
-			String jdbc = "jdbc:jtds:sqlserver://172.18.9.23/semplest_testing";
-			String comment = "Keyword Service for Exp ESB. Updated by InstallationSetup (Nan).";
-			writeProps(path, properties, jdbc, comment);
-		}
-		catch(FileNotFoundException e){
-			//file not found. probably the service is not installed.
-			//do nothing
-		}
-		catch(IOException e){
-			//do nothing
-		}
-	}
-	
-	public void setServicePropsDev(String serviceName){		
+	public void setProps(String serviceName, String jdbc, String comment){
 		try{
 			String path = "C:\\" + serviceName + "\\bin\\system.properties";
 			Properties properties = new Properties();
@@ -212,66 +172,7 @@ public class InstallationSetup {
 			properties.load(in);
 			in.close();		
 			
-			String jdbc = "jdbc:jtds:sqlserver://172.18.9.23/semplest";
-			String comment = "Service on Dev Box. Updated by InstallationSetup (Nan).";
-			writeProps(path, properties, jdbc, comment);
-		}
-		catch(FileNotFoundException e){
-			//file not found. probably the service is not installed.
-			//do nothing
-		}
-		catch(IOException e){
-			//do nothing
-		}
-	}
-	
-	public void setServicePropsTest(String serviceName){		
-		try{
-			String path = "C:\\" + serviceName + "\\bin\\system.properties";
-			Properties properties = new Properties();
-			FileInputStream in = new FileInputStream(path);
-			properties.load(in);
-			in.close();		
-			
-			String jdbc = "jdbc:jtds:sqlserver://172.18.9.35/semplestTest";
-			String comment = "Service on Test Box. Updated by InstallationSetup (Nan).";
-			writeProps(path, properties, jdbc, comment);
-		}
-		catch(FileNotFoundException e){
-			//file not found. probably the service is not installed.
-			//do nothing
-		}
-		catch(IOException e){
-			//do nothing
-		}
-	}	
-	
-	public void setServicePropsExp(String serviceName){		
-		try{
-			String path = "C:\\" + serviceName + "\\bin\\system.properties";
-			Properties properties = new Properties();
-			FileInputStream in = new FileInputStream(path);
-			properties.load(in);
-			in.close();		
-			
-			String jdbc = "jdbc:jtds:sqlserver://172.18.9.23/semplest_testing";
-			String comment = "Service for Exp. Updated by InstallationSetup (Nan).";
-			writeProps(path, properties, jdbc, comment);
-		}
-		catch(FileNotFoundException e){
-			//file not found. probably the service is not installed.
-			//do nothing
-		}
-		catch(IOException e){
-			//do nothing
-		}
-	}
-	
-	public void writeProps(String filePath, Properties props, String jdbc, String comment){
-		
-		try{
-			
-			FileWriter out = new FileWriter(filePath);
+			FileWriter out = new FileWriter(path);
 			BufferedWriter writer = new BufferedWriter(out);
 			
 			Date now = new Date();
@@ -282,26 +183,65 @@ public class InstallationSetup {
 			writer.write("#Date: " + now.toString());
 			writer.newLine();
 			
-			writer.append("semplest.service" + " = " + props.getProperty("semplest.service"));
+			writer.append("semplest.service" + " = " + properties.getProperty("semplest.service"));
 			writer.newLine();
-			writer.append("YAJSW.servicename" + " = " + props.getProperty("YAJSW.servicename"));
+			writer.append("YAJSW.servicename" + " = " + properties.getProperty("YAJSW.servicename"));
 			writer.newLine();
-			writer.append("jdbc.driverClassName" + " = " + props.getProperty("jdbc.driverClassName"));
+			writer.append("jdbc.driverClassName" + " = " + properties.getProperty("jdbc.driverClassName"));
 			writer.newLine();
 			writer.append("jdbc.url" + " = " + jdbc);  //update the JDBC link
 			writer.newLine();
-			writer.append("jdbc.username" + " = " + props.getProperty("jdbc.username"));
+			writer.append("jdbc.username" + " = " + properties.getProperty("jdbc.username"));
 			writer.newLine();
-			writer.append("jdbc.password" + " = " + props.getProperty("jdbc.password"));
+			writer.append("jdbc.password" + " = " + properties.getProperty("jdbc.password"));
 			writer.newLine();
 			
 			writer.close();
-		
 		}
-		catch(Exception e){
+		catch(FileNotFoundException e){
+			//file not found. probably the service is not installed.
 			//do nothing
 		}
+		catch(IOException e){
+			//do nothing
+		}
+	}
+	
+	public void init(){
+		//for DEV box
+		ServerConfiguration devConfig = new ServerConfiguration(DevJdbc, DevEsbServer, DevServiceServer, DevKeywordServer);
+		config.put(SERVER_BOX.DEV, devConfig);
 		
+		//for TEST box
+		ServerConfiguration testConfig = new ServerConfiguration(TestJdbc, TestEsbServer, TestServiceServer, TestKeywordServer);
+		config.put(SERVER_BOX.TEST, testConfig);
+		
+		//for EXP box
+		ServerConfiguration expConfig = new ServerConfiguration(ExpJdbc, ExpEsbServer, ExpServiceServer, ExpKeywordServer);
+		config.put(SERVER_BOX.EXP, expConfig);
+
+		//for UAT box
+		ServerConfiguration uatConfig = new ServerConfiguration(UatJdbc, UatEsbServer, UatServiceServer, UatKeywordServer);
+		config.put(SERVER_BOX.UAT, uatConfig);
+		
+	}
+	
+	public class ServerConfiguration{
+		public String jdbc;
+		public String esbServer;
+		public String serviceServer;
+		public String keywordServer;
+		
+		public ServerConfiguration(){}
+		
+		public ServerConfiguration(String jdbc, String esbServer,
+				String serviceServer, String keywordServer) {
+			super();
+			this.jdbc = jdbc;
+			this.esbServer = esbServer;
+			this.serviceServer = serviceServer;
+			this.keywordServer = keywordServer;
+		}		
 	}
 	
 }
