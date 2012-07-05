f93d981d080e3da943a9c7d6cb50f9fe90bfbc12
raj <raj@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-05 16:00:13 +0000

    hashMap to Map
    git-svn-id: file:///semplestb/final_repo@3017 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestKeywords/src/semplest/keywords/javautils/vsUtils.java
+++ SemplestKeywords/src/semplest/keywords/javautils/vsUtils.java
@@ -26,60 +26,83 @@ public class vsUtils {
 
   // - static interface -------------------------------------------------------
   // length of the vector (L2 norm)
-  public static Integer cLen( HashMap<String,Integer> wc){
+  public static Integer cLen( Map<String,Integer> wc){
     return (int) Math.round( Math.sqrt( sSquares( wc ) ) ); }
   // normalize vector to UNIT length (L2 norm) 
-  public static Double cNorm( HashMap<String,Integer> wc){
+  public static Double cNorm( Map<String,Integer> wc){
     return ( UNIT * 1.0 / Math.max( Math.sqrt( sSquares( wc ) ), 1.0 ));}
-  public static HashMap<String,Integer> cNormalize(HashMap<String,Integer> wc){
-    HashMap<String,Integer> omap = new HashMap<String,Integer>();
+  public static Map<String,Integer> cNormalize(Map<String,Integer> wc){
+    if( null == wc ) return null;
+    Map<String,Integer> omap = new HashMap<String,Integer>();
     Double w = cNorm( wc );
     for( Map.Entry<String,Integer> e: wc.entrySet() )
       omap.put( e.getKey(), (int) Math.round( e.getValue() * w ));
     return omap;
   }
+  // cosine distance between two vectors 
+  // [Note:] we check intersection rather than equality of keywords (as ngram)
+  public static Double cDist( Map<String,Integer> a,
+      Map<String,Integer> b){
+    Map<String,Integer> na = ngWc( cNormalize( a ));
+    Map<String,Integer> nb = ngWc( cNormalize( b ));
+    Double dotp = 0.0;
+    Set<String> common = na.keySet();
+    common.retainAll( nb.keySet() );
+    for( String w: common)
+      dotp += na.get( w ) * nb.get( w );
+    return Math.acos( dotp / (UNIT * UNIT * 1.0) );
+  }
+
+  // - add vectors together --------------------------------------------------
   // add together a collection of vectors (wc) 
-  public static HashMap<String,Integer> cAdd( HashMap<String, 
-      HashMap<String,Integer>>  wcs){
-    HashMap<String,Integer> omap = new HashMap<String,Integer>();
-    for( Map.Entry<String,HashMap<String,Integer>> wc: wcs.entrySet() ) 
+  public static Map<String,Integer> cAdd( Map<String, 
+      Map<String,Integer>>  wcs){
+    Map<String,Integer> omap = new HashMap<String,Integer>();
+    for( Map.Entry<String,Map<String,Integer>> wc: wcs.entrySet() ) 
       for( Map.Entry<String,Integer> e: wc.getValue().entrySet() )
         omap.put( e.getKey(), (omap.containsKey(e.getKey()) ? 
             omap.get(e.getKey()) : 0) + e.getValue());  
     return omap;
   }
-  // cosine distance between two vectors 
-  // [Note:] we check intersection rather than equality of keywords (as ngram)
-  public static Double cDist( HashMap<String,Integer> a,
-      HashMap<String,Integer> b){
-    HashMap<List<String>,Integer> na = tokenize( cNormalize( a ));
-    HashMap<List<String>,Integer> nb = tokenize( cNormalize( b ));
-    Double dotp = 0.0;
-    for( Map.Entry<List<String>,Integer> ae: na.entrySet() )
-      for( Map.Entry<List<String>,Integer> be: nb.entrySet() ){
-        ae.getKey().retainAll( be.getKey());
-        if( ae.getKey().size() > 0)
-          dotp += ae.getValue() * be.getValue();
-      }
-    return Math.acos( dotp / (UNIT * UNIT * 1.0) );
+  // weighted add
+  public static Map<String,Integer> cAdd( Map<String, 
+      Map<String,Integer>>  wcs, Map<String,Double> wts){
+    Map<String,Integer> omap = new HashMap<String,Integer>();
+    for( Map.Entry<String,Map<String,Integer>> wc: wcs.entrySet() ) 
+      for( Map.Entry<String,Integer> e: wc.getValue().entrySet() )
+        if( wts.containsKey( e.getKey())) {
+        Double wt = wts.get( e.getKey());
+        Integer cv = omap.containsKey(e.getKey()) ? omap.get(e.getKey()) : 0;
+        Integer nv = cv + (int)Math.round( e.getValue()*wts.get( e.getKey()));
+        omap.put( e.getKey(), nv );
+      } 
+      return omap;
   }
-  // break up ngram Hashmap into collection-of-words Hashmap
-  private static HashMap<List<String>,Integer> tokenize( 
-      HashMap<String,Integer> m){
-    HashMap<List<String>,Integer> res = new HashMap<List<String>,Integer>();
-    for( Map.Entry<String,Integer> e: m.entrySet() )
-      res.put( new ArrayList( Arrays.asList(e.getKey().split("\\+"))), 
-          e.getValue()); 
-    return res;
+  // sum normalizes before adding
+  public static Map<String,Integer> cSum( Map<String, 
+      Map<String,Integer>>  wcs){
+    Map<String,Integer> omap = new HashMap<String,Integer>();
+    for( Map.Entry<String,Map<String,Integer>> wc: wcs.entrySet() ){ 
+      Map<String,Integer> m = wc.getValue();
+      Map<String,Integer> n = cNormalize( m ); 
+      for( Map.Entry<String,Integer> e: n.entrySet() ){
+        omap.put( e.getKey(), (omap.containsKey(e.getKey()) ? 
+            omap.get(e.getKey()) : 0) + e.getValue()); 
+      }
+    }
+    return cNormalize( omap );
   }
 
   // Combine a bunch of vectors (wcs), based on their distance to a reference wc
-  public static HashMap<String,Integer> cCombine( 
-      HashMap<String, HashMap<String,Integer>> wcs, HashMap<String,Integer> rv){
-    HashMap<String,Double> omap = new HashMap<String,Double>();
-    for( Map.Entry<String,HashMap<String,Integer>> wc: wcs.entrySet() ) {
-      Double w = Math.PI/2.0 - cDist( wc.getValue(),rv );  // the weight
-      HashMap<String,Integer> nwc = cNormalize( wc.getValue() );
+  public static Map<String,Integer> cCombine( 
+      Map<String, Map<String,Integer>> wcs, Map<String,Integer> rv){
+    Map<String,Double> omap = new HashMap<String,Double>();
+    for( Map.Entry<String,Map<String,Integer>> wc: wcs.entrySet() ) {
+      Map<String,Integer> swc = ngWc( wc.getValue() );     // ngram-ct to word-ct
+      Double w = Math.PI/2.0 - cDist( swc,rv );  // the weight
+      System.out.printf("%s :: wt: %.3f sizes(%d,%d)\n", 
+          wc.getKey(), w*100.0, swc.size(), rv.size() );
+      Map<String,Integer> nwc = cNormalize( wc.getValue() );
       for( Map.Entry<String,Integer> e: nwc.entrySet() )
         if( omap.containsKey( e.getKey() ) )
           omap.put( e.getKey(), omap.get( e.getKey() ) + e.getValue() * w );
@@ -87,43 +110,64 @@ public class vsUtils {
           omap.put( e.getKey(), e.getValue() * w );
     }
     // convert to int, normalize, return
-    HashMap<String,Integer> rmap = new HashMap<String,Integer>();
+    Map<String,Integer> rmap = new HashMap<String,Integer>();
     for( Map.Entry<String,Double> e: omap.entrySet() )
       rmap.put( e.getKey(), (int) Math.round( e.getValue()) );
 
     return cNormalize( rmap );
   }
+  // Combine a bunch of vectors (wcs), based on their distance to a reference url 
+  public static Map<String,Integer> cCombine( 
+    Map<String, Map<String,Integer>> wcs, String rurl){
+    String urltext = "";
+    try {
+      urltext = TextUtils.HTMLText( rurl );
+    } catch (Exception e){ e.printStackTrace();}
+    return cCombine( wcs, cWc( urltext )); 
+  }
+  // - Utilities -------------------------------------------
+  // Ngrams 
+  // Return individual word-count from ngram word-count
+  public static Map<String,Integer> ngWc( Map<String,Integer> ngwcs){
+    Map<String,Integer> omap = new HashMap<String,Integer>();
+    for( Map.Entry<String,Integer> e : ngwcs.entrySet() )
+      for( String w: e.getKey().split("\\+")){
+        int cur = omap.containsKey( w ) ? omap.get( w ) : 0; 
+        omap.put( w, cur +  e.getValue()); 
+      }
+    return omap;
+  }
   // returns map sorted by word-count in descending order
-  public static TreeSet sortMap( HashMap<String,Integer> wc ){
-    TreeSet<Map.Entry<String,Integer>> tm = new TreeSet<Map.Entry<String,Integer>>(
-        new Comparator<Map.Entry<String,Integer>>(){
-          @Override public int compare( Map.Entry<String,Integer> a, 
-            Map.Entry<String,Integer> b){ 
+  public static TreeSet sortMap( Map<String,Integer> wc ){
+    TreeSet<Map.Entry<String,Integer>> tm = 
+      new TreeSet<Map.Entry<String,Integer>>(
+          new Comparator<Map.Entry<String,Integer>>(){
+            @Override public int compare( Map.Entry<String,Integer> a, 
+              Map.Entry<String,Integer> b){ 
               return b.getValue().compareTo( a.getValue());}
           });
-    
+
     tm.addAll( wc.entrySet() );
     return tm;
   }
   // top n words of word-count (based on count)
-  public static String[] topWords( HashMap<String,Integer> wc, Integer n){
-    Set<Map.Entry<String,Integer>> rv = jUtils.take( sortMap( wc ), n );
-    Set<String> s = new HashSet<String>();
-    for( Map.Entry<String,Integer> e: rv )
-      s.add( e.getKey() );
-    return s.toArray( new String[]{} );
+  public static String[] topWords( Map<String,Integer> wc, Integer n){
+    Map<String,Integer> rv = jUtils.take( sortMap( wc ), n );
+    return rv.keySet().toArray( new String[]{} );
   }
   //------------------------------------------------------------------------
   // - private helpers -----------------------------------------------
-  private static Integer sSquares( HashMap<String,Integer> wc ){
+  private static Integer sSquares( Map<String,Integer> wc ){
     Integer res = 0;
     for( Map.Entry<String,Integer> e: wc.entrySet() )
-      res += e.getValue() * e.getValue();
+      if( e != null && e.getValue() != null )
+        res += e.getValue() * e.getValue();
+      else System.out.print(".");
     return res;
   }
 
   // - pretty printers ------------
-  private static void pWc( String label, HashMap<String,Integer> wc, int n ){
+  private static void pWc( String label, Map<String,Integer> wc, int n ){
     TreeSet<Map.Entry<String,Integer>> rv = sortMap( wc );
     System.out.print( label + " :: \t" );
     int count = 0;
@@ -136,41 +180,41 @@ public class vsUtils {
   }
 
   // - wc creator helpers ------------
-  private static HashMap<String,Integer> cWc(String[] words ){
-    HashMap<String,Integer> rv = new HashMap<String,Integer>();
+  private static Map<String,Integer> cWc(String[] words ){
+    Map<String,Integer> rv = new HashMap<String,Integer>();
     for( String w: words)
       rv.put( w, (rv.get(w) == null ? 0 : rv.get(w)) + 1 );
     return rv;
   }
-  private static HashMap<String,Integer> cWc(String words ){ 
+  private static Map<String,Integer> cWc(String words ){ 
     return cWc( words.split("\\s+") );
   }
   // ---------------------------------------------------------------------
   //- tests ---------------------------------------------------------------
   private static void test(){
-    HashMap<String,Integer> rv = cWc("a b c d");
-    HashMap<String,HashMap<String,Integer>> cs = 
-      new HashMap<String,HashMap<String,Integer>>();
+    Map<String,Integer> rv = cWc("a b c d");
+    Map<String,Map<String,Integer>> cs = 
+      new HashMap<String,Map<String,Integer>>();
     cs.put( "a0.5", cWc("a+x+y b+z+g c+g+h+u"));
     cs.put( "b0.0", cWc("a+f b+g c+g+o d+o+h"));
     cs.put( "c1.0", cWc("c+o d+h e+y"));
     cs.put( "d1.5", cWc("e+i f+h g+j"));
 
-    HashMap<String,Integer> cc = cCombine( cs, rv );
+    Map<String,Integer> cc = cCombine( cs, rv );
     System.out.println( cc );
   }
 
   private static void Test(String ngramFile, String ancestor, String ref){
     // read in categories from file and pick out those with given ancestor
-    HashMap<String,HashMap<String,Integer>> wcs = catUtils.family( 
+    Map<String,Map<String,Integer>> wcs = catUtils.family( 
         ioUtils.readWcs( ngramFile ), ancestor ); 
-    
+
     // create a reference word-count
-    HashMap<String,Integer> rv = cWc( ref );
+    Map<String,Integer> rv = cWc( ref );
 
     // combine them (and remember how long it took)
     long start = System.currentTimeMillis();
-    HashMap<String,Integer> cc = cCombine( wcs, rv );
+    Map<String,Integer> cc = cCombine( wcs, rv );
     long end = System.currentTimeMillis();
 
     // print the top 50 words (by count) and the time it took to combine 
@@ -178,44 +222,48 @@ public class vsUtils {
     System.out.println( "Combining " +wcs.size()+" cats took "
         +(end-start)+ "ms");
   }
-  public static String[] generateNgrams(String basePath, String extension,  ArrayList<String> categories, String data, int numKw){
-	    // read in categories from file and pick out those with categories selected
-		  HashMap<String,HashMap<String,Integer>> wcs = new HashMap<String,HashMap<String,Integer>>();
-		  for(String cat : categories){
-			 //Building the nGramsFileName 	
-			 String[] subCat = cat.split("/");
-			 String file = basePath+subCat[1]+extension;
-			 HashMap<String,HashMap<String,Integer>> wcsAux = catUtils.family( 
-					 ioUtils.readWcs( file ), cat); 
-			 wcs.putAll(wcsAux);
-		  }
-	    
-	    // create a reference word-count
-		System.out.println(data.length());
-	    HashMap<String,Integer> rv = cWc( data );
+  //------------
+  public static String[] generateNgrams(String basePath, String extension,
+      ArrayList<String> categories, String data, int numKw){
+    // read in categories from file and pick out those with categories selected
+    Map<String,Map<String,Integer>> wcs = new HashMap<String,Map<String,
+    Integer>>();
+    for(String cat : categories){
+      //Building the nGramsFileName 	
+      String[] subCat = cat.split("/");
+      String file = basePath+subCat[1]+extension;
+      Map<String,Map<String,Integer>> wcsAux = catUtils.family( 
+          ioUtils.readWcs( file ), cat); 
+      wcs.putAll(wcsAux);
+    }
 
-	    // combine them (remember how long it took)
-	    Long start = System.currentTimeMillis();
-	    HashMap<String,Integer> cc = cCombine( wcs, rv );
-	    System.out.println("Combine Categories seconds " + (System.currentTimeMillis()-start));
-	    String [] kw = topWords( cc, numKw );
+    // create a reference word-count
+    System.out.println(data.length());
+    Map<String,Integer> rv = cWc( data );
 
-	    return kw;
+    // combine them (remember how long it took)
+    Long start = System.currentTimeMillis();
+    Map<String,Integer> cc = cCombine( wcs, rv );
+    System.out.println("Combine Categories seconds " + 
+        (System.currentTimeMillis()-start));
+    String [] kw = topWords( cc, numKw );
 
-}
+    return kw;
+
+  }
   //-------------------------------------------------------------
   public static void main (String[] args){
     String file = "/semplest/data/dmoz/multiwords/crawl2MSNVolFiltered/shopping.2";
     String head = "top/shopping/flowers/florists/north_america";
-    String rngs = "wedding+flowers flower+shops birthday+flowers" + 
-        "wedding+bouquets flower+delivery fruit+baskets " + 
-        "valentines+day flower+arrangements flowers+plants red+roses" + 
-        "fresh+flowers single+rose flower+delivery" ;
-    String rwords = "new york realestate autos " + 
-        "business finance people politics travel sports " + 
-        "wallstreet journal television radio" + 
-        "newyork christian science monitor" ;
-    Test( file, head, rngs );
+    String rngrams = "wedding+flowers flower+shops birthday+flowers" + 
+      "wedding+bouquets flower+delivery fruit+baskets " + 
+      "valentines+day flower+arrangements flowers+plants red+roses" + 
+      "fresh+flowers single+rose flower+delivery" ;
+    String rwords = "wedding flowers birthday " + 
+      "bouquets flower delivery fruit baskets " + 
+      "valentines day arrangements plants red roses" + 
+      "fresh single " ;
+    Test( file, head, rwords );
   }
 }
 
