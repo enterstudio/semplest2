798781e4c53f00877d021e2fa2bc6172102bee0c
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-06 22:02:23 +0000

    charting
    git-svn-id: file:///semplestb/final_repo@3072 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMPlestDBScripts/StoredProcs/GetMondayChart.sql
@@ -0,0 +1,19 @@
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.sp_UpdateKeywords') AND type in (N'P', N'PC'))
+  DROP PROCEDURE dbo.GetMondayChart;
+  
+CREATE PROCEDURE GetMondayChart(@StartDate DATETIME,@EndDate DATETIME) AS
+BEGIN TRY
+DECLARE @ErrMsg VARCHAR(250)
+    DECLARE @Table TABLE (Date DateTime,Name varchar(100))
+    INSERT INTO @Table EXEC dbo.GetMondays @StartDate,@EndDate
+    SELECT * FROM vwPromotionChart WHERE TransactionDate IN (SELECT Date FROM @Table)
+END TRY
+
+BEGIN CATCH
+	DECLARE @ErrMessage	nvarchar(4000),
+          @ErrorSeverity	int,
+          @ErrorState		int;
+	SELECT @ErrMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	RAISERROR (@ErrMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
+GO
---
+++ SEMPlestDBScripts/StoredProcs/GetMondays.sql
@@ -0,0 +1,37 @@
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.sp_UpdateKeywords') AND type in (N'P', N'PC'))
+  DROP PROCEDURE dbo.GetMondays;
+USE [semplest]
+GO
+/****** Object:  StoredProcedure [dbo].[GetMondays]    Script Date: 07/06/2012 17:47:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[GetMondays](@StartDate DATETIME,@EndDate DATETIME) AS
+BEGIN TRY
+DECLARE @ErrMsg VARCHAR(250);
+WITH cte AS (
+	SELECT 1 AS DayID,
+		@StartDate AS FromDate,
+		DATENAME(dw, @StartDate) AS Dayname
+	UNION ALL
+		SELECT cte.DayID + 1 AS DayID,
+		DATEADD(d, 1 ,cte.FromDate),
+		DATENAME(dw, DATEADD(d, 1 ,cte.FromDate)) AS Dayname
+	FROM cte
+	WHERE 
+		DATEADD(d,1,cte.FromDate) < @EndDate
+	)
+
+	SELECT FromDate AS Date, Dayname FROM CTE WHERE DayName LIKE 'Monday' -- For Monday
+OPTION (MaxRecursion 370)
+END TRY
+
+BEGIN CATCH
+	DECLARE @ErrMessage	nvarchar(4000),
+          @ErrorSeverity	int,
+          @ErrorState		int;
+	SELECT @ErrMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	RAISERROR (@ErrMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
+GO
