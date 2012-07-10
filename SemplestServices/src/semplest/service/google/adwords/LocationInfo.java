d7c0dc1d9433c5f8f4918383b383317b9393f7ac
raj <raj@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-10 17:38:25 +0000

    Added reach code.
    git-svn-id: file:///semplestb/final_repo@3135 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/service/google/adwords/LocationInfo.java
+++ SemplestServices/src/semplest/service/google/adwords/LocationInfo.java
@@ -6,18 +6,76 @@ import com.google.api.adwords.lib.AdWordsUser;
 import com.google.api.adwords.lib.AdWordsService;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.log4j.Logger;
 
 public class LocationInfo {
-  
-	private static final Logger logger = Logger.getLogger(LocationInfo.class);
-	
+
+  private static final Logger logger = Logger.getLogger(LocationInfo.class);
+
   static String[] fields = {"Id","LocationName","CanonicalName","DisplayType",
     "ParentLocations","Reach"};
   AdWordsUser user = null;
   LocationCriterionServiceInterface lcs = null; 
- 
+
+  public static Map<String,Integer> reach = new HashMap<String,Integer>();
+
+  static {
+    reach.put("AL",1390);
+    reach.put("AK",324);
+    reach.put("AZ",3970);
+    reach.put("AR",741);
+    reach.put("CA",27800);
+    reach.put("CO",4170);
+    reach.put("CT",1990);
+    reach.put("DE",347);
+    reach.put("FL",9880);
+    reach.put("GA",7500);
+    reach.put("HI",557);
+    reach.put("ID",648);
+    reach.put("IL",9790);
+    reach.put("IN",2310);
+    reach.put("IA",1190);
+    reach.put("KA",1170);
+    reach.put("KY",1490);
+    reach.put("LA",1890);
+    reach.put("ME",582);
+    reach.put("MD",3120);
+    reach.put("MA",5710);
+    reach.put("MI",5740);
+    reach.put("MN",5080);
+    reach.put("MS",637);
+    reach.put("MO",3350);
+    reach.put("MT",367);
+    reach.put("NE",865);
+    reach.put("NV",1280);
+    reach.put("NH",692);
+    reach.put("NJ",4949);
+    reach.put("NM",728);
+    reach.put("NY",13400);
+    reach.put("NC",5410);
+    reach.put("ND",254);
+    reach.put("OH",5780);
+    reach.put("OK",1170);
+    reach.put("OR",2500);
+    reach.put("PA",6200);
+    reach.put("RI",511);
+    reach.put("SC",1660);
+    reach.put("SD",245);
+    reach.put("TN",2650);
+    reach.put("TX",16600);
+    reach.put("UT",1490);
+    reach.put("VT",343);
+    reach.put("VA",4710);
+    reach.put("WA",4830);
+    reach.put("WV",455);
+    reach.put("WI",3680);
+    reach.put("WY",167);
+    reach.put("US",190000);
+  }
+
   // Interface -----------------
   public LocationInfo( String clid, AdWordsUser user ) throws Exception {
     this.user  = user; // GawUtils.getUser( clid,  );
@@ -31,6 +89,21 @@ public class LocationInfo {
       return gLid( fcr[0] );
     else return 0L;
   }
+  // - reach -------
+  // The reach (in 000 people)/(% of the US) for a state/states
+  public static int reachK( String state){ 
+    if( reach.containsKey( state.toUpperCase() ))
+      return reach.get( state.toUpperCase() );
+    else return 0;
+  }
+  public static int reachPctUS( String state ){ return 100*reachK(state)/reachK("US");} 
+  public static int reachK( String[] states){ 
+    int treach = 0;
+    for( String state: states ) 
+      treach += reachK( state );
+    return treach;
+  }
+  public static int reachPctUS( String[] states){return 100*reachK(states)/reachK("US");} 
 
   // - getters ----------------------------------
   private LocationCriterion[] getCriteria( String l) throws Exception {
@@ -41,7 +114,7 @@ public class LocationInfo {
     Predicate lp = new Predicate("Locale",PredicateOperator.EQUALS, 
         new String[]{ "en" });
     s.setPredicates( new Predicate[]{sp,lp});
-  
+
     // get CampaignCriterions
     return lcs.get( s );
   }
@@ -49,8 +122,8 @@ public class LocationInfo {
   private LocationCriterion[] filter(LocationCriterion[] ls, String t){
     ArrayList<LocationCriterion> flc = new ArrayList<LocationCriterion>();
     for( LocationCriterion l: ls )
-     if(l instanceof LocationCriterion && 
-         l.getLocation().getDisplayType().equals(t)) flc.add( l );
+      if(l instanceof LocationCriterion && 
+          l.getLocation().getDisplayType().equals(t)) flc.add( l );
     return flc.toArray( new LocationCriterion[]{} );
   }
   private String gLs( LocationCriterion lc ){
@@ -65,9 +138,7 @@ public class LocationInfo {
       return lc.getLocation().getId();
     else return 0L;
   }
-
-  // ---------------------------------------------------------------------------
-  public static void main(String[] args) {
+  public static void stateTest(){
     try {
       AdWordsUser user = null;
       Long caid = Long.getLong( "" );
@@ -78,6 +149,18 @@ public class LocationInfo {
 
       System.out.println( sid + "," + cid + "," + mid );
     } catch (Exception e ){ logger.error("Problem", e);}
+  }
+
+  // ---------------------------------------------------------------------------
+  public static void main(String[] args) {
+    System.out.println( reachK( "NY") );
+    System.out.println( reachK( "ct") );
+    System.out.println( reachK( "foo") );
+    System.out.println( reachPctUS( "NY") );
+    System.out.println( reachPctUS( "foo") );
+    System.out.println( reachK( "ny ct ma".split("\\s+" )));
+    System.out.println( reachPctUS( "ny ct ma".split("\\s+" )));
+
   }
 }
 
