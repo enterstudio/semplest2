870cb55b6d255411d3c42c0ced11aa5f55a87346
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-11 15:15:32 +0000

    git-svn-id: file:///semplestb/final_repo@3160 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestSystemMonitor/src/semplest/systemmonitor/BiddingMonitorThread.java
@@ -0,0 +1,67 @@
+package semplest.systemmonitor;
+
+import semplest.server.protocol.ProtocolEnum.ServiceStatus;
+import semplest.services.client.api.SemplestBiddingServiceClient;
+import semplest.systemmonitor.MonitorObject.SERVER;
+import semplest.systemmonitor.MonitorObject.SERVICE;
+
+public class BiddingMonitorThread implements Runnable {
+	
+	private SERVICE service = SERVICE.Bidding;	
+	private MonitorObject monitor = new MonitorObject();	
+	private SystemMonitorDB db = new SystemMonitorDB();
+	
+	@Override
+	public void run() {		
+		while(!monitor.isStop()){	
+			try {
+				monitor.loadConfig();
+				System.out.println(monitor.isStop());
+				
+				for(SERVER server : SERVER.values()){
+					if(monitor.isMonitorServer(server)){
+						String esbUrl = monitor.getServerEsbUrl(server);
+						SemplestBiddingServiceClient biddingClient = new SemplestBiddingServiceClient(esbUrl, monitor.getClientTimeout());
+						
+						try {
+							String ret = biddingClient.checkStatus(monitor.getClientTimeout());
+							
+							if(ret.equals(ServiceStatus.Up.getServiceStatusValue())){
+								System.out.println(service.name() + " is running fine on " + server.name());
+								ServiceStatus status = ServiceStatus.Up;
+								monitor.setServerServiceStatus(server, status);
+								db.storeServerServiceStatus(server, service, status);
+								if(monitor.isServerServiceUp(server)){
+									//if the service used to be down, but just came back up, send notification
+									Notification.sendNotification(server, service, null);
+								}
+							}
+							else{							
+								onError(server, new Exception("Service Status - " + ret));
+							}
+						} 
+						catch (Exception e) {
+							onError(server, e);
+						}
+					}						
+				}				
+				
+				Thread.sleep(monitor.getSleepTime());	
+			} catch (Exception e) {
+				e.printStackTrace();
+			}	
+		}			
+	}		
+	
+	private void onError(SERVER server, Exception e){
+		System.out.println(service.name() + " is not running on " + server.name() + " - " + e.getMessage());						
+		//The service is not healthy
+		ServiceStatus status = ServiceStatus.Down;
+		monitor.setServerServiceStatus(server, status);		
+		db.storeServerServiceStatus(server, service, status);
+		if(monitor.isServerServiceDown(server)){
+			//if the service just went down. send notification
+			Notification.sendNotification(server, service, e.getMessage());
+		}
+	}
+}
---
+++ SemplestSystemMonitor/src/semplest/systemmonitor/MonitorObject.java
@@ -0,0 +1,121 @@
+package semplest.systemmonitor;
+
+import java.util.HashMap;
+import semplest.server.protocol.ProtocolEnum.ServiceStatus;
+
+public class MonitorObject {
+	
+	public static enum SERVER {DEV, TEST, PROD};
+	public static enum SERVICE {AdEngine, Bidding, Keyword, Mail, Scheduler};	
+	
+	private HashMap<SERVER, Status> serverServiceStatus = new HashMap<SERVER, Status>();
+	private HashMap<SERVER, Config> serverConfig = new HashMap<SERVER, Config>();
+	
+	private boolean isStop;
+	private int sleepTime;
+	private String clientTimeout;
+	private Configuration config = new Configuration();
+	
+	public MonitorObject() {
+		for(SERVER server : SERVER.values()){
+			serverServiceStatus.put(server, new Status());
+		}
+		for(SERVER server : SERVER.values()){
+			serverConfig.put(server, new Config());
+		}
+	}
+	
+	public void setServerServiceStatus(SERVER server, ServiceStatus status){		
+		Status nowStatus = serverServiceStatus.get(server);
+		nowStatus.setStatus(status);		
+		serverServiceStatus.put(server, nowStatus);
+	}
+	
+	public ServiceStatus getServerServiceStatus(SERVER server){
+		return serverServiceStatus.get(server).getStatus();
+	}
+	
+	public boolean isMonitorServer(SERVER server){
+		return serverConfig.get(server).isMonitor;
+	}
+	
+	public String getServerEsbUrl(SERVER server){
+		return serverConfig.get(server).esbUrl;
+	}
+	
+	public boolean isServerServiceDown(SERVER server){
+		return serverServiceStatus.get(server).goesDown();
+	}
+	
+	public boolean isServerServiceUp(SERVER server){
+		return serverServiceStatus.get(server).goesUp();
+	}
+
+	public boolean isStop() {
+		return isStop;
+	}
+
+	public int getSleepTime() {
+		return sleepTime;
+	}
+
+	public String getClientTimeout() {
+		return clientTimeout;
+	}
+	
+	public void loadConfig(){		
+		config.loadConiguration();
+		
+		isStop = config.isStop();
+		
+		sleepTime = config.getMonitorInterval() * 60 * 1000;
+		clientTimeout = config.getClientTimeout();		
+		
+		serverConfig.get(SERVER.DEV).esbUrl = config.getDevEsbUrl();
+		serverConfig.get(SERVER.TEST).esbUrl = config.getTestEsbUrl();
+		serverConfig.get(SERVER.PROD).esbUrl = config.getProdEsbUrl();
+		
+		serverConfig.get(SERVER.DEV).isMonitor = config.isMonitorDev();
+		serverConfig.get(SERVER.TEST).isMonitor = config.isMonitorTest();
+		serverConfig.get(SERVER.PROD).isMonitor = config.isMonitorProd();
+	}
+	
+	//Classes
+	private class Status{
+		private ServiceStatus prevStatus;
+		private ServiceStatus currStatus;
+		
+		public Status(){
+			prevStatus = ServiceStatus.Up;
+			currStatus = ServiceStatus.Up;
+		}
+		
+		public void setStatus(ServiceStatus status){
+			this.prevStatus = this.currStatus;
+			this.currStatus = status;
+		}
+		
+		public ServiceStatus getStatus(){
+			return currStatus;
+		}
+		
+		public boolean goesDown(){
+			if(prevStatus == ServiceStatus.Up && currStatus == ServiceStatus.Down){
+				return true;
+			}
+			return false;
+		}
+		
+		public boolean goesUp(){
+			if(prevStatus == ServiceStatus.Down && currStatus == ServiceStatus.Up){
+				return true;
+			}
+			return false;
+		}
+	}
+	
+	private class Config{
+		public String esbUrl;
+		public boolean isMonitor;
+	}
+}
