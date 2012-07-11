96cb69fc537e2035ffb02266298fcea648d1ac3e
lluis <lluis@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-11 17:20:58 +0000

    git-svn-id: file:///semplestb/final_repo@3170 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/test/SpentAPIReport.java
+++ SemplestServices/src/semplest/test/SpentAPIReport.java
@@ -40,238 +40,239 @@ public class SpentAPIReport {
 		String methodName;
 		Long methodUnits;
 		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-		Date startDate = df.parse("06/16/2012");
+		Date startDate = df.parse("07/01/2012");
+		Date endDate = new Date();
 		System.out.println(startDate);
-		System.out.println(new Date());
+		System.out.println(endDate);
 		Long totalUnits = google.getSpentAPIUnitsPerAccountID(accountId, startDate, startDate);
 		System.out.println("Total Units :"+totalUnits);
 		
 		//Method 1
 		serviceName = "CampaignCriterionService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 1
 		serviceName = "CampaignCriterionService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 2
 		serviceName = "AdGroupCriterionService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 2
 		serviceName = "AdGroupCriterionService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 3
 		serviceName = "CampaignService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 3
 		serviceName = "CampaignService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 4
 		serviceName = "CampaignTargetService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 4
 		serviceName = "CampaignTargetService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 5
 		serviceName = "AdGroupService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 5
 		serviceName = "AdGroupService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 6
 		serviceName = "AdGroupAdService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 6
 		serviceName = "AdGroupAdService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 7
 		serviceName = "AdParamService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 7
 		serviceName = "AdParamService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 8
 		serviceName = "CampaignCriterionService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 8
 		serviceName = "CampaignCriterionService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 9
 		serviceName = "DataService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 9
 		serviceName = "DataService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 10
 		serviceName = "UserListService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 10
 		serviceName = "UserListService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 11
 		serviceName = "TrafficEstimatorService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 12
 		serviceName = "TargetingIdeaService";
 		methodName = "getBulkKeywordIdeas";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 12
 		serviceName = "TargetingIdeaService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 13
 		serviceName = "ReportDefinitionService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 13
 		serviceName = "ReportDefinitionService";
 		methodName = "getReportFields";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 14
 		serviceName = "AlertService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 15
 		serviceName = "CustomerSyncService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 16
 		serviceName = "InfoService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 17
 		serviceName = "ServicedAccountService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 18
 		serviceName = "MutateJobService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 	
 		//Method 18
 		serviceName = "MutateJobService";
 		methodName = "getResult";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 18
 		serviceName = "MutateJobService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 19
 		serviceName = "BulkMutateJobService";
 		methodName = "mutate";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 19
 		serviceName = "BulkMutateJobService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 20
 		serviceName = "GeoLocationService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 21
 		serviceName = "ConstantDataService";
 		methodName = "getCarrierCriterion";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 21
 		serviceName = "ConstantDataService";
 		methodName = "getLanguageCriterion";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 		
 		//Method 21
 		serviceName = "LocationCriterionService";
 		methodName = "get";
-		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  startDate);
+		methodUnits = google.getSpentAPIUnitsPerMethodAndAccountID(accountId, serviceName, methodName, startDate,  endDate);
 		System.out.println("Total Units service("+serviceName+") method("+methodName +") : "+methodUnits);
 	}
 
