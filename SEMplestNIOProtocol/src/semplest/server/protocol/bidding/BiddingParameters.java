8df9c0abd16bc6840353ebfda1b20db59def6d5e
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-16 16:45:14 +0000

    Bidding params object
    git-svn-id: file:///semplestb/final_repo@3253 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SEMplestNIOProtocol/src/semplest/server/protocol/bidding/BiddingParameters.java
@@ -0,0 +1,42 @@
+package semplest.server.protocol.bidding;
+
+public class BiddingParameters
+{
+	private Integer SemplestBiddingBudgetMultFactor;
+	private Float SemplestBiddingInitialBidBoostFactor;
+	private Integer SemplestBiddingPercentileValue;
+	private Float SemplestBiddingMarginFactor;
+	public Integer getSemplestBiddingBudgetMultFactor()
+	{
+		return SemplestBiddingBudgetMultFactor;
+	}
+	public void setSemplestBiddingBudgetMultFactor(Integer semplestBiddingBudgetMultFactor)
+	{
+		SemplestBiddingBudgetMultFactor = semplestBiddingBudgetMultFactor;
+	}
+	public Float getSemplestBiddingInitialBidBoostFactor()
+	{
+		return SemplestBiddingInitialBidBoostFactor;
+	}
+	public void setSemplestBiddingInitialBidBoostFactor(Float semplestBiddingInitialBidBoostFactor)
+	{
+		SemplestBiddingInitialBidBoostFactor = semplestBiddingInitialBidBoostFactor;
+	}
+	public Integer getSemplestBiddingPercentileValue()
+	{
+		return SemplestBiddingPercentileValue;
+	}
+	public void setSemplestBiddingPercentileValue(Integer semplestBiddingPercentileValue)
+	{
+		SemplestBiddingPercentileValue = semplestBiddingPercentileValue;
+	}
+	public Float getSemplestBiddingMarginFactor()
+	{
+		return SemplestBiddingMarginFactor;
+	}
+	public void setSemplestBiddingMarginFactor(Float semplestBiddingMarginFactor)
+	{
+		SemplestBiddingMarginFactor = semplestBiddingMarginFactor;
+	}
+
+}
