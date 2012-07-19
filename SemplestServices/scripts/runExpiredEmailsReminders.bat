5c625c637d2a84abda6f81c0fca9e12c9c3bde18
julian <julian@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-19 16:37:54 +0000

    initial commit
    git-svn-id: file:///semplestb/final_repo@3335 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestServices/scripts/runExpiredEmailsReminders.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo JAVA_HOME: %JAVA_HOME%
+echo CLASSPATH: %CLASSPATH%
+
+java semplest.server.job.ExpiredEmailSender
+
---
+++ SemplestServices/src/semplest/server/job/ExpiredEmailSender.java
@@ -0,0 +1,13 @@
+package semplest.server.job;
+
+import org.apache.log4j.Logger;
+
+public class ExpiredEmailSender
+{
+	private static final Logger log = Logger.getLogger(ExpiredEmailSender.class);
+	
+	public static void main(final String[] args)
+	{
+		log.info("Hello!");
+	}
+}
