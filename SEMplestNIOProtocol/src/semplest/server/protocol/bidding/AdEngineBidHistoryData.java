82a9ec1beb7404a50afbbeec06f53f44356c5d17
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-24 16:09:29 +0000

    added hashCode() and equals()
    git-svn-id: file:///semplestb/final_repo@3434 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/server/protocol/bidding/AdEngineBidHistoryData.java
+++ SEMplestNIOProtocol/src/semplest/server/protocol/bidding/AdEngineBidHistoryData.java
@@ -126,5 +126,96 @@ public class AdEngineBidHistoryData {
 				+ ", startDate=" + startDate + ", endDate=" + endDate
 				+ ", transactionDate=" + transactionDate + "]";
 	}
+	@Override
+	public int hashCode() { // kept transactionDate out
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((adEngine == null) ? 0 : adEngine.hashCode());
+		result = prime * result + ((avgBid == null) ? 0 : avgBid.hashCode());
+		result = prime * result + ((avgCPC == null) ? 0 : avgCPC.hashCode());
+		result = prime * result + ((clicks == null) ? 0 : clicks.hashCode());
+		result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
+		result = prime * result
+				+ ((impressions == null) ? 0 : impressions.hashCode());
+		result = prime * result + ((keyword == null) ? 0 : keyword.hashCode());
+		result = prime * result
+				+ ((matchType == null) ? 0 : matchType.hashCode());
+		result = prime * result
+				+ ((position == null) ? 0 : position.hashCode());
+		result = prime * result
+				+ ((searchVol == null) ? 0 : searchVol.hashCode());
+		result = prime * result
+				+ ((startDate == null) ? 0 : startDate.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) { // kept transactionDate out
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AdEngineBidHistoryData other = (AdEngineBidHistoryData) obj;
+		if (adEngine == null) {
+			if (other.adEngine != null)
+				return false;
+		} else if (!adEngine.equals(other.adEngine))
+			return false;
+		if (avgBid == null) {
+			if (other.avgBid != null)
+				return false;
+		} else if (!avgBid.equals(other.avgBid))
+			return false;
+		if (avgCPC == null) {
+			if (other.avgCPC != null)
+				return false;
+		} else if (!avgCPC.equals(other.avgCPC))
+			return false;
+		if (clicks == null) {
+			if (other.clicks != null)
+				return false;
+		} else if (!clicks.equals(other.clicks))
+			return false;
+		if (endDate == null) {
+			if (other.endDate != null)
+				return false;
+		} else if (!endDate.equals(other.endDate))
+			return false;
+		if (impressions == null) {
+			if (other.impressions != null)
+				return false;
+		} else if (!impressions.equals(other.impressions))
+			return false;
+		if (keyword == null) {
+			if (other.keyword != null)
+				return false;
+		} else if (!keyword.equals(other.keyword))
+			return false;
+		if (matchType == null) {
+			if (other.matchType != null)
+				return false;
+		} else if (!matchType.equals(other.matchType))
+			return false;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (searchVol == null) {
+			if (other.searchVol != null)
+				return false;
+		} else if (!searchVol.equals(other.searchVol))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
