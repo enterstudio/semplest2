028dfa159796468393066265c0d24d9a248e345d
subhojit <subhojit@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-07-26 16:38:34 +0000

    commented resetCampaingn method call in main() to avoid any accidental campaign reset incident
    git-svn-id: file:///semplestb/final_repo@3500 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/service/bidding/BidObject.java
+++ SemplestServices/src/semplest/service/bidding/BidObject.java
@@ -974,7 +974,7 @@ public class BidObject
 			//bidObject.setBidsUpdate(promotionID, searchEngine, budgetData);
 			//bidObject.setBidsInitialWeek(promotionID, searchEngine, budgetData);
 			
-			bidObject.resetCampaign(promotionID, searchEngine);
+			//bidObject.resetCampaign(promotionID, searchEngine);
 			
 			
 
@@ -1017,12 +1017,12 @@ public class BidObject
 			}
 			*/
 			
-//			List<BidElement> bidData = SemplestDB.getLatestBids(promotionID, searchEngine);
-//			System.out.println(bidData.size());
-//
-//			for(BidElement b : bidData){
-//				System.out.println(b);
-//			}
+			List<BidElement> bidData = SemplestDB.getLatestBids(promotionID, searchEngine);
+			System.out.println(bidData.size());
+
+			for(BidElement b : bidData){
+				System.out.println(b);
+			}
 		
 
 			
