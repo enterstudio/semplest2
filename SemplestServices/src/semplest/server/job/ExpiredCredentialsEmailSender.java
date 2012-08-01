874f6e220d49cab7410f125ae7bfcb464ae052e2
julian <julian@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-01 19:34:16 +0000

    git-svn-id: file:///semplestb/final_repo@3606 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestServices/src/semplest/server/job/ExpiredCredentialsEmailSender.java
+++ SemplestServices/src/semplest/server/job/ExpiredCredentialsEmailSender.java
@@ -150,7 +150,7 @@ public class ExpiredCredentialsEmailSender
 		try
 		{
 			log.info("Starting process for sending reminder emails to customers who have not finished registering");
-			PropertyConfigurator.configure("log4j_server.properties");
+			PropertyConfigurator.configure("C:/SemplestAdengineService/properties/log4j_server.properties");
 			BasicConfigurator.configure();	
 			final ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("Service.xml");
 			Object object = new Object();
