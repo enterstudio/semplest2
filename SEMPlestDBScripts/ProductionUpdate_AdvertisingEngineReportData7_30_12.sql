cb0b487f7b9ff3bb9ece4f86478c8bc182a1e7c4
julian <julian@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-06 18:22:26 +0000

    added change for GetKeywordForAdEngine where no longer filtering out items with IsDeleted=true
    git-svn-id: file:///semplestb/final_repo@3654 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMPlestDBScripts/ProductionUpdate_AdvertisingEngineReportData7_30_12.sql
+++ SEMPlestDBScripts/ProductionUpdate_AdvertisingEngineReportData7_30_12.sql
@@ -581,3 +581,51 @@ GO
 
 
 
+USE [semplestTest]
+GO
+/****** Object:  StoredProcedure [dbo].[GetKeywordForAdEngine]    Script Date: 08/06/2012 14:19:41 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- +----------------------------------------------------------------------------------------------------------------+
+-- | Author  - Mitch                                                                                                |
+-- | Written - 4/20/2012																							|
+-- | Parms   - 																							|
+-- | Purpose - 						|
+-- +----------------------------------------------------------------------------------------------------------------+
+
+ALTER PROCEDURE [dbo].[GetKeywordForAdEngine]
+(
+	@PromotionPK            INT,
+	@IsTargetGoogle bit = 0,
+	@IsTargetMSN bit = 0
+)
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+	DECLARE @ErrMsg VARCHAR(250)
+
+	--validate data
+	IF NOT EXISTS (select * from Promotion p where p.PromotionPK =@PromotionPK)
+	BEGIN
+		SELECT @ErrMsg = 'The Promotion was not found.'; 
+		RAISERROR (@ErrMsg, 16, 1);
+	END;
+	--get general info
+	
+	select k.KeywordPK, k.Keyword, pka.IsTargetGoogle, pka.IsTargetMSN, pka.IsActive, pka.IsDeleted, pka.IsNegative, pka.SemplestProbability
+	from Promotion p 
+	inner join PromotionKeywordAssociation pka on pka.PromotionFK = p.PromotionPK
+	inner join Keyword k on k.KeywordPK = pka.KeywordFK
+	where p.PromotionPK = @PromotionPK and (pka.IsTargetGoogle = @IsTargetGoogle or pka.IsTargetMSN = @IsTargetMSN) and pka.IsActive = 1
+	
+END TRY
+BEGIN CATCH
+	DECLARE @ErrMessage	nvarchar(4000),
+          @ErrorSeverity	int,
+          @ErrorState		int;
+	SELECT @ErrMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	RAISERROR (@ErrMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
