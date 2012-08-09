5c80dc72485e18ee1a9fdc1f64b2c8903becb091
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-09 18:18:16 +0000

    git-svn-id: file:///semplestb/final_repo@3731 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestTests/src/semplest/tools/GeneralDB.java
@@ -0,0 +1,26 @@
+package semplest.tools;
+
+import java.util.HashMap;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import semplest.server.service.springjdbc.BaseDB;
+
+public class GeneralDB extends BaseDB{
+	
+	private static ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("Service.xml");
+	
+	public static HashMap<String,String> getChaseOrbitalConf(){
+		HashMap<String,String> ChaseOrbitalConfMap = new HashMap<String,String>();		
+		
+		String sql = "UPDATE SystemMonitor SET Status = ?, TimeStamp = CURRENT_TIMESTAMP WHERE Server = ? AND Service = ?";
+		jdbcTemplate.update(sql);
+		
+		String hostName = "";
+		String failoverName = "";
+		
+		ChaseOrbitalConfMap.put("engine.hostname", hostName);
+		ChaseOrbitalConfMap.put("engine.hostname.failover", failoverName);
+		
+		return ChaseOrbitalConfMap;
+	}
+}
