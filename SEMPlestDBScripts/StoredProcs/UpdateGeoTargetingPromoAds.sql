632824fc20cbe2ccb95f21836056c5dd5951820f
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-13 15:34:37 +0000

    geotarget is null issue
    git-svn-id: file:///semplestb/final_repo@3786 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMPlestDBScripts/StoredProcs/UpdateGeoTargetingPromoAds.sql
+++ SEMPlestDBScripts/StoredProcs/UpdateGeoTargetingPromoAds.sql
@@ -24,43 +24,47 @@ BEGIN TRY
 	
 	begin Transaction
 	
-		UPDATE promotion set LandingPageURL = @LandingUrl, DisplayUrl = @DisplayUrl where PromotionPK = @PromotionPK
+		UPDATE promotion set LandingPageURL = @LandingUrl, DisplayUrl = @DisplayUrl WHERE PromotionPK = @PromotionPK
 		
-		insert into dbo.GeoTargeting(PromotionFK,Address,City,StateCodeFK,Zip,ProximityRadius,Latitude,Longitude)
-			select @PromotionPK,g.Address,g.City,g.StateCodeFK,g.Zip,g.ProximityRadius,g.Latitude,g.Longitude from @GeoTVP g where g.operation='I'
+		INSERT INTO dbo.GeoTargeting(PromotionFK,Address,City,StateCodeFK,Zip,ProximityRadius,Latitude,Longitude)
+			select @PromotionPK,g.Address,g.City,g.StateCodeFK,g.Zip,g.ProximityRadius,g.Latitude,g.Longitude from @GeoTVP g WHERE g.operation='I'
 
-        update g SET 
+        UPDATE g SET 
 			g.Address = gt.Address, g.City= gt.City, g.StateCodeFK = gt.StateCodeFK, g.Zip = gt.Zip, 
 			g.ProximityRadius = gt.ProximityRadius,g.Latitude = gt.Latitude, g.Longitude = gt.Longitude 
 			FROM dbo.GeoTargeting g 
-			INNER JOIN @GeoTVP gt ON gt.PKEY = g.GeoTargetingPK where gt.Operation = 'U'
+			INNER JOIN @GeoTVP gt ON gt.PKEY = g.GeoTargetingPK WHERE gt.Operation = 'U'
 			
-        delete from dbo.GeoTargeting from dbo.GeoTargeting g INNER JOIN  @GEOTVP gt on gt.PKEY = g.GeoTargetingPK where gt.Operation= 'D'
+        DELETE FROM dbo.GeoTargeting FROM dbo.GeoTargeting g INNER JOIN  @GEOTVP gt ON gt.PKEY = g.GeoTargetingPK WHERE gt.Operation= 'D'
         
-        insert into dbo.PromotionAds (PromotionFK,AdTextLine1,AdTextLine2,AdTitle,CreatedDate)
-			select @PromotionPK,a.AdTextLine1,a.AdTextLine2,a.AdTitle,current_timestamp from @AdTVP a where a.Operation ='I'
+        INSERT INTO dbo.PromotionAds (PromotionFK,AdTextLine1,AdTextLine2,AdTitle,CreatedDate)
+			select @PromotionPK,a.AdTextLine1,a.AdTextLine2,a.AdTitle,current_timestamp from @AdTVP a WHERE a.Operation ='I'
 			
-		update p SET 
+		UPDATE p SET 
 			p.AdTextLine1 = a.AdTextLine1, p.AdTextLine2 = a.AdTextLine2, p.AdTitle = a.AdTitle
 			FROM dbo.PromotionAds p 
 			INNER JOIN @AdTVP a ON a.PKEY = p.PromotionAdsPK WHERE a.Operation = 'U'
 			
-		--delete from dbo.PromotionAds from dbo.PromotionAds p INNER JOIN  @AdTVP a on a.PKEY = p.PromotionAdsPK where a.Operation= 'D'
+		DELETE from dbo.PromotionAds from dbo.PromotionAds p INNER JOIN  @AdTVP a on a.PKEY = p.PromotionAdsPK where a.Operation= 'D'
 
      commit transaction   
 	END
 	
 	SELECT g.GeoTargetingPK as PKEY, gt.UID as UID from dbo.GeoTargeting g 
-	INNER JOIN @GEOTVP gt on 
-			g.Address = gt.Address and g.City= gt.City and g.StateCodeFK = gt.StateCodeFK 
-			and g.Zip = gt.Zip and g.ProximityRadius = gt.ProximityRadius and
-			g.Latitude = gt.Latitude and g.Longitude = gt.Longitude 
-	WHERE gt.operation = 'I'
+	inner JOIN @GeoTVP gt on 
+			(g.Address = gt.Address or (g.Address is null and gt.Address is null)) and
+			(g.City= gt.City or (g.City is null and gt.City is null)) and 
+			(g.StateCodeFK = gt.StateCodeFK or (g.StateCodeFK is null and gt.StateCodeFK is null)) and
+			(g.Zip  = gt.Zip  or (g.Zip  is null and gt.Zip  is null)) and 
+			(g.ProximityRadius  = gt.ProximityRadius  or (g.ProximityRadius  is null and gt.ProximityRadius  is null)) and
+			(g.Latitude  = gt.Latitude  or (g.Latitude  is null and gt.Latitude  is null)) and 
+			(g.Longitude  = gt.Longitude  or (g.Longitude  is null and gt.Longitude  is null)) 
+	WHERE gt.operation = 'I' and g.PromotionFK = @PromotionPK
 	UNION
 	SELECT p.PromotionAdsPK as PKEY, a.UID as UID from dbo.PromotionAds p 
 	INNER JOIN @AdTVP a on 
 			p.AdTextLine1 = a.AdTextLine1 and p.AdTextLine2 = a.AdTextLine2 and p.AdTitle = a.AdTitle
-	WHERE a.Operation = 'I' and p.IsDeleted=0
+	WHERE a.Operation = 'I' and p.IsDeleted=0 and p.PromotionFK = @PromotionPK
 	
 END TRY
 BEGIN CATCH
