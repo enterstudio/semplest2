8629368f92f145d78ed99e3d41e551e06bbbc7ce
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-14 13:27:11 +0000

    new types
    git-svn-id: file:///semplestb/final_repo@3795 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMPlestDBScripts/ProductionUpdate_AdvertisingEngineReportData7_30_12.sql
+++ SEMPlestDBScripts/ProductionUpdate_AdvertisingEngineReportData7_30_12.sql
@@ -1,4 +1,4 @@
-update Configuration set SemplestAdEngineReportLookbackDays = 21
+update Configuration set SemplestAdEngineReportLookbackDays = 28
 Go
 alter table promotion drop column BudgetToAddDate
 GO
@@ -1031,6 +1031,169 @@ END TRY
 BEGIN CATCH
 IF XACT_STATE() != 0 OR @@TRANCOUNT > 0
     ROLLBACK TRANSACTION;
+	DECLARE @ErrMessage	nvarchar(4000),
+          @ErrorSeverity	int,
+          @ErrorState		int;
+	SELECT @ErrMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	RAISERROR (@ErrMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
+GO
+
+CREATE TYPE [dbo].[GeoTargetTableType] AS TABLE(
+	[Address] [varchar](150) NULL,
+	[City] [varchar](100) NULL,
+	[StateCodeFK] [int] NULL,
+	[Zip] [nvarchar](15) NULL,
+	[Longitude] [decimal] (18,10) NULL,
+	[Latitude] [decimal] (18,10) NULL,
+	[ProximityRadius] [decimal] (18,10) NULL,
+	[UID] [varchar](100) NULL,
+	[PKEY] [int],
+	[Operation][varchar](1) NOT NULL
+	)
+
+
+GO
+
+CREATE TYPE [dbo].[PromoAdTableType] AS TABLE(
+	[AdTitle] [nvarchar](25) NULL,
+	[AdTextLine1] [nvarchar](35) NULL,
+	[AdTextLine2] [nvarchar](35) NULL,
+	[UID] [varchar](100) NULL,
+	[PKEY] [int],
+	[Operation][varchar](1) NOT NULL
+	)
+
+GO
+
+CREATE TYPE [dbo].[SiteLinksTableType] AS TABLE(
+	[LinkText] [nvarchar](35) NULL,
+	[LinkUrl] [varchar](100) NULL,
+	[UID] [varchar](100) NULL,
+	[PKEY] [int] NULL,
+	[Operation] [varchar](1) NOT NULL
+)
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.UpdateSiteLinks') AND type in (N'P', N'PC'))
+  DROP PROCEDURE dbo.UpdateSiteLinks;
+GO
+
+-- +----------------------------------------------------------------------------------------------------------------+
+-- | Author  - Andre                                                                                                |
+-- | Written - 8/11/2012																							|
+-- | Parms   - 																							|
+-- | Purpose - 						|
+-- +----------------------------------------------------------------------------------------------------------------+
+
+CREATE PROCEDURE dbo.UpdateSiteLinks
+(
+	@PromotionPK			INT,
+	@SlTVP					dbo.SiteLinksTableType READONLY
+)
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+	BEGIN
+	
+	begin Transaction
+        INSERT INTO dbo.SiteLinks (PromotionFK,LinkText,LinkURL)
+			select @PromotionPK,st.LinkText,st.LinkURL from @SlTVP st WHERE st.Operation ='I'
+			
+		UPDATE s SET 
+			s.LinkText = st.LinkText, s.LinkURL = st.LinkURL
+			FROM dbo.SiteLinks s 
+			INNER JOIN @SlTVP st ON st.PKEY = s.SiteLInkPK WHERE st.Operation = 'U'
+			
+		DELETE from dbo.SiteLinks from dbo.SiteLinks s INNER JOIN  @SlTVP st on st.PKEY = s.SiteLInkPK where st.Operation= 'D'
+     commit transaction   
+	END
+	
+	SELECT s.SiteLInkPK as PKEY, st.UID as UID from dbo.SiteLinks s 
+	inner JOIN @SlTVP st on 
+			st.LinkText = s.LinkText and st.LinkURL = s.LinkURL
+	WHERE st.operation = 'I' and s.PromotionFK = @PromotionPK 
+
+END TRY
+BEGIN CATCH
+	DECLARE @ErrMessage	nvarchar(4000),
+          @ErrorSeverity	int,
+          @ErrorState		int;
+	SELECT @ErrMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	RAISERROR (@ErrMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.UpdateGeoTargetingPromoAds') AND type in (N'P', N'PC'))
+  DROP PROCEDURE dbo.UpdateGeoTargetingPromoAds;
+GO
+
+-- +----------------------------------------------------------------------------------------------------------------+
+-- | Author  - Andre                                                                                                |
+-- | Written - 8/11/2012																							|
+-- | Parms   - 																							|
+-- | Purpose - 						|
+-- +----------------------------------------------------------------------------------------------------------------+
+
+CREATE PROCEDURE dbo.UpdateGeoTargetingPromoAds
+(
+	@PromotionPK            INT,
+	@LandingUrl				nvarchar(1024),
+	@DisplayUrl				nvarchar(35),
+	@GeoTVP					dbo.GeoTargetTableType READONLY,
+	@AdTVP					dbo.PromoAdTableType READONLY
+)
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+	BEGIN
+	
+	begin Transaction
+	
+		UPDATE promotion set LandingPageURL = @LandingUrl, DisplayUrl = @DisplayUrl WHERE PromotionPK = @PromotionPK
+		
+		INSERT INTO dbo.GeoTargeting(PromotionFK,Address,City,StateCodeFK,Zip,ProximityRadius,Latitude,Longitude)
+			select @PromotionPK,g.Address,g.City,g.StateCodeFK,g.Zip,g.ProximityRadius,g.Latitude,g.Longitude from @GeoTVP g WHERE g.operation='I'
+
+        UPDATE g SET 
+			g.Address = gt.Address, g.City= gt.City, g.StateCodeFK = gt.StateCodeFK, g.Zip = gt.Zip, 
+			g.ProximityRadius = gt.ProximityRadius,g.Latitude = gt.Latitude, g.Longitude = gt.Longitude 
+			FROM dbo.GeoTargeting g 
+			INNER JOIN @GeoTVP gt ON gt.PKEY = g.GeoTargetingPK WHERE gt.Operation = 'U'
+			
+        DELETE FROM dbo.GeoTargeting FROM dbo.GeoTargeting g INNER JOIN  @GEOTVP gt ON gt.PKEY = g.GeoTargetingPK WHERE gt.Operation= 'D'
+        
+        INSERT INTO dbo.PromotionAds (PromotionFK,AdTextLine1,AdTextLine2,AdTitle,CreatedDate)
+			select @PromotionPK,a.AdTextLine1,a.AdTextLine2,a.AdTitle,current_timestamp from @AdTVP a WHERE a.Operation ='I'
+			
+		UPDATE p SET 
+			p.AdTextLine1 = a.AdTextLine1, p.AdTextLine2 = a.AdTextLine2, p.AdTitle = a.AdTitle
+			FROM dbo.PromotionAds p 
+			INNER JOIN @AdTVP a ON a.PKEY = p.PromotionAdsPK WHERE a.Operation = 'U'
+			
+		DELETE from dbo.PromotionAds from dbo.PromotionAds p INNER JOIN  @AdTVP a on a.PKEY = p.PromotionAdsPK where a.Operation= 'D'
+
+     commit transaction   
+	END
+	
+	SELECT g.GeoTargetingPK as PKEY, gt.UID as UID from dbo.GeoTargeting g 
+	inner JOIN @GeoTVP gt on 
+			(g.Address = gt.Address or (g.Address is null and gt.Address is null)) and
+			(g.City= gt.City or (g.City is null and gt.City is null)) and 
+			(g.StateCodeFK = gt.StateCodeFK or (g.StateCodeFK is null and gt.StateCodeFK is null)) and
+			(g.Zip  = gt.Zip  or (g.Zip  is null and gt.Zip  is null)) and 
+			(g.ProximityRadius  = gt.ProximityRadius  or (g.ProximityRadius  is null and gt.ProximityRadius  is null)) and
+			(g.Latitude  = gt.Latitude  or (g.Latitude  is null and gt.Latitude  is null)) and 
+			(g.Longitude  = gt.Longitude  or (g.Longitude  is null and gt.Longitude  is null)) 
+	WHERE gt.operation = 'I' and g.PromotionFK = @PromotionPK
+	UNION
+	SELECT p.PromotionAdsPK as PKEY, a.UID as UID from dbo.PromotionAds p 
+	INNER JOIN @AdTVP a on 
+			p.AdTextLine1 = a.AdTextLine1 and p.AdTextLine2 = a.AdTextLine2 and p.AdTitle = a.AdTitle
+	WHERE a.Operation = 'I' and p.IsDeleted=0 and p.PromotionFK = @PromotionPK
+	
+END TRY
+BEGIN CATCH
 	DECLARE @ErrMessage	nvarchar(4000),
           @ErrorSeverity	int,
           @ErrorState		int;
