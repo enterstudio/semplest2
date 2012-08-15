a8d0ea38e9d0767e6b4b1462267a59c74e5d468d
julian <julian@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-15 18:37:09 +0000

    not checking if keyword is deleted or inactive if trying to save a bid
    git-svn-id: file:///semplestb/final_repo@3810 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMPlestDBScripts/StoredProcs/SetBidObject.sql
+++ SEMPlestDBScripts/StoredProcs/SetBidObject.sql
@@ -45,14 +45,7 @@ BEGIN TRY
 	BEGIN
 		SELECT @ErrMsg = 'The Selected keyword does not exist'; 
 		RAISERROR (@ErrMsg, 16, 1);
-	END;
-	--check to see if keyword is active and not deleted for the promo
-	if EXISTS (select * from PromotionKeywordAssociation pka 
-				inner join Keyword k on k.KeywordPK = pka.KeywordFK where pka.PromotionFK = @PromotionPK and k.Keyword = @Keyword and (pka.IsActive = 0 OR pka.IsDeleted = 1))
-	BEGIN
-		SELECT @ErrMsg = 'The Selected keyword [' + @Keyword + '] is either Inactive or Deleted for the Promotion [' + convert(varchar(10),@PromotionPK) + ']'; 
-		RAISERROR (@ErrMsg, 16, 1);
-	END;			
+	END;		
 	select @AdEngineID = a.AdvertisingEnginePK from AdvertisingEngine a where a.AdvertisingEngine = @AdvertisingEngine
 	select @BidTypeID = bt.BidTypePK from BidType bt where bt.BidType = @BidType
 	if (@IsNegative = 1)
