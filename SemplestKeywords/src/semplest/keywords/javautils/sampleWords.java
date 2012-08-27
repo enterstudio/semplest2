adeedaf6d2d9dcc2a7e4ac58e69ba9e197944e9a
raj <raj@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-27 16:55:48 +0000

    git-svn-id: file:///semplestb/final_repo@3920 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestKeywords/src/semplest/keywords/javautils/sampleWords.java
+++ SemplestKeywords/src/semplest/keywords/javautils/sampleWords.java
@@ -14,27 +14,21 @@ public class sampleWords {
   public sampleWords(){
     wcs = ioUtils.readWcounts(f);
     dl = new DmozLucene();
-    loadDesc( dl, dfile);
   }
-  private void loadDesc( DmozLucene dl, String f){
-    Map<String,String> map = ioUtils.readDescs( f );
-    for(Map.Entry<String,String> e : map.entrySet())
-      dl.add( e.getKey(), e.getValue() );
-    dl.done();
-  }
+  
 
   // Public interface ----------
-  public String getWords( String q ){
+  public String getWords( String q ) throws Exception {
     String cleanq = TextUtils.stemvString( q );
     if( cleanq.length() < 2 ) return "";
-    String[] res = dl.search( cleanq );
+    String[] res = dl.search( cleanq, 10 );
     if( res.length < 1 ) return "";
 //    System.out.println( res[0] );
     return wcs.get( res[0] );
   }
 
   //------------
-  public static void main( String[] args) {
+  public static void main( String[] args) throws Exception  {
     sampleWords sw = new sampleWords();
     
     String samplequery = "ski snowboard pants jacket";
