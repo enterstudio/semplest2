111c9e600497001049ac5cd4e447dbd623199523
julian <julian@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-29 17:19:17 +0000

    added 3 columns into configuration table needed by keyword service
    git-svn-id: file:///semplestb/final_repo@3946 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMPlestDBScripts/ProductionUpdate_release_003.sql
+++ SEMPlestDBScripts/ProductionUpdate_release_003.sql
@@ -1,2 +1,20 @@
-  update configuration set ReminderEmailUrlPrefix ='https://www.semplest.com/profile/verify?token='
-  GO
+update configuration set ReminderEmailUrlPrefix ='https://www.semplest.com/profile/verify?token='
+GO
+  
+ALTER TABLE Configuration ADD SemplestKeywordslucenedir varchar(100)
+go
+
+ALTER TABLE Configuration ADD SemplestKeywordsbdbdir varchar(100)
+go
+
+ALTER TABLE Configuration ADD SemplestKeywordsdffile varchar(100)
+go
+
+update Configuration set SemplestKeywordslucenedir = 'data/dmoz/lucene/'
+go
+
+update Configuration set SemplestKeywordsbdbdir = 'data/dmoz/bdb/'
+go
+
+update Configuration set SemplestKeywordsdffile = 'data/word.dict'
+go
