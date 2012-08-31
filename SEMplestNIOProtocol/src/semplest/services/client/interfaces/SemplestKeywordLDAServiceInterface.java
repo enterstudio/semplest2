1b4d6fa4141de3bcebcb7ffeb263d5f36fd93d8f
julian <julian@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-08-31 13:44:55 +0000

    logging exceptions, other general refactorings
    git-svn-id: file:///semplestb/final_repo@3972 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestKeywordLDAServiceInterface.java
+++ SEMplestNIOProtocol/src/semplest/services/client/interfaces/SemplestKeywordLDAServiceInterface.java
@@ -1,6 +1,6 @@
 package semplest.services.client.interfaces;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import semplest.server.protocol.adengine.GeoTargetObject;
 import semplest.server.protocol.adengine.KeywordProbabilityObject;
@@ -9,8 +9,6 @@ public interface SemplestKeywordLDAServiceInterface extends ServiceInitialize
 {
 	//Company name : not used currently
 	//N grams should be removed in the future, we are getting all ngrams???
-	public abstract ArrayList<String> getCategories(String companyName, String promotionName, String description, String[] adds, String url) throws Exception;
-	
-	public KeywordProbabilityObject[] getKeywords(ArrayList<String> categories,String companyName,  String[] searchEngines,
-			String promotionName, String description, String[] adds, String url, GeoTargetObject[] gt, Integer[] nGrams) throws Exception ;
+	public abstract List<String> getCategories(String companyName, String promotionName, String description, String[] adds, String url) throws Exception;	
+	public KeywordProbabilityObject[] getKeywords(List<String> categories,String companyName,  String[] searchEngines, String promotionName, String description, String[] adds, String url, GeoTargetObject[] gt, Integer[] nGrams) throws Exception ;
 }
