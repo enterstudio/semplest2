4e90fbb0bdc0f82ded6039f16e04bd229031e317
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-09-05 19:58:04 +0000

    git-svn-id: file:///semplestb/final_repo@4014 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestDmozDB/src/semplest/dmoz/tree/DmozImporter.java
+++ SemplestDmozDB/src/semplest/dmoz/tree/DmozImporter.java
@@ -3,15 +3,15 @@ package semplest.dmoz.tree;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import semplest.dmoz.springjdbc.BaseDB;
 import semplest.util.SemplestUtils;
 
 public class DmozImporter extends BaseDB {
+	
+	private static Integer maxBatchSize = 5000;
 	
 	public static void main(String[] args){
 		try {
@@ -30,8 +30,8 @@ public class DmozImporter extends BaseDB {
 		
 		String dmozTreeFile = properties.getProperty("dmoz.treeFile");
 		String categoryCidFile = properties.getProperty("dmoz.idFile");
-		String categoryDescriptionFile = properties.getProperty("dmoz.urlFile");
-		String categoryUrlsFile = properties.getProperty("dmoz.descriptionFile");		
+		String categoryDescriptionFile = properties.getProperty("dmoz.descriptionFile");
+		String categoryUrlsFile = properties.getProperty("dmoz.urlFile");		
 		
 		//Build DMOZ Tree
 		System.out.println("Building Dmoz tree and getting all tree nodes...");
@@ -41,34 +41,61 @@ public class DmozImporter extends BaseDB {
 		
 		//Store DMOZ tree to a file
 		System.out.println("Storing Dmoz tree to file...");
-		storeTreeToFile(dmozTree, dmozTreeFile);		
+		storeTreeToFile(dmozTree, dmozTreeFile);	
+		
+		System.out.println("Done.");
+		
+		//Batch and store the tree to database
+		final List<List<DmozTreeNode>> batches = SemplestUtils.getBatches(dmozTree, maxBatchSize);
+		System.out.println("Going to store " + batches.size() + " batchs (of " + maxBatchSize + " node entries) to DB.");
+		Long counter = 0L;		
+		for (final List<DmozTreeNode> batch : batches)
+		{			
+			Long start = System.currentTimeMillis();
+			System.out.println("Storing batch #" + counter + " to DB...");
+			ArrayList<String> batchSql = new ArrayList<String>();
+			for(DmozTreeNode node : batch){
+				if(node == null){
+					continue;
+				}
+				Long nodeId = node.getNodeID();
+				Long nodeParentId = node.getParentID();
+				String nodeFullName = node.getFullName().replace("'", "''");
+				String nodeDescription = node.getCategoryData() == null? null : (node.getCategoryData().getDescription() == null? null : node.getCategoryData().getDescription().replace("'", "''"));				
+				
+				String sqlDmoz = "INSERT INTO DMOZ(DmozNodePK,NodeText,ParentNodeID,NodeDescription) " +
+						"VALUES("+ nodeId +",'" + nodeFullName + "'," + nodeParentId + ",'" + nodeDescription + "');";
+				
+				batchSql.add(sqlDmoz);
+				
+				if(node.getCategoryData() == null || node.getCategoryData().getUrlsAndDescs() == null){
+					continue;
+				}
+				for(String url : node.getCategoryData().getUrlsAndDescs().keySet()){
+					String urlDesc = node.getCategoryData().getUrlsAndDescs().get(url);
+					String sqlUrl = "INSERT INTO URLData(DmozNodeFK,URL,Level,URLDescription) " +
+							"VALUES(" + nodeId + ",'" + url.replace("'", "''") + "'," + 1 + "," + urlDesc + ");";
+					batchSql.add(sqlUrl);
+				}
+			}
+			jdbcTemplate.batchUpdate(batchSql.toArray(new String[batchSql.size()]));
+			System.out.println("	took " + (System.currentTimeMillis() - start)*1d/1000 + " secs.");
+			counter++;
+		}
 	}	
 	
 	//helper methods
 	private static void storeTreeToFile(List<DmozTreeNode> dmozTree, String path) throws Exception{
 		FileWriter writer = new FileWriter(path);
-		writer.write("DmozNodePK,SemplestID,NodeText,ParentNodeID,NodeDescription\n");
-		/*
+		writer.write("DmozNodePK|NodeText|ParentNodeID|NodeDescription\n");
+		
 		for(DmozTreeNode node : dmozTree){		
 			writer.append(node.getNodeID() + "," 
-						+ node.getCategoryData().getCategoryId() + "," 
 						+ node.getFullName() + ","
 						+ node.getParentID() + ","
 						+ node.getCategoryData().getDescription() + "\n");
-		}
-		*/
+		}		
 		
-		for(int i = 0; i < 100; i++){
-			String desc = dmozTree.get(i).getCategoryData() == null? null :  dmozTree.get(i).getCategoryData().getDescription();
-			Long id = dmozTree.get(i).getCategoryData() == null? null : dmozTree.get(i).getCategoryData().getCategoryId();
-			writer.append(dmozTree.get(i).getNodeID() + "," 
-					+ id + "," 
-					+ dmozTree.get(i).getFullName() + ","
-					+ dmozTree.get(i).getParentID() + ","
-					+ desc + "\n");
-		}
-		
-		writer.close();		
 	}
 
 }
