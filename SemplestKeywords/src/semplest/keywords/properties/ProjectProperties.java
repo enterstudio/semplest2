8381e4173ba5b1980bbe84b34f12f8f09e9e9ba7
julian <julian@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-09-10 13:22:56 +0000

    persisting keyword data into sql database
    git-svn-id: file:///semplestb/final_repo@4036 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestKeywords/src/semplest/keywords/properties/ProjectProperties.java
+++ SemplestKeywords/src/semplest/keywords/properties/ProjectProperties.java
@@ -1,9 +1,7 @@
 package semplest.keywords.properties;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
