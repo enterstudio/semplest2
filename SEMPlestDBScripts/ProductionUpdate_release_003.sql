b097a76f8dfa77cefd0d3a81f614fa68d7be3318
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-09-11 15:31:39 +0000

    git-svn-id: file:///semplestb/final_repo@4052 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMPlestDBScripts/ProductionUpdate_release_003.sql
+++ SEMPlestDBScripts/ProductionUpdate_release_003.sql
@@ -25,3 +25,35 @@ go
 
 update Configuration set numTopics = 120
 go
+
+---
+--- CREATE TABLE: dbo.UserType
+---
+CREATE TABLE dbo.UserType
+(
+	UserTypePK int NOT NULL IDENTITY,
+	UserType varchar(50) NOT NULL,
+	PRIMARY KEY CLUSTERED (UserTypePK)
+)
+GO
+---
+--- CREATE COLUMN: UserTypeFK
+---
+ALTER TABLE dbo.Users ADD UserTypeFK int
+GO
+---
+--- CREATE FOREIGN KEY CONSTRAINT: REL_UserType_Users_2
+---
+ALTER TABLE dbo.Users ADD 
+	CONSTRAINT REL_UserType_Users_2 FOREIGN KEY (UserTypeFK)
+		REFERENCES dbo.UserType(UserTypePK)
+GO
+
+---
+--- CREATE COLUMN: IsKeywordServiceOnly
+---
+ALTER TABLE dbo.Promotion ADD IsKeywordServiceOnly bit NOT NULL DEFAULT 1
+GO
+
+insert into UserType(UserType) Values ('KeywordOnly')
+insert into UserType(UserType) Values ('KeywordBid')
