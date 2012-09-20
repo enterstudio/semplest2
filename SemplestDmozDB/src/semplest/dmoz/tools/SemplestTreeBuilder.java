a48e44577dedbe6ac01210a23547260c5a53d4ca
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-09-20 20:00:51 +0000

    git-svn-id: file:///semplestb/final_repo@4160 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestDmozDB/src/semplest/dmoz/tools/SemplestTreeBuilder.java
+++ SemplestDmozDB/src/semplest/dmoz/tools/SemplestTreeBuilder.java
@@ -1,54 +1,40 @@
 package semplest.dmoz.tools;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
+import semplest.dmoz.SemplestTreeDB;
+import semplest.dmoz.pruning.CharDigLeafFilter;
+import semplest.dmoz.pruning.DmozToSemplestFilter;
+import semplest.dmoz.pruning.DmozToSemplestTreeConverter;
+import semplest.dmoz.pruning.NorthAmericaUSMergingFilter;
+import semplest.dmoz.pruning.RegionalRemovalFilter;
 import semplest.dmoz.tree.DmozTreeNode;
 import semplest.dmoz.tree.TreeFunctions;
 
 public class SemplestTreeBuilder {
 	
-	public void buildMap() throws Exception
-	{
-		Map<Long,Long> semplestDmozNodesMap = new HashMap<Long,Long>();
-		Map<Long,Long> semplestUrlDataMap = new HashMap<Long,Long>();
-		
+	public static void main(String[] args) throws Exception
+	{		
 		//load the entire tree from the DB
-		DmozTreeNode dmozTree = DbTreeOperator.loadTreeFromDB("top");
+		DmozTreeNode dmozTree = DbTreeOperator.loadTreeFromDB("top/business/financial_services");
 		
 		//apply processors on the tree, and convert it
-		//TODO
+		//TODO				
+		DmozToSemplestTreeConverter dmozToSemplestConv = new DmozToSemplestTreeConverter(dmozTree);
+		Set<DmozToSemplestFilter> filterSet = new HashSet<DmozToSemplestFilter>();
+		filterSet.add(new RegionalRemovalFilter(null));
+		filterSet.add(new CharDigLeafFilter(null));
+		filterSet.add(new NorthAmericaUSMergingFilter(null));
+		dmozToSemplestConv.setFilterSet(filterSet);
+		dmozToSemplestConv.prune();			
 		
-		//now we have the new tree, it has the exactly same nodeID for each node, but different relationship between nodes/node data
-		//generate tree map
-		List<DmozTreeNode> treeNodesList = TreeFunctions.getTreeInList(dmozTree);
-		
-		for(DmozTreeNode treeNode : treeNodesList)
-		{
-			Long nodeId = treeNode.getNodeID();
-			Long parentId = treeNode.getParentID();				
-			Set<Long> urlIds = treeNode.getSemplestTreeMapData().getSemplestUrlDataIDs();
-			
-			semplestDmozNodesMap.put(nodeId, parentId);
-			
-			for(Long urlId : urlIds)
-			{
-				semplestUrlDataMap.put(nodeId, urlId);
-			}
-		}
+		DmozTreeNode semplestTree = dmozToSemplestConv.getTree();			
 		
 		//put the tree maps in database
-		//TODO
-		
-	}
-	
-	private void storeSemplestDmozMap(Map<Long,Long> semplestDmozNodesMap){
-		
-	}
-	
-	private void storeSemplestUrlDataMap(Map<Long,Long> semplestUrlDataMap){
+		List<DmozTreeNode> semplestTreeNodes = TreeFunctions.getTreeInList(semplestTree);
+		SemplestTreeDB.AddSemplestTree(semplestTreeNodes);
 		
 	}
 	
