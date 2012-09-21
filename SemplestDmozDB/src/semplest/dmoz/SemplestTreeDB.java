58ecb597b4c8aff914aef1f3278eb8b7dd09bc56
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-09-21 15:44:04 +0000

    git-svn-id: file:///semplestb/final_repo@4170 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestDmozDB/src/semplest/dmoz/SemplestTreeDB.java
+++ SemplestDmozDB/src/semplest/dmoz/SemplestTreeDB.java
@@ -12,24 +12,10 @@ import semplest.dmoz.springjdbc.BaseDB;
 import semplest.dmoz.tree.DmozTreeNode;
 import semplest.dmoz.tree.UrlDataObject;
 
-public class SemplestTreeDB extends BaseDB{
-	
-	public static void AddSemplestTree(final List<DmozTreeNode> semplestTreeNodes)
-	{
-		//form the request list
-		final List<AddSemplestTreeRequest> addSemplestTreeRequests = new ArrayList<AddSemplestTreeRequest>();
-		for(DmozTreeNode node : semplestTreeNodes){
-			List<UrlDataObject> list = node.getCategoryData().getUrlData();
-			for(UrlDataObject urlData : list){				
-				AddSemplestTreeRequest request = new AddSemplestTreeRequest();
-				request.setDMOZSemplestPK(node.getNodeID());
-				request.setDMOZURLDataPK(urlData.getUrlDataPK());
-				request.setDomain(getDomain(urlData.getUrl()));
-				
-				addSemplestTreeRequests.add(request);
-			}
-		}		
+public class SemplestTreeDB extends BaseDB{	
 		
+	public static void AddSemplestTree(final List<AddSemplestTreeRequest> addSemplestTreeRequests)
+	{				
 		//call the store proc in batch
 		String sql = "{call AddSemplestTree(?,?,?)}";
 		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter()
@@ -49,28 +35,5 @@ public class SemplestTreeDB extends BaseDB{
 				return addSemplestTreeRequests.size();
 			}
 		});
-	}
-	
-	//helper method
-	private static String getDomain(String url)
-	{
-		String[] parts = url.split("://");
-		String subUrl;
-		if(parts.length > 1){
-			subUrl = parts[1];
-		}
-		else{
-			subUrl = parts[0];
-		}
-		int index = subUrl.indexOf("/");
-		String domain;
-		if(index > 0){
-			domain = subUrl.substring(0, index);
-		}
-		else{
-			domain = subUrl;
-		}
-		
-		return domain;
 	}
 }
