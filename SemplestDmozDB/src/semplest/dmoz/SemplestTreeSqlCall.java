b6e73b0ce1d3b28c6c4a192a53bd58e318a153d1
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-09-21 15:16:28 +0000

    git-svn-id: file:///semplestb/final_repo@4167 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestDmozDB/src/semplest/dmoz/SemplestTreeSqlCall.java
+++ SemplestDmozDB/src/semplest/dmoz/SemplestTreeSqlCall.java
@@ -1,5 +1,16 @@
 package semplest.dmoz;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+
+import semplest.dmoz.objects.AddSemplestTreeRequest;
+import semplest.dmoz.tree.DmozTreeNode;
+import semplest.dmoz.tree.UrlDataObject;
+
 public class SemplestTreeSqlCall extends DmozDB
 {
 	
@@ -15,10 +26,68 @@ public class SemplestTreeSqlCall extends DmozDB
 			"insert into NodeURLAssociation(SemplestFK, URLDataFK, [Level], ParentURLDataID) " +
 			"select ?, @urlID, ud.Level,ud.ParentURLDataID from URLData ud where ud.UrlDataPK = ?";
 	
-	public static void runSemplestTree(int DMOZSemplestPK, int DMOZURLDataPK, String Domain)
+	public static void runSemplestTree(final List<DmozTreeNode> semplestTreeNodes) //int DMOZSemplestPK, int DMOZURLDataPK, String Domain)
 	{
-		final int rowcount = jdbcTemplate.update(sql, 
-				 new Object[]{DMOZSemplestPK,DMOZSemplestPK, Domain, Domain, Domain,DMOZURLDataPK, DMOZURLDataPK, DMOZSemplestPK, DMOZURLDataPK});
+		final List<AddSemplestTreeRequest> addSemplestTreeRequests = new ArrayList<AddSemplestTreeRequest>();
+		for(DmozTreeNode node : semplestTreeNodes){
+			List<UrlDataObject> list = node.getCategoryData().getUrlData();
+			for(UrlDataObject urlData : list){				
+				AddSemplestTreeRequest request = new AddSemplestTreeRequest();
+				request.setDMOZSemplestPK(node.getNodeID());
+				request.setDMOZURLDataPK(urlData.getUrlDataPK());
+				request.setDomain(getDomain(urlData.getUrl()));
+				
+				addSemplestTreeRequests.add(request);
+			}
+		}
+		//final int rowcount = jdbcTemplate.update(sql, 
+		//		 new Object[]{DMOZSemplestPK,DMOZSemplestPK, Domain, Domain, Domain,DMOZURLDataPK, DMOZURLDataPK, DMOZSemplestPK, DMOZURLDataPK});
+		
+		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter()
+		{
+			@Override
+			public void setValues(PreparedStatement ps, int i) throws SQLException
+			{
+				AddSemplestTreeRequest request = addSemplestTreeRequests.get(i);
+				ps.setLong(1, request.getDMOZSemplestPK());
+				ps.setLong(2, request.getDMOZSemplestPK());
+				ps.setString(3, request.getDomain());
+				ps.setString(4, request.getDomain());
+				ps.setString(5, request.getDomain());
+				ps.setLong(6, request.getDMOZURLDataPK());
+				ps.setLong(7, request.getDMOZURLDataPK());
+				ps.setLong(8, request.getDMOZSemplestPK());
+				ps.setLong(9, request.getDMOZURLDataPK());
+				
+			}
+			@Override
+			public int getBatchSize()
+			{
+				return addSemplestTreeRequests.size();
+			}
+
+		});
+	}
+	private static String getDomain(String url)
+	{
+		String[] parts = url.split("://");
+		String subUrl;
+		if(parts.length > 1){
+			subUrl = parts[1];
+		}
+		else{
+			subUrl = parts[0];
+		}
+		int index = subUrl.indexOf("/");
+		String domain;
+		if(index > 0){
+			domain = subUrl.substring(0, index);
+		}
+		else{
+			domain = subUrl;
+		}
+		
+		return domain;
 	}
 
 }
