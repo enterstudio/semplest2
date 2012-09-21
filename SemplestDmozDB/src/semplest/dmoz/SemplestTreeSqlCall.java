1633fe7c5d2df950f58d98e29e6c4220a26e2943
Nan <Nan@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-09-21 15:43:57 +0000

    git-svn-id: file:///semplestb/final_repo@4169 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SemplestDmozDB/src/semplest/dmoz/SemplestTreeSqlCall.java
+++ SemplestDmozDB/src/semplest/dmoz/SemplestTreeSqlCall.java
@@ -2,14 +2,11 @@ package semplest.dmoz;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 
 import semplest.dmoz.objects.AddSemplestTreeRequest;
-import semplest.dmoz.tree.DmozTreeNode;
-import semplest.dmoz.tree.UrlDataObject;
 
 public class SemplestTreeSqlCall extends DmozDB
 {
@@ -26,23 +23,8 @@ public class SemplestTreeSqlCall extends DmozDB
 			"insert into NodeURLAssociation(SemplestFK, URLDataFK, [Level], ParentURLDataID) " +
 			"select ?, @urlID, ud.Level,ud.ParentURLDataID from URLData ud where ud.UrlDataPK = ?";
 	
-	public static void runSemplestTree(final List<DmozTreeNode> semplestTreeNodes) //int DMOZSemplestPK, int DMOZURLDataPK, String Domain)
-	{
-		final List<AddSemplestTreeRequest> addSemplestTreeRequests = new ArrayList<AddSemplestTreeRequest>();
-		for(DmozTreeNode node : semplestTreeNodes){
-			List<UrlDataObject> list = node.getCategoryData().getUrlData();
-			for(UrlDataObject urlData : list){				
-				AddSemplestTreeRequest request = new AddSemplestTreeRequest();
-				request.setDMOZSemplestPK(node.getNodeID());
-				request.setDMOZURLDataPK(urlData.getUrlDataPK());
-				request.setDomain(getDomain(urlData.getUrl()));
-				
-				addSemplestTreeRequests.add(request);
-			}
-		}
-		//final int rowcount = jdbcTemplate.update(sql, 
-		//		 new Object[]{DMOZSemplestPK,DMOZSemplestPK, Domain, Domain, Domain,DMOZURLDataPK, DMOZURLDataPK, DMOZSemplestPK, DMOZURLDataPK});
-		
+	public static void runSemplestTree(final List<AddSemplestTreeRequest> addSemplestTreeRequests) //int DMOZSemplestPK, int DMOZURLDataPK, String Domain)
+	{		
 		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter()
 		{
 			@Override
@@ -67,27 +49,6 @@ public class SemplestTreeSqlCall extends DmozDB
 			}
 
 		});
-	}
-	private static String getDomain(String url)
-	{
-		String[] parts = url.split("://");
-		String subUrl;
-		if(parts.length > 1){
-			subUrl = parts[1];
-		}
-		else{
-			subUrl = parts[0];
-		}
-		int index = subUrl.indexOf("/");
-		String domain;
-		if(index > 0){
-			domain = subUrl.substring(0, index);
-		}
-		else{
-			domain = subUrl;
-		}
-		
-		return domain;
 	}
 
 }
