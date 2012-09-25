39b044eda3e7e61a8877ba89e9eed08322449bed
mitch <mitch@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-09-25 18:49:14 +0000

    git-svn-id: file:///semplestb/final_repo@4201 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMplestNIOProtocol/src/semplest/service/filetransfer/SecureFTPUsingSSH.java
+++ SEMplestNIOProtocol/src/semplest/service/filetransfer/SecureFTPUsingSSH.java
@@ -123,7 +123,7 @@ public class SecureFTPUsingSSH
 		}
 		catch (JSchException e)
 		{
-			logger.error("SSH Connect Failed " + e.getMessage() + user + ":" +  host + ":" + String.valueOf(port) );
+			logger.error("SSH Connect Failed " + e.getMessage() + user + ":" +  host + ":" + String.valueOf(port), e );
 			return false;
 		}
 		return true;
@@ -147,7 +147,7 @@ public class SecureFTPUsingSSH
 		}
 		catch (SftpException e)
 		{
-			throw new IOException("Error Could not delete file " + fileName + ": " + e.getMessage());
+			throw new IOException("Error Could not delete file " + fileName + ": " + e.getMessage(), e);
 		}
 		return true;
 	}
@@ -173,8 +173,8 @@ public class SecureFTPUsingSSH
 		}
 		catch (Exception e)
 		{
-			logger.error("Disconnect error..." + e.getMessage());
-			throw new IOException(e.getMessage());
+			logger.error("Disconnect error..." + e.getMessage(), e);
+			throw new IOException(e.getMessage(), e);
 		}
 	}
 
@@ -197,7 +197,7 @@ public class SecureFTPUsingSSH
 		}
 		catch (IOException e)
 		{
-			throw new IOException("Error in fileExists : " + e.getMessage());
+			throw new IOException("Error in fileExists : " + e.getMessage(), e);
 		}
 		return false;
 	}
@@ -222,7 +222,7 @@ public class SecureFTPUsingSSH
 		}
 		catch (Exception e)
 		{
-			throw new IOException(e.getMessage());
+			throw new IOException(e.getMessage(), e);
 		}
 
 	}
@@ -288,7 +288,7 @@ public class SecureFTPUsingSSH
 		}
 		catch (SftpException e)
 		{
-			throw new IOException("Error in getFileList for: " + dir + " : " + e.getMessage());
+			throw new IOException("Error in getFileList for: " + dir + " : " + e.getMessage(), e);
 		}
 	}
 
@@ -408,7 +408,7 @@ public class SecureFTPUsingSSH
 		}
 		catch (SftpException e)
 		{
-			throw new IOException("Error sending file " + localfile.getName() + " remoteFileName " + remoteFileName + ":" + e.getMessage());
+			throw new IOException("Error sending file " + localfile.getName() + " remoteFileName " + remoteFileName + ":" + e.getMessage(), e);
 		}
 	}
 
@@ -431,7 +431,7 @@ public class SecureFTPUsingSSH
 		
 		catch (SftpException e)
 		{
-			logger.error("renameRemoteFile " + e.getMessage());
+			logger.error("renameRemoteFile " + e.getMessage(), e);
 			throw new IOException(e.getMessage());
 		}
 	}
@@ -452,7 +452,7 @@ public class SecureFTPUsingSSH
 		}
 		catch (IOException e)
 		{
-			logger.error(e.getMessage());
+			logger.error(e.getMessage(), e);
 			throw e;
 		}
 	}
@@ -493,12 +493,5 @@ public class SecureFTPUsingSSH
 			return false;
 		}
 	}
-	/*
-	 * private static String help = "      Available commands:\n"+ "      * means unimplemented command.\n"+ "cd path                       Change remote directory to 'path'\n"+ "lcd path                      Change local directory to 'path'\n"+ "chgrp grp path                Change group of file 'path' to 'grp'\n"+ "chmod mode path               Change permissions of file 'path' to 'mode'\n"+ "chown own path                Change owner of file 'path' to 'own'\n"+
-	 * "help                          Display this help text\n"+ "get remote-path [local-path]  Download file\n"+ "get-resume remote-path [local-path]  Resume to download file.\n"+ "get-append remote-path [local-path]  Append remote file to local file\n"+ "*lls [ls-options [path]]      Display local directory listing\n"+ "ln oldpath newpath            Symlink remote file\n"+ "*lmkdir path                  Create local directory\n"+ "lpwd                          Print local working directory\n"+
-	 * "ls [path]                     Display remote directory listing\n"+ "*lumask umask                 Set local umask to 'umask'\n"+ "mkdir path                    Create remote directory\n"+ "put local-path [remote-path]  Upload file\n"+ "put-resume local-path [remote-path]  Resume to upload file\n"+ "put-append local-path [remote-path]  Append local file to remote file.\n"+ "pwd                           Display remote working directory\n"+
-	 * "stat path                     Display info about path\n"+ "exit                          Quit sftp\n"+ "quit                          Quit sftp\n"+ "rename oldpath newpath        Rename remote file\n"+ "rmdir path                    Remove remote directory\n"+ "rm path                       Delete remote file\n"+ "symlink oldpath newpath       Symlink remote file\n"+ "readlink path                 Check the target of a symbolic link\n"+
-	 * "realpath path                 Canonicalize the path\n"+ "rekey                         Key re-exchanging\n"+ "compression level             Packet compression will be enabled\n"+ "version                       Show SFTP version\n"+ "?                             Synonym for help"; }
-	 */
 
 }
