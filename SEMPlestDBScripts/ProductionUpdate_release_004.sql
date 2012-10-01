65243e098f5dc68dac7b63ccad81e3d5d721d9a6
julian <julian@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-10-01 19:14:57 +0000

    git-svn-id: file:///semplestb/final_repo@4247 02200ff9-b5b2-46f0-9171-221b09c08c7b

--- SEMPlestDBScripts/ProductionUpdate_release_004.sql
+++ SEMPlestDBScripts/ProductionUpdate_release_004.sql
@@ -1,16 +1,6 @@
 /*
 update the status of existing campaigns
 */
----
---- CREATE COLUMN: KeywordTopPercent
----
-ALTER TABLE dbo.Configuration ADD KeywordTopPercent float DEFAULT 0.65
-GO
-
-update Configuration set KeywordTopPercent = 0.65
-
-insert into Job values ('CLEANER', GETDATE())
-insert into Job values ('CHASE_INTEGRATION', GETDATE())
 
 --Live
 insert into PromotionAdengineStatus(PromotionFK,AdvertisingEngineFK,PromotionStatusFK)
@@ -51,3 +41,66 @@ inner join PromotionStatus ps on ps.PromotionStatusPK = paes.PromotionStatusFK
 group by paes.PromotionFK )
 --order by p.PromotionPK
 
+
+
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.GetAllPromotionData') AND type in (N'P', N'PC'))
+  DROP PROCEDURE dbo.GetAllPromotionData;
+GO
+
+-- +----------------------------------------------------------------------------------------------------------------+
+-- | Author  - Mitch                                                                                                |
+-- | Written - 4/20/2012																							|
+-- | Parms   - 																							|
+-- | Purpose - 						|
+-- +----------------------------------------------------------------------------------------------------------------+
+
+CREATE PROCEDURE dbo.GetAllPromotionData
+(
+	@PromotionPK            INT
+)
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+	DECLARE @ErrMsg VARCHAR(250)
+
+	--validate data
+	IF NOT EXISTS (select * from Promotion p where p.PromotionPK =@PromotionPK)
+	BEGIN
+		SELECT @ErrMsg = 'The Promotion was not found.'; 
+		RAISERROR (@ErrMsg, 16, 1);
+	END;
+	--get general info
+	select p.PromotionPK,p.ProductGroupFK,p.PromotionName,p.PromotionDescription,p.LandingPageURL,p.DisplayURL,
+		p.PromotionBudgetAmount,p.PromotionStartDate, p.PromotionEndDate,bc.BudgetCycle,
+		p.CycleStartDate,
+		p.RemainingBudgetInCycle,p.StartBudgetInCycle,
+		p.EditedDate, p.IsLaunched, p.IsCompleted,p.IsPaused,p.CreatedDate, aep.AdvertisingEngineCampaignPK, aep.AdvertisingEngineAdGroupID , 
+		aea.AdvertisingEngineAccountPK,aea.AdvertisingEngineAccountNumber, ae.AdvertisingEngine, p.IsAutobid, p.AutoBidMaxCPC, p.CycleEndDate, P.CreditCardProfileFK
+
+		from Promotion p 
+		inner join BudgetCycle bc on bc.BudgetCyclePK = p.BudgetCycleFK
+		left join AdvertisingEnginePromotion aep on aep.PromotionFK = p.PromotionPK
+		left join AdvertisingEngineAccount aea on aea.AdvertisingEngineAccountPK = aep.AdvertisingEngineAccountFK
+		left join AdvertisingEngine ae on aea.AdvertisingEngineFK = ae.AdvertisingEnginePK
+		where p.PromotionPK  = @PromotionPK
+		--get ADs
+		select pa.PromotionAdsPK,pa.PromotionFK,  pa.AdTitle, pa.AdTextLine1, pa.AdTextLine2, aea.AdvertisingEngineAdPK [AdEngineAdID], pa.IsDeleted, pa.CreatedDate, pa.DeletedDate, aea.AdvertisingEngineFK from Promotion p
+		inner join PromotionAds pa on pa.PromotionFK = p.PromotionPK
+		left outer join AdvertisingEngineAds aea on aea.PromotionAdsFK = pa.PromotionAdsPK
+		where p.PromotionPK = @PromotionPK
+		--get Geotargeting
+		select gt.Address, gt.City,st.StateAbbr [State], gt.Zip, gt.Latitude, gt.Longitude,gt.ProximityRadius [Radius] from Promotion p 
+		inner join GeoTargeting gt on gt.PromotionFK = p.PromotionPK
+		left join StateCode st on st.StateAbbrPK = gt.StateCodeFK
+		where p.PromotionPK = @PromotionPK			 
+	
+END TRY
+BEGIN CATCH
+	DECLARE @ErrMessage	nvarchar(4000),
+          @ErrorSeverity	int,
+          @ErrorState		int;
+	SELECT @ErrMessage = ERROR_MESSAGE(), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	RAISERROR (@ErrMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
+GO
