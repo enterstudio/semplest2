8b933707908a4231cba8b67ff61621ba7e00c398
andre <andre@02200ff9-b5b2-46f0-9171-221b09c08c7b>
2012-10-12 19:02:03 +0000

    billtype stuff
    git-svn-id: file:///semplestb/final_repo@4273 02200ff9-b5b2-46f0-9171-221b09c08c7b

---
+++ SemplestWeb/SemplestAll/SharedResources/Models/Repositories/BillTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+
+namespace SharedResources.Models.Repositories
+{
+    public class BillTypeRepository : IBillTypeRepository
+    {
+        private readonly SemplestModel.Semplest _dbcontext;
+
+        public BillTypeRepository(SemplestModel.Semplest dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public int GetBillTypeCode(string billType)
+        {
+            return _dbcontext.BillTypes.Single(t => t.BillType1 == billType).BillTypePK;
+        }
+    }
+}
---
+++ SemplestWeb/SemplestAll/SharedResources/Models/Repositories/IBillTypeRepository.cs
@@ -0,0 +1,7 @@
+namespace SharedResources.Models.Repositories
+{
+    public interface IBillTypeRepository
+    {
+        int GetBillTypeCode(string billType);
+    }
+}
